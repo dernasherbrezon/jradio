2d9bbce216255c7501f7adc443364f13311f1327
dernasherbrezon <rodionovamp@mail.ru>
2017-12-16 13:02:45 +0000

    create waterfall

--- src/main/java/ru/r2cloud/jradio/sink/Waterfall.java
+++ src/main/java/ru/r2cloud/jradio/sink/Waterfall.java
@@ -12,29 +12,28 @@ import ru.r2cloud.jradio.source.WavFileSource;
 public class Waterfall implements Closeable {
 
 	private final int d_fftsize;
-	private final int d_wintype;
-	private final double d_center_freq;
 	private final WavFileSource source;
 	private final FloatFFT_1D fft;
 	private final int numRowsPerSecond;
 	private final WaterfallPalette palette = new WaterfallPalette(0.0f, -160.0f, 0x000000, 0x0000e7, 0x0094ff, 0x00ffb8, 0x2eff00, 0xffff00, 0xff8800, 0xff0000, 0xff007c);
 
-	public Waterfall(WavFileSource source, int d_wintype, double d_center_freq, int numRowsPerSecond, int width) {
+	public Waterfall(WavFileSource source, int numRowsPerSecond, int width) {
 		this.source = source;
 		this.d_fftsize = width;
-		this.d_wintype = d_wintype;
-		this.d_center_freq = d_center_freq;
 		this.fft = new FloatFFT_1D(d_fftsize);
 		this.numRowsPerSecond = numRowsPerSecond;
 	}
 
 	public BufferedImage save() throws IOException {
-		int height = (int) (source.getFrameLength() / (source.getFormat().getSampleRate() / numRowsPerSecond));
+		int maxPossibleNumberOfBlocksPerRow = (int) (source.getFormat().getSampleRate() / numRowsPerSecond);
+		// requested number of rows might be more than available in fft. adjust height
+		if (maxPossibleNumberOfBlocksPerRow < d_fftsize) {
+			maxPossibleNumberOfBlocksPerRow = d_fftsize;
+		}
+		int height = (int) (source.getFrameLength() / maxPossibleNumberOfBlocksPerRow);
 		BufferedImage image = new BufferedImage(d_fftsize, height, BufferedImage.TYPE_INT_RGB);
 
-		int maxPossibleNumberOfBlocksPerRow = (int) (source.getFormat().getSampleRate() / numRowsPerSecond);
 		int skipOnEveryRow = maxPossibleNumberOfBlocksPerRow - d_fftsize;
-		//FIXME skipOnEveryRow could be negative!!
 
 		float iNormalizationFactor = (float) 1 / d_fftsize;
 
@@ -46,9 +45,13 @@ public class Waterfall implements Closeable {
 				float[] newBuf = new float[d_fftsize * 2];
 				for (int i = 0; i < newBuf.length; i += 2) {
 					newBuf[i] = source.readFloat();
-					newBuf[i + 1] = 0.0f;
+					if (source.getFormat().getChannels() == 2) {
+						newBuf[i + 1] = source.readFloat();
+					} else {
+						newBuf[i + 1] = 0.0f;
+					}
 				}
-				// apply windowing function to the previous data
+				// TODO apply windowing function to the previous data
 				previousBuf = newBuf;
 				fft.complexForward(previousBuf);
 				float[] result = new float[d_fftsize];
@@ -64,19 +67,17 @@ public class Waterfall implements Closeable {
 				System.arraycopy(result, length, result, 0, result.length - length);
 				System.arraycopy(tmp, 0, result, result.length - length, length);
 
-				System.out.println(currentRow);
 				for (int i = 0; i < result.length; i++) {
-					image.setRGB(i, currentRow, palette.getRGB(result[i]));
-					// graphic.setColor(curColor);
-					// graphic.
-					// graphic.fillRect(i * blockWidth, currentRow *
-					// blockHeight, blockWidth, blockHeight);
+					image.setRGB(i, height - currentRow - 1, palette.getRGB(result[i]));
 				}
 
 				currentRow++;
 
 				for (int i = 0; i < skipOnEveryRow; i++) {
 					source.readFloat();
+					if (source.getFormat().getChannels() == 2) {
+						source.readFloat();
+					}
 				}
 			} catch (EOFException e) {
 				break;
--- src/test/java/ru/r2cloud/jradio/sink/WaterfallTest.java
+++ src/test/java/ru/r2cloud/jradio/sink/WaterfallTest.java
@@ -1,45 +1,49 @@
 package ru.r2cloud.jradio.sink;
 
+import static org.junit.Assert.assertEquals;
+
 import java.awt.image.BufferedImage;
-import java.io.BufferedInputStream;
+import java.io.EOFException;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.UUID;
 
 import javax.imageio.ImageIO;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import ru.r2cloud.jradio.WavFileSourceTest;
 import ru.r2cloud.jradio.source.WavFileSource;
 
 public class WaterfallTest {
 
+	@Rule
+	public TemporaryFolder tempFolder = new TemporaryFolder();
+
 	@Test
 	public void testSuccess() throws Exception {
-		Waterfall waterfall = new Waterfall(new WavFileSource(new BufferedInputStream(new FileInputStream("/Users/dernasherbrezon/Downloads/1.wav"))), 1, 0.0, 50, 1024);
-//		Waterfall waterfall = new Waterfall(new WavFileSource(WaterfallTest.class.getClassLoader().getResourceAsStream("aausat-4.wav")), 1, 0.0, 100, 1024);
+		Waterfall waterfall = new Waterfall(new WavFileSource(WaterfallTest.class.getClassLoader().getResourceAsStream("aausat-4.wav")), 100, 1024);
 		BufferedImage image = waterfall.save();
-		ImageIO.write(image, "png", new FileOutputStream("test7.png"));
+		File actual = new File(tempFolder.getRoot(), UUID.randomUUID().toString());
+		ImageIO.write(image, "png", new FileOutputStream(actual));
 		waterfall.close();
-	}
-
-	public static void main(String[] args) throws Exception {
-		
-		AudioInputStream ais = AudioSystem.getAudioInputStream(new BufferedInputStream(new FileInputStream("/Users/dernasherbrezon/Downloads/satnogs_43408_2017-12-10T17-50-11.wav")));
-		System.out.println("estimated: " + ais.getFrameLength());
-		ais.close();
-		WavFileSource source = new WavFileSource(new BufferedInputStream(new FileInputStream("/Users/dernasherbrezon/Downloads/satnogs_43408_2017-12-10T17-50-11.wav")));
-		int actual = 0;
-		while( true ) {
-			try {
-				source.readFloat();
-				actual++;
-			} catch (Exception e) {
-				System.out.println("actual: " + actual);
-				break;
+		try (InputStream is1 = WavFileSourceTest.class.getClassLoader().getResourceAsStream("expectedWaterfall.png"); InputStream is2 = new FileInputStream(actual)) {
+			while (true) {
+				try {
+					int expected = is1.read();
+					if (expected == -1) {
+						break;
+					}
+					assertEquals(expected, is2.read());
+				} catch (EOFException e) {
+					break;
+				}
 			}
 		}
-		source.close();
 	}
+
 }
