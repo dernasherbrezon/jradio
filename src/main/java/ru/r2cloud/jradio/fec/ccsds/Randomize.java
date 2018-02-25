3c80793aa2996be673e830dfd4b40b01ebfd86a3
dernasherbrezon <rodionovamp@mail.ru>
2018-02-25 20:14:41 +0000

    init sequence only once

--- src/main/java/ru/r2cloud/jradio/fec/ccsds/Randomize.java
+++ src/main/java/ru/r2cloud/jradio/fec/ccsds/Randomize.java
@@ -2,14 +2,14 @@ package ru.r2cloud.jradio.fec.ccsds;
 
 public class Randomize {
 
-	public static byte[] shuffle(byte[] data) {
-		int[] sequence = new int[255];
+	private final static int[] sequence = new int[255];
+
+	static {
 		int[] x = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 		int i;
 
 		/*
-		 * The pseudo random sequence shall be generated using the polynomial
-		 * h(x) = x8 + x7 + x5 + x3 + 1
+		 * The pseudo random sequence shall be generated using the polynomial h(x) = x8 + x7 + x5 + x3 + 1
 		 */
 		for (i = 0; i < sequence.length * 8; i++) {
 			sequence[i / 8] = sequence[i / 8] | x[1] << 7 >> (i % 8);
@@ -23,10 +23,12 @@ public class Randomize {
 			x[7] = x[8];
 			x[8] = x[0];
 		}
+	}
 
+	public static byte[] shuffle(byte[] data) {
 		byte[] result = new byte[data.length];
-		for (i = 0; i < data.length; i++) {
-			result[i] = (byte)(data[i] ^ sequence[i % sequence.length]);
+		for (int i = 0; i < data.length; i++) {
+			result[i] = (byte) (data[i] ^ sequence[i % sequence.length]);
 		}
 		return result;
 	}
