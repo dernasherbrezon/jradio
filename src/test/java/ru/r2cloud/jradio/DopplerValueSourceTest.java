05ca04ef4c9e7f4c2a3f13a62e56b65aa347cd8c
dernasherbrezon <rodionovamp@mail.ru>
2018-10-04 23:17:40 +0100

    fix test

--- src/test/java/ru/r2cloud/jradio/DopplerValueSourceTest.java
+++ src/test/java/ru/r2cloud/jradio/DopplerValueSourceTest.java
@@ -8,15 +8,15 @@ public class DopplerValueSourceTest {
 
 	@Test
 	public void test() {
-		DopplerValueSource source = new DopplerValueSource(10, 137, 5, 0L) {
+		DopplerValueSource source = new DopplerValueSource(10, 137, 200, 0L) {
 
 			@Override
 			public long getDopplerFrequency(long satelliteFrequency, long currentTimeMillis) {
 				if (currentTimeMillis == 0) {
 					return 0;
-				} else if (currentTimeMillis == 5) {
+				} else if (currentTimeMillis == 200) {
 					return 1;
-				} else if (currentTimeMillis == 10) {
+				} else if (currentTimeMillis == 400) {
 					return 2;
 				}
 				return 0;
