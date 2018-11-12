20eb73d7ea18331a5852d91727759f523ebed5ac
dernasherbrezon <rodionovamp@mail.ru>
2018-11-12 13:02:55 +0000

    add more complex aausat4 decoding

--- src/test/java/ru/r2cloud/jradio/AAUSAT4Test.java
+++ src/test/java/ru/r2cloud/jradio/AAUSAT4Test.java
@@ -3,6 +3,9 @@ package ru.r2cloud.jradio;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.InputStream;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Test;
 
@@ -12,11 +15,21 @@ import ru.r2cloud.jradio.blocks.ClockRecoveryMM;
 import ru.r2cloud.jradio.blocks.CorrelateAccessCodeTag;
 import ru.r2cloud.jradio.blocks.FixedLengthTagger;
 import ru.r2cloud.jradio.blocks.FloatToChar;
+import ru.r2cloud.jradio.blocks.LowPassFilter;
+import ru.r2cloud.jradio.blocks.Multiply;
+import ru.r2cloud.jradio.blocks.MultiplyConst;
+import ru.r2cloud.jradio.blocks.QuadratureDemodulation;
 import ru.r2cloud.jradio.blocks.Rail;
 import ru.r2cloud.jradio.blocks.TaggedStreamToPdu;
+import ru.r2cloud.jradio.blocks.Window;
 import ru.r2cloud.jradio.csp.Priority;
+import ru.r2cloud.jradio.detection.GmskFrequencyCorrection;
+import ru.r2cloud.jradio.detection.PeakDetection;
+import ru.r2cloud.jradio.detection.PeakInterval;
+import ru.r2cloud.jradio.detection.PeakValueSource;
+import ru.r2cloud.jradio.source.SigSource;
 import ru.r2cloud.jradio.source.WavFileSource;
-import ru.r2cloud.jradio.source.WavFileSourceTest;
+import ru.r2cloud.jradio.source.Waveform;
 import ru.r2cloud.jradio.util.RepeatedWavSource;
 import ru.r2cloud.jradio.util.ThroughputStream;
 
@@ -55,19 +68,37 @@ public class AAUSAT4Test {
 		}
 		input.close();
 		t.interrupt();
-		
+
 		System.out.println("average " + throughputStream.getAverage() + " bytes/s");
 		// average 17697814 bytes/s
 	}
 
 	@Test
+	public void testPeakBasedFrequencyCorrection() throws Exception {
+		List<PeakInterval> peaks;
+		try (WavFileSource source = new WavFileSource(getStream())) {
+			PeakDetection detection = new PeakDetection(100, -80.0f, 3);
+			peaks = detection.process(source);
+		}
+		WavFileSource source = new WavFileSource(getStream());
+		SigSource source2 = new SigSource(Waveform.COMPLEX, (long) source.getContext().getSampleRate(), new PeakValueSource(peaks, new GmskFrequencyCorrection(2400, 10)), 1.0f);
+		Multiply mul = new Multiply(source, source2, true);
+		QuadratureDemodulation qd = new QuadratureDemodulation(mul, 0.4f);
+		LowPassFilter lpf = new LowPassFilter(qd, 1.0, 1500.0f, 100, Window.WIN_HAMMING, 6.76);
+		MultiplyConst mc = new MultiplyConst(lpf, 1.0f);
+		setupDemodulator(mc);
+		assertTrue(input.hasNext());
+
+	}
+
+	private static InputStream getStream() throws Exception {
+		return AAUSAT4Test.class.getClassLoader().getResourceAsStream("aausat-4-with-offset-2.wav");
+	}
+
+	@Test
 	public void testSuccess() throws Exception {
-		WavFileSource source = new WavFileSource(WavFileSourceTest.class.getClassLoader().getResourceAsStream("aausat-4.wav"));
-		ClockRecoveryMM clockRecovery = new ClockRecoveryMM(source, 20.0f, (float) (0.25 * 0.175 * 0.175), 0.005f, 0.175f, 0.005f);
-		Rail rail = new Rail(clockRecovery, -1.0f, 1.0f);
-		FloatToChar f2char = new FloatToChar(rail, 127.0f);
-		CorrelateAccessCodeTag correlateTag = new CorrelateAccessCodeTag(f2char, 10, "010011110101101000110100010000110101010101000010", true);
-		input = new AAUSAT4(new TaggedStreamToPdu(new FixedLengthTagger(correlateTag, AAUSAT4.VITERBI_TAIL_SIZE + 8))); // 8 for fsm
+		WavFileSource source = new WavFileSource(AAUSAT4Test.class.getClassLoader().getResourceAsStream("aausat-4.wav"));
+		setupDemodulator(source);
 		assertTrue(input.hasNext());
 		AAUSAT4Beacon beacon = input.next();
 		assertEquals(4, beacon.getCom().getBootCount());
@@ -91,6 +122,14 @@ public class AAUSAT4Test {
 		assertEquals(Priority.CSP_PRIO_CRITICAL, beacon.getPriority());
 		assertEquals(343, beacon.getAis2().getBootCount());
 		assertEquals(65535, beacon.getAis2().getUniqueMssi());
+	}
+
+	private void setupDemodulator(FloatInput source) {
+		ClockRecoveryMM clockRecovery = new ClockRecoveryMM(source, source.getContext().getSampleRate() / 2400, (float) (0.25 * 0.175 * 0.175), 0.005f, 0.175f, 0.005f);
+		Rail rail = new Rail(clockRecovery, -1.0f, 1.0f);
+		FloatToChar f2char = new FloatToChar(rail, 127.0f);
+		CorrelateAccessCodeTag correlateTag = new CorrelateAccessCodeTag(f2char, 10, "010011110101101000110100010000110101010101000010", true);
+		input = new AAUSAT4(new TaggedStreamToPdu(new FixedLengthTagger(correlateTag, AAUSAT4.VITERBI_TAIL_SIZE + 8))); // 8 for fsm
 	}
 
 	@After
