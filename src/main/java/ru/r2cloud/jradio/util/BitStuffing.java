fdffe3adffefb8918a7e6b92b6c0b917407f5bf2
dernasherbrezon <rodionovamp@mail.ru>
2018-12-27 12:35:04 +0000

    de-stuff bits

---
+++ src/main/java/ru/r2cloud/jradio/util/BitStuffing.java
@@ -0,0 +1,53 @@
+package ru.r2cloud.jradio.util;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class BitStuffing {
+
+	public static byte[] destuffOnes(byte[] data, int run) throws IOException {
+		byte[] result = new byte[data.length];
+		int outputSize = 0;
+		int currentRun = 0;
+		int outputBitPosition = 0;
+		int outputByte = 0;
+		for (int i = 0; i < data.length; i++) {
+			for (int j = 0; j < 8; j++) {
+				int inputBit = ((data[i] & 0xFF) >> (7 - j)) & 0x1;
+				if (currentRun == run) {
+					if (inputBit == 1) {
+						throw new IOException("unexpected long run of ones");
+					}
+					currentRun = 0;
+					continue;
+				}
+
+				if (inputBit == 0) {
+					currentRun = 0;
+				} else {
+					currentRun += 1;
+				}
+
+				if (outputBitPosition > 7) {
+					result[outputSize] = (byte) outputByte;
+					outputBitPosition = 0;
+					outputSize++;
+					outputByte = 0;
+				}
+
+				outputByte = outputByte << 1;
+				outputByte = outputByte | inputBit;
+				outputBitPosition++;
+			}
+		}
+
+		//fill wih zeroes remainder
+		if (outputBitPosition > 0) {
+			outputByte = outputByte << (8 - outputBitPosition);
+			result[outputSize] = (byte) outputByte;
+			outputSize++;
+		}
+		return Arrays.copyOfRange(result, 0, outputSize);
+	}
+
+}
---
+++ src/test/java/ru/r2cloud/jradio/util/BitStuffingTest.java
@@ -0,0 +1,18 @@
+package ru.r2cloud.jradio.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BitStuffingTest {
+
+	@Test
+	public void testDestuff() throws Exception {
+		byte[] data = new byte[] { (byte) 0b1111_1000, 0b0101_1010 };
+		byte[] result = BitStuffing.destuffOnes(data, 5);
+		assertEquals(2, result.length);
+		assertEquals((byte) 0b1111_1000, result[0]);
+		assertEquals((byte) 0b1011_0100, result[1]);
+	}
+
+}
