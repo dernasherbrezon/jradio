68841dc8451e8d3fca90acb734a08976c8b83b79
dernasherbrezon <rodionovamp@mail.ru>
2019-01-17 22:16:37 +0000

    fix test

--- src/test/java/ru/r2cloud/jradio/blocks/MultiplyTest.java
+++ src/test/java/ru/r2cloud/jradio/blocks/MultiplyTest.java
@@ -10,8 +10,8 @@ public class MultiplyTest {
 
 	@Test
 	public void test() throws Exception {
-		Multiply multiply = new Multiply(new ArrayFloatInput(2.0f), new ArrayFloatInput(2.0f));
-		assertEquals(0.0f, multiply.readFloat(), 0.0f);
+		Multiply multiply = new Multiply(new ArrayFloatInput(2.0f, 0.0f), new ArrayFloatInput(2.0f, 0.0f));
+		assertEquals(4.0f, multiply.readFloat(), 0.0f);
 		multiply.close();
 	}
 
