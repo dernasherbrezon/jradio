0a44bc83da292196fa5aa905f82050917a4969b4
dernasherbrezon <rodionovamp@mail.ru>
2019-05-09 09:05:24 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/beesat4/Apid30.java
+++ src/main/java/ru/r2cloud/jradio/beesat4/Apid30.java
@@ -7,421 +7,421 @@ import ru.r2cloud.jradio.util.StreamUtils;
 
 public class Apid30 {
 
-	private float CPDHMCU; // PDH uC
-	private float CPDHCAM; // Camera consumption
+	private float cpdhmcu; // pdh uc
+	private float cpdhcam; // camera consumption
 
-	private int PDHCECCM; // Err Ctr CAN bus
-	private int PDHCECTC; // Err Ctr invalid TC
-	private int PDHCETLII; // Err Ctr loading Image Chunks
-	private int PDHCETLC; // Err Ctr loading Image Catalog
-	private int PDHCTI; // Ctr incoming Telemetry TC
-	private int PDHCTH; // Ctr Telemetry TC
-	private int PDHCEISI; // Err Ctr saving images
-	private int PDHCEICI; // Err Ctr image capture
-	private int PDHCEIDS; // Err Ctr delete slot
-	private PdhMode PDHMODE; // Current workmode
-	private int PDHCII; // Ctr incoming Image TC
-	private int PDHCIH; // Ctr Image TC
-	private int PDHCESCM; // Err Ctr CRC messages
-	private int PDHCESSM; // Err Ctr saving messages
-	private int PDHCESCB; // Err Ctr CRC blocks
-	private int PDHCESLB; // Err Ctr loading blocks
-	private int PDHCESSS; // Err Ctr saving in scratch pad
-	private int PDHCESSB; // Err Ctr saving blocks
-	private int PDHCESEB; // Err Ctr erasing blocks
-	private int PDHCESCI; // Err Ctr checking image
-	private int PDHCSI; // Ctr incoming SW Upload TC
-	private int PDHCSH; // Ctr SW Upload TC
-	private int PDHBTIMG; // PDH boot slot
-	private int PDHSWVERSION; // PDH Software Revision
-	private long PDHCSTSYS; // PDH Uptime
-	private long PDHCTSTUTC; // PDH Time UTC
-	private int GPSCTI; // Ctr incoming GPS TC
-	private int GPSCTH; // Ctr GPS TC
-	private int GPSECH; // Err Ctr GPS Checksum
-	private int GPSECM; // Err Ctr GPS Commands
-	private int GPSEWS; // Err Ctr GPS Warm-Start
-	private int GPSECS; // Err Ctr GPS Cold-Start
-	private int GPSFCM; // Last Failed Command
-	private int GPSEAL; // Err Ctr delete Almanac
-	private int GPSETL; // Err Ctr delete TLE
-	private int GPSESA; // Err Ctr save Almanac
-	private int GPSF13; // Err Ctr save F13
-	private int GPSSLT; // Current GPS Storage Slot
-	private int GPSESD; // Err Ctr save Almanac Header
+	private int pdhceccm; // err ctr can bus
+	private int pdhcectc; // err ctr invalid tc
+	private int pdhcetlii; // err ctr loading image chunks
+	private int pdhcetlc; // err ctr loading image catalog
+	private int pdhcti; // ctr incoming telemetry tc
+	private int pdhcth; // ctr telemetry tc
+	private int pdhceisi; // err ctr saving images
+	private int pdhceici; // err ctr image capture
+	private int pdhceids; // err ctr delete slot
+	private PdhMode pdhmode; // Current workmode
+	private int pdhcii; // ctr incoming image tc
+	private int pdhcih; // ctr image tc
+	private int pdhcescm; // err ctr crc messages
+	private int pdhcessm; // err ctr saving messages
+	private int pdhcescb; // err ctr crc blocks
+	private int pdhceslb; // err ctr loading blocks
+	private int pdhcesss; // err ctr saving in scratch pad
+	private int pdhcessb; // err ctr saving blocks
+	private int pdhceseb; // err ctr erasing blocks
+	private int pdhcesci; // err ctr checking image
+	private int pdhcsi; // ctr incoming sw upload tc
+	private int pdhcsh; // ctr sw upload tc
+	private int pdhbtimg; // pdh boot slot
+	private int pdhswversion; // pdh software revision
+	private long pdhcstsys; // pdh uptime
+	private long pdhctstutc; // pdh time utc
+	private int gpscti; // ctr incoming gps tc
+	private int gpscth; // ctr gps tc
+	private int gpsech; // err ctr gps checksum
+	private int gpsecm; // err ctr gps commands
+	private int gpsews; // err ctr gps warm-start
+	private int gpsecs; // err ctr gps cold-start
+	private int gpsfcm; // last failed command
+	private int gpseal; // err ctr delete almanac
+	private int gpsetl; // err ctr delete tle
+	private int gpsesa; // err ctr save almanac
+	private int gpsf13; // err ctr save f13
+	private int gpsslt; // current gps storage slot
+	private int gpsesd; // err ctr save almanac header
 
 	public Apid30(DataInputStream dis) throws IOException {
-		CPDHMCU = dis.readUnsignedShort() * 0.04029304f;
-		CPDHCAM = dis.readUnsignedShort() * 0.201465201f;
+		cpdhmcu = dis.readUnsignedShort() * 0.04029304f;
+		cpdhcam = dis.readUnsignedShort() * 0.201465201f;
 		dis.skipBytes(2);
-		PDHCECCM = dis.readUnsignedByte();
-		PDHCECTC = dis.readUnsignedByte();
-		PDHCETLII = dis.readUnsignedByte();
-		PDHCETLC = dis.readUnsignedByte();
-		PDHCTI = dis.readUnsignedByte();
-		PDHCTH = dis.readUnsignedByte();
-		PDHCEISI = dis.readUnsignedByte();
-		PDHCEICI = dis.readUnsignedByte();
-		PDHCEIDS = dis.readUnsignedByte();
-		PDHMODE = PdhMode.valueOfCode(dis.readUnsignedByte());
-		PDHCII = dis.readUnsignedByte();
-		PDHCIH = dis.readUnsignedByte();
-		PDHCESCM = dis.readUnsignedByte();
-		PDHCESSM = dis.readUnsignedByte();
-		PDHCESCB = dis.readUnsignedByte();
-		PDHCESLB = dis.readUnsignedByte();
-		PDHCESSS = dis.readUnsignedByte();
-		PDHCESSB = dis.readUnsignedByte();
-		PDHCESEB = dis.readUnsignedByte();
-		PDHCESCI = dis.readUnsignedByte();
-		PDHCSI = dis.readUnsignedByte();
-		PDHCSH = dis.readUnsignedByte();
-		PDHBTIMG = dis.readUnsignedByte();
-		PDHSWVERSION = dis.readUnsignedShort();
-		PDHCSTSYS = StreamUtils.readUnsignedInt(dis);
-		PDHCTSTUTC = StreamUtils.readUnsignedInt(dis);
-		GPSCTI = dis.readUnsignedByte();
-		GPSCTH = dis.readUnsignedByte();
-		GPSECH = dis.readUnsignedByte();
-		GPSECM = dis.readUnsignedByte();
-		GPSEWS = dis.readUnsignedByte();
-		GPSECS = dis.readUnsignedByte();
-		GPSFCM = dis.readUnsignedShort();
-		GPSEAL = dis.readUnsignedByte();
-		GPSETL = dis.readUnsignedByte();
-		GPSESA = dis.readUnsignedByte();
-		GPSF13 = dis.readUnsignedByte();
-		GPSSLT = dis.readUnsignedByte();
-		GPSESD = dis.readUnsignedByte();
+		pdhceccm = dis.readUnsignedByte();
+		pdhcectc = dis.readUnsignedByte();
+		pdhcetlii = dis.readUnsignedByte();
+		pdhcetlc = dis.readUnsignedByte();
+		pdhcti = dis.readUnsignedByte();
+		pdhcth = dis.readUnsignedByte();
+		pdhceisi = dis.readUnsignedByte();
+		pdhceici = dis.readUnsignedByte();
+		pdhceids = dis.readUnsignedByte();
+		pdhmode = PdhMode.valueOfCode(dis.readUnsignedByte());
+		pdhcii = dis.readUnsignedByte();
+		pdhcih = dis.readUnsignedByte();
+		pdhcescm = dis.readUnsignedByte();
+		pdhcessm = dis.readUnsignedByte();
+		pdhcescb = dis.readUnsignedByte();
+		pdhceslb = dis.readUnsignedByte();
+		pdhcesss = dis.readUnsignedByte();
+		pdhcessb = dis.readUnsignedByte();
+		pdhceseb = dis.readUnsignedByte();
+		pdhcesci = dis.readUnsignedByte();
+		pdhcsi = dis.readUnsignedByte();
+		pdhcsh = dis.readUnsignedByte();
+		pdhbtimg = dis.readUnsignedByte();
+		pdhswversion = dis.readUnsignedShort();
+		pdhcstsys = StreamUtils.readUnsignedInt(dis);
+		pdhctstutc = StreamUtils.readUnsignedInt(dis);
+		gpscti = dis.readUnsignedByte();
+		gpscth = dis.readUnsignedByte();
+		gpsech = dis.readUnsignedByte();
+		gpsecm = dis.readUnsignedByte();
+		gpsews = dis.readUnsignedByte();
+		gpsecs = dis.readUnsignedByte();
+		gpsfcm = dis.readUnsignedShort();
+		gpseal = dis.readUnsignedByte();
+		gpsetl = dis.readUnsignedByte();
+		gpsesa = dis.readUnsignedByte();
+		gpsf13 = dis.readUnsignedByte();
+		gpsslt = dis.readUnsignedByte();
+		gpsesd = dis.readUnsignedByte();
 		dis.skipBytes(73);
 	}
 
-	public float getCPDHMCU() {
-		return CPDHMCU;
+	public float getCpdhmcu() {
+		return cpdhmcu;
 	}
 
-	public void setCPDHMCU(float cPDHMCU) {
-		CPDHMCU = cPDHMCU;
+	public void setCpdhmcu(float cpdhmcu) {
+		this.cpdhmcu = cpdhmcu;
 	}
 
-	public float getCPDHCAM() {
-		return CPDHCAM;
+	public float getCpdhcam() {
+		return cpdhcam;
 	}
 
-	public void setCPDHCAM(float cPDHCAM) {
-		CPDHCAM = cPDHCAM;
+	public void setCpdhcam(float cpdhcam) {
+		this.cpdhcam = cpdhcam;
 	}
 
-	public int getPDHCECCM() {
-		return PDHCECCM;
+	public int getPdhceccm() {
+		return pdhceccm;
 	}
 
-	public void setPDHCECCM(int pDHCECCM) {
-		PDHCECCM = pDHCECCM;
+	public void setPdhceccm(int pdhceccm) {
+		this.pdhceccm = pdhceccm;
 	}
 
-	public int getPDHCECTC() {
-		return PDHCECTC;
+	public int getPdhcectc() {
+		return pdhcectc;
 	}
 
-	public void setPDHCECTC(int pDHCECTC) {
-		PDHCECTC = pDHCECTC;
+	public void setPdhcectc(int pdhcectc) {
+		this.pdhcectc = pdhcectc;
 	}
 
-	public int getPDHCETLII() {
-		return PDHCETLII;
+	public int getPdhcetlii() {
+		return pdhcetlii;
 	}
 
-	public void setPDHCETLII(int pDHCETLII) {
-		PDHCETLII = pDHCETLII;
+	public void setPdhcetlii(int pdhcetlii) {
+		this.pdhcetlii = pdhcetlii;
 	}
 
-	public int getPDHCETLC() {
-		return PDHCETLC;
+	public int getPdhcetlc() {
+		return pdhcetlc;
 	}
 
-	public void setPDHCETLC(int pDHCETLC) {
-		PDHCETLC = pDHCETLC;
+	public void setPdhcetlc(int pdhcetlc) {
+		this.pdhcetlc = pdhcetlc;
 	}
 
-	public int getPDHCTI() {
-		return PDHCTI;
+	public int getPdhcti() {
+		return pdhcti;
 	}
 
-	public void setPDHCTI(int pDHCTI) {
-		PDHCTI = pDHCTI;
+	public void setPdhcti(int pdhcti) {
+		this.pdhcti = pdhcti;
 	}
 
-	public int getPDHCTH() {
-		return PDHCTH;
+	public int getPdhcth() {
+		return pdhcth;
 	}
 
-	public void setPDHCTH(int pDHCTH) {
-		PDHCTH = pDHCTH;
+	public void setPdhcth(int pdhcth) {
+		this.pdhcth = pdhcth;
 	}
 
-	public int getPDHCEISI() {
-		return PDHCEISI;
+	public int getPdhceisi() {
+		return pdhceisi;
 	}
 
-	public void setPDHCEISI(int pDHCEISI) {
-		PDHCEISI = pDHCEISI;
+	public void setPdhceisi(int pdhceisi) {
+		this.pdhceisi = pdhceisi;
 	}
 
-	public int getPDHCEICI() {
-		return PDHCEICI;
+	public int getPdhceici() {
+		return pdhceici;
 	}
 
-	public void setPDHCEICI(int pDHCEICI) {
-		PDHCEICI = pDHCEICI;
+	public void setPdhceici(int pdhceici) {
+		this.pdhceici = pdhceici;
 	}
 
-	public int getPDHCEIDS() {
-		return PDHCEIDS;
+	public int getPdhceids() {
+		return pdhceids;
 	}
 
-	public void setPDHCEIDS(int pDHCEIDS) {
-		PDHCEIDS = pDHCEIDS;
+	public void setPdhceids(int pdhceids) {
+		this.pdhceids = pdhceids;
 	}
 
-	public PdhMode getPDHMODE() {
-		return PDHMODE;
+	public PdhMode getPdhmode() {
+		return pdhmode;
 	}
 
-	public void setPDHMODE(PdhMode pDHMODE) {
-		PDHMODE = pDHMODE;
+	public void setPdhmode(PdhMode pdhmode) {
+		this.pdhmode = pdhmode;
 	}
 
-	public int getPDHCII() {
-		return PDHCII;
+	public int getPdhcii() {
+		return pdhcii;
 	}
 
-	public void setPDHCII(int pDHCII) {
-		PDHCII = pDHCII;
+	public void setPdhcii(int pdhcii) {
+		this.pdhcii = pdhcii;
 	}
 
-	public int getPDHCIH() {
-		return PDHCIH;
+	public int getPdhcih() {
+		return pdhcih;
 	}
 
-	public void setPDHCIH(int pDHCIH) {
-		PDHCIH = pDHCIH;
+	public void setPdhcih(int pdhcih) {
+		this.pdhcih = pdhcih;
 	}
 
-	public int getPDHCESCM() {
-		return PDHCESCM;
+	public int getPdhcescm() {
+		return pdhcescm;
 	}
 
-	public void setPDHCESCM(int pDHCESCM) {
-		PDHCESCM = pDHCESCM;
+	public void setPdhcescm(int pdhcescm) {
+		this.pdhcescm = pdhcescm;
 	}
 
-	public int getPDHCESSM() {
-		return PDHCESSM;
+	public int getPdhcessm() {
+		return pdhcessm;
 	}
 
-	public void setPDHCESSM(int pDHCESSM) {
-		PDHCESSM = pDHCESSM;
+	public void setPdhcessm(int pdhcessm) {
+		this.pdhcessm = pdhcessm;
 	}
 
-	public int getPDHCESCB() {
-		return PDHCESCB;
+	public int getPdhcescb() {
+		return pdhcescb;
 	}
 
-	public void setPDHCESCB(int pDHCESCB) {
-		PDHCESCB = pDHCESCB;
+	public void setPdhcescb(int pdhcescb) {
+		this.pdhcescb = pdhcescb;
 	}
 
-	public int getPDHCESLB() {
-		return PDHCESLB;
+	public int getPdhceslb() {
+		return pdhceslb;
 	}
 
-	public void setPDHCESLB(int pDHCESLB) {
-		PDHCESLB = pDHCESLB;
+	public void setPdhceslb(int pdhceslb) {
+		this.pdhceslb = pdhceslb;
 	}
 
-	public int getPDHCESSS() {
-		return PDHCESSS;
+	public int getPdhcesss() {
+		return pdhcesss;
 	}
 
-	public void setPDHCESSS(int pDHCESSS) {
-		PDHCESSS = pDHCESSS;
+	public void setPdhcesss(int pdhcesss) {
+		this.pdhcesss = pdhcesss;
 	}
 
-	public int getPDHCESSB() {
-		return PDHCESSB;
+	public int getPdhcessb() {
+		return pdhcessb;
 	}
 
-	public void setPDHCESSB(int pDHCESSB) {
-		PDHCESSB = pDHCESSB;
+	public void setPdhcessb(int pdhcessb) {
+		this.pdhcessb = pdhcessb;
 	}
 
-	public int getPDHCESEB() {
-		return PDHCESEB;
+	public int getPdhceseb() {
+		return pdhceseb;
 	}
 
-	public void setPDHCESEB(int pDHCESEB) {
-		PDHCESEB = pDHCESEB;
+	public void setPdhceseb(int pdhceseb) {
+		this.pdhceseb = pdhceseb;
 	}
 
-	public int getPDHCESCI() {
-		return PDHCESCI;
+	public int getPdhcesci() {
+		return pdhcesci;
 	}
 
-	public void setPDHCESCI(int pDHCESCI) {
-		PDHCESCI = pDHCESCI;
+	public void setPdhcesci(int pdhcesci) {
+		this.pdhcesci = pdhcesci;
 	}
 
-	public int getPDHCSI() {
-		return PDHCSI;
+	public int getPdhcsi() {
+		return pdhcsi;
 	}
 
-	public void setPDHCSI(int pDHCSI) {
-		PDHCSI = pDHCSI;
+	public void setPdhcsi(int pdhcsi) {
+		this.pdhcsi = pdhcsi;
 	}
 
-	public int getPDHCSH() {
-		return PDHCSH;
+	public int getPdhcsh() {
+		return pdhcsh;
 	}
 
-	public void setPDHCSH(int pDHCSH) {
-		PDHCSH = pDHCSH;
+	public void setPdhcsh(int pdhcsh) {
+		this.pdhcsh = pdhcsh;
 	}
 
-	public int getPDHBTIMG() {
-		return PDHBTIMG;
+	public int getPdhbtimg() {
+		return pdhbtimg;
 	}
 
-	public void setPDHBTIMG(int pDHBTIMG) {
-		PDHBTIMG = pDHBTIMG;
+	public void setPdhbtimg(int pdhbtimg) {
+		this.pdhbtimg = pdhbtimg;
 	}
 
-	public int getPDHSWVERSION() {
-		return PDHSWVERSION;
+	public int getPdhswversion() {
+		return pdhswversion;
 	}
 
-	public void setPDHSWVERSION(int pDHSWVERSION) {
-		PDHSWVERSION = pDHSWVERSION;
+	public void setPdhswversion(int pdhswversion) {
+		this.pdhswversion = pdhswversion;
 	}
 
-	public long getPDHCSTSYS() {
-		return PDHCSTSYS;
+	public long getPdhcstsys() {
+		return pdhcstsys;
 	}
 
-	public void setPDHCSTSYS(long pDHCSTSYS) {
-		PDHCSTSYS = pDHCSTSYS;
+	public void setPdhcstsys(long pdhcstsys) {
+		this.pdhcstsys = pdhcstsys;
 	}
 
-	public long getPDHCTSTUTC() {
-		return PDHCTSTUTC;
+	public long getPdhctstutc() {
+		return pdhctstutc;
 	}
 
-	public void setPDHCTSTUTC(long pDHCTSTUTC) {
-		PDHCTSTUTC = pDHCTSTUTC;
+	public void setPdhctstutc(long pdhctstutc) {
+		this.pdhctstutc = pdhctstutc;
 	}
 
-	public int getGPSCTI() {
-		return GPSCTI;
+	public int getGpscti() {
+		return gpscti;
 	}
 
-	public void setGPSCTI(int gPSCTI) {
-		GPSCTI = gPSCTI;
+	public void setGpscti(int gpscti) {
+		this.gpscti = gpscti;
 	}
 
-	public int getGPSCTH() {
-		return GPSCTH;
+	public int getGpscth() {
+		return gpscth;
 	}
 
-	public void setGPSCTH(int gPSCTH) {
-		GPSCTH = gPSCTH;
+	public void setGpscth(int gpscth) {
+		this.gpscth = gpscth;
 	}
 
-	public int getGPSECH() {
-		return GPSECH;
+	public int getGpsech() {
+		return gpsech;
 	}
 
-	public void setGPSECH(int gPSECH) {
-		GPSECH = gPSECH;
+	public void setGpsech(int gpsech) {
+		this.gpsech = gpsech;
 	}
 
-	public int getGPSECM() {
-		return GPSECM;
+	public int getGpsecm() {
+		return gpsecm;
 	}
 
-	public void setGPSECM(int gPSECM) {
-		GPSECM = gPSECM;
+	public void setGpsecm(int gpsecm) {
+		this.gpsecm = gpsecm;
 	}
 
-	public int getGPSEWS() {
-		return GPSEWS;
+	public int getGpsews() {
+		return gpsews;
 	}
 
-	public void setGPSEWS(int gPSEWS) {
-		GPSEWS = gPSEWS;
+	public void setGpsews(int gpsews) {
+		this.gpsews = gpsews;
 	}
 
-	public int getGPSECS() {
-		return GPSECS;
+	public int getGpsecs() {
+		return gpsecs;
 	}
 
-	public void setGPSECS(int gPSECS) {
-		GPSECS = gPSECS;
+	public void setGpsecs(int gpsecs) {
+		this.gpsecs = gpsecs;
 	}
 
-	public int getGPSFCM() {
-		return GPSFCM;
+	public int getGpsfcm() {
+		return gpsfcm;
 	}
 
-	public void setGPSFCM(int gPSFCM) {
-		GPSFCM = gPSFCM;
+	public void setGpsfcm(int gpsfcm) {
+		this.gpsfcm = gpsfcm;
 	}
 
-	public int getGPSEAL() {
-		return GPSEAL;
+	public int getGpseal() {
+		return gpseal;
 	}
 
-	public void setGPSEAL(int gPSEAL) {
-		GPSEAL = gPSEAL;
+	public void setGpseal(int gpseal) {
+		this.gpseal = gpseal;
 	}
 
-	public int getGPSETL() {
-		return GPSETL;
+	public int getGpsetl() {
+		return gpsetl;
 	}
 
-	public void setGPSETL(int gPSETL) {
-		GPSETL = gPSETL;
+	public void setGpsetl(int gpsetl) {
+		this.gpsetl = gpsetl;
 	}
 
-	public int getGPSESA() {
-		return GPSESA;
+	public int getGpsesa() {
+		return gpsesa;
 	}
 
-	public void setGPSESA(int gPSESA) {
-		GPSESA = gPSESA;
+	public void setGpsesa(int gpsesa) {
+		this.gpsesa = gpsesa;
 	}
 
-	public int getGPSF13() {
-		return GPSF13;
+	public int getGpsf13() {
+		return gpsf13;
 	}
 
-	public void setGPSF13(int gPSF13) {
-		GPSF13 = gPSF13;
+	public void setGpsf13(int gpsf13) {
+		this.gpsf13 = gpsf13;
 	}
 
-	public int getGPSSLT() {
-		return GPSSLT;
+	public int getGpsslt() {
+		return gpsslt;
 	}
 
-	public void setGPSSLT(int gPSSLT) {
-		GPSSLT = gPSSLT;
+	public void setGpsslt(int gpsslt) {
+		this.gpsslt = gpsslt;
 	}
 
-	public int getGPSESD() {
-		return GPSESD;
+	public int getGpsesd() {
+		return gpsesd;
 	}
 
-	public void setGPSESD(int gPSESD) {
-		GPSESD = gPSESD;
+	public void setGpsesd(int gpsesd) {
+		this.gpsesd = gpsesd;
 	}
 
 }
