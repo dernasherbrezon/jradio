a79474bc6fad29485685d8e0953448b716b2856b
dernasherbrezon <rodionovamp@mail.ru>
2019-05-11 20:45:59 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/beesat4/Apid32.java
+++ src/main/java/ru/r2cloud/jradio/beesat4/Apid32.java
@@ -7,112 +7,113 @@ import ru.r2cloud.jradio.util.StreamUtils;
 
 public class Apid32 {
 
-	private int[] ICID;                //  Image ID Slot X
-	private long[] ICDATETIME;         //  Image capture datetime Slot X
-	private int[] ICSIZE;              //  Image size in chunks Slot X
-	private boolean[] ICTXDONE;        //  JPEG transfer done Slot X
-	private boolean[] ICOF;            //  JPEG fifo overflow Slot X
-	private boolean[] ICWBSTEADY;      //  Auto whitebalance steady Slot X
-	private boolean[] ICAWBLIMIT;      //  Auto whitebalance limits reached Slot X
-	private boolean[] ICAELIMIT;       //  Auto exposure limit Slot X
-	private int[] ICTRIES;             //  Capture tries Slot X
+	private int[] icid;                //  image id slot x
+	private long[] icdatetime;         //  image capture datetime Slot X
+	private int[] icsize;              //  image size in chunks slot X
+	private boolean[] ictxdone;        //  jpeg transfer done slot X
+	private boolean[] icof;            //  jpeg fifo overflow slot X
+	private boolean[] icwbsteady;      //  auto whitebalance steady Slot X
+	private boolean[] icawblimit;      //  auto whitebalance limits reached Slot X
+	private boolean[] icaelimit;       //  auto exposure limit slot X
+	private int[] ictries;             //  capture tries slot x
 
 	public Apid32(DataInputStream dis) throws IOException {
 		int size = 15;
-		ICID = new int[size];
-		ICDATETIME = new long[size];
-		ICSIZE = new int[size];
-		ICTXDONE = new boolean[size];
-		ICOF = new boolean[size];
-		ICWBSTEADY = new boolean[size];
-		ICAWBLIMIT = new boolean[size];
-		ICAELIMIT = new boolean[size];
-		ICTRIES = new int[size];
+		icid = new int[size];
+		icdatetime = new long[size];
+		icsize = new int[size];
+		ictxdone = new boolean[size];
+		icof = new boolean[size];
+		icwbsteady = new boolean[size];
+		icawblimit = new boolean[size];
+		icaelimit = new boolean[size];
+		ictries = new int[size];
 		for (int i = 0; i < size; i++) {
-			ICID[i] = dis.readUnsignedByte();
-			ICDATETIME[i] = StreamUtils.readUnsignedInt(dis);
-			ICSIZE[i] = dis.readUnsignedShort();
+			icid[i] = dis.readUnsignedByte();
+			icdatetime[i] = StreamUtils.readUnsignedInt(dis);
+			icsize[i] = dis.readUnsignedShort();
 			int raw = dis.readUnsignedByte();
-			ICTXDONE[i] = ((raw >> 7) & 0x1) > 0;
-			ICOF[i] = ((raw >> 6) & 0x1) > 0;
-			ICWBSTEADY[i] = ((raw >> 5) & 0x1) > 0;
-			ICAWBLIMIT[i] = ((raw >> 4) & 0x1) > 0;
-			ICAELIMIT[i] = ((raw >> 3) & 0x1) > 0;
-			ICTRIES[i] = (raw & 0b111);
+			ictxdone[i] = ((raw >> 7) & 0x1) > 0;
+			icof[i] = ((raw >> 6) & 0x1) > 0;
+			icwbsteady[i] = ((raw >> 5) & 0x1) > 0;
+			icawblimit[i] = ((raw >> 4) & 0x1) > 0;
+			icaelimit[i] = ((raw >> 3) & 0x1) > 0;
+			ictries[i] = (raw & 0b111);
 		}
 		dis.skipBytes(6);
 	}
 
-	public int[] getICID() {
-		return ICID;
+	public int[] getIcid() {
+		return icid;
 	}
 
-	public void setICID(int[] iCID) {
-		ICID = iCID;
+	public void setIcid(int[] icid) {
+		this.icid = icid;
 	}
 
-	public long[] getICDATETIME() {
-		return ICDATETIME;
+	public long[] getIcdatetime() {
+		return icdatetime;
 	}
 
-	public void setICDATETIME(long[] iCDATETIME) {
-		ICDATETIME = iCDATETIME;
+	public void setIcdatetime(long[] icdatetime) {
+		this.icdatetime = icdatetime;
 	}
 
-	public int[] getICSIZE() {
-		return ICSIZE;
+	public int[] getIcsize() {
+		return icsize;
 	}
 
-	public void setICSIZE(int[] iCSIZE) {
-		ICSIZE = iCSIZE;
+	public void setIcsize(int[] icsize) {
+		this.icsize = icsize;
 	}
 
-	public boolean[] getICTXDONE() {
-		return ICTXDONE;
+	public boolean[] getIctxdone() {
+		return ictxdone;
 	}
 
-	public void setICTXDONE(boolean[] iCTXDONE) {
-		ICTXDONE = iCTXDONE;
+	public void setIctxdone(boolean[] ictxdone) {
+		this.ictxdone = ictxdone;
 	}
 
-	public boolean[] getICOF() {
-		return ICOF;
+	public boolean[] getIcof() {
+		return icof;
 	}
 
-	public void setICOF(boolean[] iCOF) {
-		ICOF = iCOF;
+	public void setIcof(boolean[] icof) {
+		this.icof = icof;
 	}
 
-	public boolean[] getICWBSTEADY() {
-		return ICWBSTEADY;
+	public boolean[] getIcwbsteady() {
+		return icwbsteady;
 	}
 
-	public void setICWBSTEADY(boolean[] iCWBSTEADY) {
-		ICWBSTEADY = iCWBSTEADY;
+	public void setIcwbsteady(boolean[] icwbsteady) {
+		this.icwbsteady = icwbsteady;
 	}
 
-	public boolean[] getICAWBLIMIT() {
-		return ICAWBLIMIT;
+	public boolean[] getIcawblimit() {
+		return icawblimit;
 	}
 
-	public void setICAWBLIMIT(boolean[] iCAWBLIMIT) {
-		ICAWBLIMIT = iCAWBLIMIT;
+	public void setIcawblimit(boolean[] icawblimit) {
+		this.icawblimit = icawblimit;
 	}
 
-	public boolean[] getICAELIMIT() {
-		return ICAELIMIT;
+	public boolean[] getIcaelimit() {
+		return icaelimit;
 	}
 
-	public void setICAELIMIT(boolean[] iCAELIMIT) {
-		ICAELIMIT = iCAELIMIT;
+	public void setIcaelimit(boolean[] icaelimit) {
+		this.icaelimit = icaelimit;
 	}
 
-	public int[] getICTRIES() {
-		return ICTRIES;
+	public int[] getIctries() {
+		return ictries;
 	}
 
-	public void setICTRIES(int[] iCTRIES) {
-		ICTRIES = iCTRIES;
+	public void setIctries(int[] ictries) {
+		this.ictries = ictries;
 	}
 
+	
 }
