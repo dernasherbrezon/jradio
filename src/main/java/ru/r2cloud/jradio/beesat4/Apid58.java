07256898b631502aaabfb13e8e9d1d14ba553471
dernasherbrezon <rodionovamp@mail.ru>
2019-05-19 09:28:38 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/beesat4/Apid58.java
+++ src/main/java/ru/r2cloud/jradio/beesat4/Apid58.java
@@ -5,279 +5,279 @@ import java.io.IOException;
 
 public class Apid58 {
 
-	private float MAG1TY;     // MFS #1 Incline Temp Y
-	private float MAG1TZ;     // MFS #1 Incline Temp Z
-	private float MAG2TX;     // MFS #2 Incline Temp X
-	private float MAG2TY;     // MFS #2 Incline Temp Y
-	private float MAG2TZ;     // MFS #2 Incline Temp Z
-	private float GYRMSX;     // Gyro Max Scale X
-	private float GYRMSY;     // Gyro Max Scale Y
-	private float GYRMSZ;     // Gyro Max Scale Z
-	private float GYRMOX;     // Gyro Max Offset X
-	private float GYRMOY;     // Gyro Max Offset Y
-	private float GYRMOZ;     // Gyro Max Offset Z
-	private float GYRMTX;     // Gyro Max Incline Temp X
-	private float GYRMTY;     // Gyro Max Incline Temp Y
-	private float GYRMTZ;     // Gyro Max Incline Temp Z
-	private float GYRMST;     // Gyro Max Scale Temperature
-	private float GYRMOT;     // Gyro Max Offset Temperature
-	private float GYRLSX;     // Gyro L3G Scale X
-	private float GYRLSY;     // Gyro L3G Scale Y
-	private float GYRLSZ;     // Gyro L3G Scale Z
-	private float GYRLOX;     // Gyro L3G Offset X
-	private float GYRLOY;     // Gyro L3G Offset Y
-	private float GYRLOZ;     // Gyro L3G Offset Z
-	private float GYRLTX;     // Gyro L3G Incline Temp X
-	private float GYRLTY;     // Gyro L3G Incline Temp Y
-	private float GYRLTZ;     // Gyro L3G Incline Temp Z
-	private float GYRLST;     // Gyro L3G Scale Temperature
-	private float GYRLOT;     // Gyro L3G Offset Temperature
+	private float mag1ty; // MFS #1 Incline Temp Y
+	private float mag1tz; // MFS #1 Incline Temp Z
+	private float mag2tx; // MFS #2 Incline Temp X
+	private float mag2ty; // MFS #2 Incline Temp Y
+	private float mag2tz; // MFS #2 Incline Temp Z
+	private float gyrmsx; // Gyro Max Scale X
+	private float gyrmsy; // Gyro Max Scale Y
+	private float gyrmsz; // Gyro Max Scale Z
+	private float gyrmox; // Gyro Max Offset X
+	private float gyrmoy; // Gyro Max Offset Y
+	private float gyrmoz; // Gyro Max Offset Z
+	private float gyrmtx; // Gyro Max Incline Temp X
+	private float gyrmty; // Gyro Max Incline Temp Y
+	private float gyrmtz; // Gyro Max Incline Temp Z
+	private float gyrmst; // Gyro Max Scale Temperature
+	private float gyrmot; // Gyro Max Offset Temperature
+	private float gyrlsx; // Gyro L3G Scale X
+	private float gyrlsy; // Gyro L3G Scale Y
+	private float gyrlsz; // Gyro L3G Scale Z
+	private float gyrlox; // Gyro L3G Offset X
+	private float gyrloy; // Gyro L3G Offset Y
+	private float gyrloz; // Gyro L3G Offset Z
+	private float gyrltx; // Gyro L3G Incline Temp X
+	private float gyrlty; // Gyro L3G Incline Temp Y
+	private float gyrltz; // Gyro L3G Incline Temp Z
+	private float gyrlst; // Gyro L3G Scale Temperature
+	private float gyrlot; // Gyro L3G Offset Temperature
 
 	public Apid58(DataInputStream dis) throws IOException {
-		MAG1TY = dis.readFloat();
-		MAG1TZ = dis.readFloat();
-		MAG2TX = dis.readFloat();
-		MAG2TY = dis.readFloat();
-		MAG2TZ = dis.readFloat();
-		GYRMSX = dis.readFloat();
-		GYRMSY = dis.readFloat();
-		GYRMSZ = dis.readFloat();
-		GYRMOX = dis.readFloat();
-		GYRMOY = dis.readFloat();
-		GYRMOZ = dis.readFloat();
-		GYRMTX = dis.readFloat();
-		GYRMTY = dis.readFloat();
-		GYRMTZ = dis.readFloat();
-		GYRMST = dis.readFloat();
-		GYRMOT = dis.readFloat();
-		GYRLSX = dis.readFloat();
-		GYRLSY = dis.readFloat();
-		GYRLSZ = dis.readFloat();
-		GYRLOX = dis.readFloat();
-		GYRLOY = dis.readFloat();
-		GYRLOZ = dis.readFloat();
-		GYRLTX = dis.readFloat();
-		GYRLTY = dis.readFloat();
-		GYRLTZ = dis.readFloat();
-		GYRLST = dis.readFloat();
-		GYRLOT = dis.readFloat();
+		mag1ty = dis.readFloat();
+		mag1tz = dis.readFloat();
+		mag2tx = dis.readFloat();
+		mag2ty = dis.readFloat();
+		mag2tz = dis.readFloat();
+		gyrmsx = dis.readFloat();
+		gyrmsy = dis.readFloat();
+		gyrmsz = dis.readFloat();
+		gyrmox = dis.readFloat();
+		gyrmoy = dis.readFloat();
+		gyrmoz = dis.readFloat();
+		gyrmtx = dis.readFloat();
+		gyrmty = dis.readFloat();
+		gyrmtz = dis.readFloat();
+		gyrmst = dis.readFloat();
+		gyrmot = dis.readFloat();
+		gyrlsx = dis.readFloat();
+		gyrlsy = dis.readFloat();
+		gyrlsz = dis.readFloat();
+		gyrlox = dis.readFloat();
+		gyrloy = dis.readFloat();
+		gyrloz = dis.readFloat();
+		gyrltx = dis.readFloat();
+		gyrlty = dis.readFloat();
+		gyrltz = dis.readFloat();
+		gyrlst = dis.readFloat();
+		gyrlot = dis.readFloat();
 		dis.skipBytes(18);
 	}
 
-	public float getMAG1TY() {
-		return MAG1TY;
+	public float getMag1ty() {
+		return mag1ty;
 	}
 
-	public void setMAG1TY(float mAG1TY) {
-		MAG1TY = mAG1TY;
+	public void setMag1ty(float mag1ty) {
+		this.mag1ty = mag1ty;
 	}
 
-	public float getMAG1TZ() {
-		return MAG1TZ;
+	public float getMag1tz() {
+		return mag1tz;
 	}
 
-	public void setMAG1TZ(float mAG1TZ) {
-		MAG1TZ = mAG1TZ;
+	public void setMag1tz(float mag1tz) {
+		this.mag1tz = mag1tz;
 	}
 
-	public float getMAG2TX() {
-		return MAG2TX;
+	public float getMag2tx() {
+		return mag2tx;
 	}
 
-	public void setMAG2TX(float mAG2TX) {
-		MAG2TX = mAG2TX;
+	public void setMag2tx(float mag2tx) {
+		this.mag2tx = mag2tx;
 	}
 
-	public float getMAG2TY() {
-		return MAG2TY;
+	public float getMag2ty() {
+		return mag2ty;
 	}
 
-	public void setMAG2TY(float mAG2TY) {
-		MAG2TY = mAG2TY;
+	public void setMag2ty(float mag2ty) {
+		this.mag2ty = mag2ty;
 	}
 
-	public float getMAG2TZ() {
-		return MAG2TZ;
+	public float getMag2tz() {
+		return mag2tz;
 	}
 
-	public void setMAG2TZ(float mAG2TZ) {
-		MAG2TZ = mAG2TZ;
+	public void setMag2tz(float mag2tz) {
+		this.mag2tz = mag2tz;
 	}
 
-	public float getGYRMSX() {
-		return GYRMSX;
+	public float getGyrmsx() {
+		return gyrmsx;
 	}
 
-	public void setGYRMSX(float gYRMSX) {
-		GYRMSX = gYRMSX;
+	public void setGyrmsx(float gyrmsx) {
+		this.gyrmsx = gyrmsx;
 	}
 
-	public float getGYRMSY() {
-		return GYRMSY;
+	public float getGyrmsy() {
+		return gyrmsy;
 	}
 
-	public void setGYRMSY(float gYRMSY) {
-		GYRMSY = gYRMSY;
+	public void setGyrmsy(float gyrmsy) {
+		this.gyrmsy = gyrmsy;
 	}
 
-	public float getGYRMSZ() {
-		return GYRMSZ;
+	public float getGyrmsz() {
+		return gyrmsz;
 	}
 
-	public void setGYRMSZ(float gYRMSZ) {
-		GYRMSZ = gYRMSZ;
+	public void setGyrmsz(float gyrmsz) {
+		this.gyrmsz = gyrmsz;
 	}
 
-	public float getGYRMOX() {
-		return GYRMOX;
+	public float getGyrmox() {
+		return gyrmox;
 	}
 
-	public void setGYRMOX(float gYRMOX) {
-		GYRMOX = gYRMOX;
+	public void setGyrmox(float gyrmox) {
+		this.gyrmox = gyrmox;
 	}
 
-	public float getGYRMOY() {
-		return GYRMOY;
+	public float getGyrmoy() {
+		return gyrmoy;
 	}
 
-	public void setGYRMOY(float gYRMOY) {
-		GYRMOY = gYRMOY;
+	public void setGyrmoy(float gyrmoy) {
+		this.gyrmoy = gyrmoy;
 	}
 
-	public float getGYRMOZ() {
-		return GYRMOZ;
+	public float getGyrmoz() {
+		return gyrmoz;
 	}
 
-	public void setGYRMOZ(float gYRMOZ) {
-		GYRMOZ = gYRMOZ;
+	public void setGyrmoz(float gyrmoz) {
+		this.gyrmoz = gyrmoz;
 	}
 
-	public float getGYRMTX() {
-		return GYRMTX;
+	public float getGyrmtx() {
+		return gyrmtx;
 	}
 
-	public void setGYRMTX(float gYRMTX) {
-		GYRMTX = gYRMTX;
+	public void setGyrmtx(float gyrmtx) {
+		this.gyrmtx = gyrmtx;
 	}
 
-	public float getGYRMTY() {
-		return GYRMTY;
+	public float getGyrmty() {
+		return gyrmty;
 	}
 
-	public void setGYRMTY(float gYRMTY) {
-		GYRMTY = gYRMTY;
+	public void setGyrmty(float gyrmty) {
+		this.gyrmty = gyrmty;
 	}
 
-	public float getGYRMTZ() {
-		return GYRMTZ;
+	public float getGyrmtz() {
+		return gyrmtz;
 	}
 
-	public void setGYRMTZ(float gYRMTZ) {
-		GYRMTZ = gYRMTZ;
+	public void setGyrmtz(float gyrmtz) {
+		this.gyrmtz = gyrmtz;
 	}
 
-	public float getGYRMST() {
-		return GYRMST;
+	public float getGyrmst() {
+		return gyrmst;
 	}
 
-	public void setGYRMST(float gYRMST) {
-		GYRMST = gYRMST;
+	public void setGyrmst(float gyrmst) {
+		this.gyrmst = gyrmst;
 	}
 
-	public float getGYRMOT() {
-		return GYRMOT;
+	public float getGyrmot() {
+		return gyrmot;
 	}
 
-	public void setGYRMOT(float gYRMOT) {
-		GYRMOT = gYRMOT;
+	public void setGyrmot(float gyrmot) {
+		this.gyrmot = gyrmot;
 	}
 
-	public float getGYRLSX() {
-		return GYRLSX;
+	public float getGyrlsx() {
+		return gyrlsx;
 	}
 
-	public void setGYRLSX(float gYRLSX) {
-		GYRLSX = gYRLSX;
+	public void setGyrlsx(float gyrlsx) {
+		this.gyrlsx = gyrlsx;
 	}
 
-	public float getGYRLSY() {
-		return GYRLSY;
+	public float getGyrlsy() {
+		return gyrlsy;
 	}
 
-	public void setGYRLSY(float gYRLSY) {
-		GYRLSY = gYRLSY;
+	public void setGyrlsy(float gyrlsy) {
+		this.gyrlsy = gyrlsy;
 	}
 
-	public float getGYRLSZ() {
-		return GYRLSZ;
+	public float getGyrlsz() {
+		return gyrlsz;
 	}
 
-	public void setGYRLSZ(float gYRLSZ) {
-		GYRLSZ = gYRLSZ;
+	public void setGyrlsz(float gyrlsz) {
+		this.gyrlsz = gyrlsz;
 	}
 
-	public float getGYRLOX() {
-		return GYRLOX;
+	public float getGyrlox() {
+		return gyrlox;
 	}
 
-	public void setGYRLOX(float gYRLOX) {
-		GYRLOX = gYRLOX;
+	public void setGyrlox(float gyrlox) {
+		this.gyrlox = gyrlox;
 	}
 
-	public float getGYRLOY() {
-		return GYRLOY;
+	public float getGyrloy() {
+		return gyrloy;
 	}
 
-	public void setGYRLOY(float gYRLOY) {
-		GYRLOY = gYRLOY;
+	public void setGyrloy(float gyrloy) {
+		this.gyrloy = gyrloy;
 	}
 
-	public float getGYRLOZ() {
-		return GYRLOZ;
+	public float getGyrloz() {
+		return gyrloz;
 	}
 
-	public void setGYRLOZ(float gYRLOZ) {
-		GYRLOZ = gYRLOZ;
+	public void setGyrloz(float gyrloz) {
+		this.gyrloz = gyrloz;
 	}
 
-	public float getGYRLTX() {
-		return GYRLTX;
+	public float getGyrltx() {
+		return gyrltx;
 	}
 
-	public void setGYRLTX(float gYRLTX) {
-		GYRLTX = gYRLTX;
+	public void setGyrltx(float gyrltx) {
+		this.gyrltx = gyrltx;
 	}
 
-	public float getGYRLTY() {
-		return GYRLTY;
+	public float getGyrlty() {
+		return gyrlty;
 	}
 
-	public void setGYRLTY(float gYRLTY) {
-		GYRLTY = gYRLTY;
+	public void setGyrlty(float gyrlty) {
+		this.gyrlty = gyrlty;
 	}
 
-	public float getGYRLTZ() {
-		return GYRLTZ;
+	public float getGyrltz() {
+		return gyrltz;
 	}
 
-	public void setGYRLTZ(float gYRLTZ) {
-		GYRLTZ = gYRLTZ;
+	public void setGyrltz(float gyrltz) {
+		this.gyrltz = gyrltz;
 	}
 
-	public float getGYRLST() {
-		return GYRLST;
+	public float getGyrlst() {
+		return gyrlst;
 	}
 
-	public void setGYRLST(float gYRLST) {
-		GYRLST = gYRLST;
+	public void setGyrlst(float gyrlst) {
+		this.gyrlst = gyrlst;
 	}
 
-	public float getGYRLOT() {
-		return GYRLOT;
+	public float getGyrlot() {
+		return gyrlot;
 	}
 
-	public void setGYRLOT(float gYRLOT) {
-		GYRLOT = gYRLOT;
+	public void setGyrlot(float gyrlot) {
+		this.gyrlot = gyrlot;
 	}
 
 }
