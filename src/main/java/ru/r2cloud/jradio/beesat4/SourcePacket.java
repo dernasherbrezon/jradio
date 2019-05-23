3800f02debed6bd1cca0f93a7296f9cc12fe5005
dernasherbrezon <rodionovamp@mail.ru>
2019-05-23 16:01:04 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/beesat4/SourcePacket.java
+++ src/main/java/ru/r2cloud/jradio/beesat4/SourcePacket.java
@@ -16,84 +16,84 @@ public class SourcePacket {
 	private Apid3 apid3;
 	private Apid4 apid4;
 	private Apid5 apid5;
-	private Apid6U12 apid6_12;
+	private Apid6U12 apid6U12;
 	private Apid13 apid13;
-	private Apid14U29 apid14_29;
+	private Apid14U29 apid14U29;
 	private Apid30 apid30;
 	private Apid31 apid31;
 	private Apid32 apid32;
-	private Apid6U12 apid33_39;
+	private Apid6U12 apid33U39;
 	private Apid40 apid40;
 	private Apid42 apid42;
-	private Apid43U50 apid43_50;
-	private Apid51U54 apid51_54;
+	private Apid43U50 apid43U50;
+	private Apid51U54 apid51U54;
 	private Apid57 apid57;
 	private Apid58 apid58;
 	private Apid59 apid59;
-	private Apid6U12 apid60_66;
+	private Apid6U12 apid60U66;
 
-	private int PVN; // Packet version number
-	private boolean PT; // Packet Type Indicator
-	private boolean SHF; // Packet Secondary header flag
-	private int APID; // Application process identifier
-	private int SEQFLAG; // GroupingFlags
-	private int PSC; // Source Sequence Count
-	private int PDL; // Packet data length
+	private int pvn; // packet version number
+	private boolean pt; // packet type indicator
+	private boolean shf; // packet secondary header flag
+	private int apid; // application process identifier
+	private int seqflag; // groupingflags
+	private int psc; // source sequence count
+	private int pdl; // packet data length
 
 	public void readExternal(DataInputStream dis) throws IOException {
 		int raw = dis.readUnsignedShort();
-		PVN = ((raw >> 13) & 0b111);
-		PT = ((raw >> 12) & 0b1) > 0;
-		SHF = ((raw >> 11) & 0b1) > 0;
-		APID = (raw & 0b11111111111);
+		pvn = ((raw >> 13) & 0b111);
+		pt = ((raw >> 12) & 0b1) > 0;
+		shf = ((raw >> 11) & 0b1) > 0;
+		apid = (raw & 0b11111111111);
 		raw = dis.readUnsignedShort();
-		SEQFLAG = ((raw >> 14) & 0b11);
-		PSC = raw & 0x3FFF;
-		PDL = dis.readUnsignedShort();
-		if (APID == 0) {
+		seqflag = ((raw >> 14) & 0b11);
+		psc = raw & 0x3FFF;
+		pdl = dis.readUnsignedShort();
+		if (apid == 0) {
 			apid0 = new Apid0(dis);
-		} else if (APID == 1) {
+		} else if (apid == 1) {
 			apid1 = new Apid1(dis);
-		} else if (APID == 2) {
+		} else if (apid == 2) {
 			apid2 = new Apid2(dis);
-		} else if (APID == 3) {
+		} else if (apid == 3) {
 			apid3 = new Apid3(dis);
-		} else if (APID == 4) {
+		} else if (apid == 4) {
 			apid4 = new Apid4(dis);
-		} else if (APID == 5) {
+		} else if (apid == 5) {
 			apid5 = new Apid5(dis);
-		} else if (APID >= 6 && APID <= 12) {
-			apid6_12 = new Apid6U12(dis);
-		} else if (APID == 13) {
+		} else if (apid >= 6 && apid <= 12) {
+			apid6U12 = new Apid6U12(dis);
+		} else if (apid == 13) {
 			apid13 = new Apid13(dis);
-		} else if (APID >= 14 && APID <= 29) {
-			apid14_29 = new Apid14U29(dis);
-		} else if (APID == 30) {
+		} else if (apid >= 14 && apid <= 29) {
+			apid14U29 = new Apid14U29(dis);
+		} else if (apid == 30) {
 			apid30 = new Apid30(dis);
-		} else if (APID == 31) {
+		} else if (apid == 31) {
 			apid31 = new Apid31(dis);
-		} else if (APID == 32) {
+		} else if (apid == 32) {
 			apid32 = new Apid32(dis);
-		} else if (APID >= 33 && APID <= 39) {
-			apid33_39 = new Apid6U12(dis);
-		} else if (APID == 40) {
+		} else if (apid >= 33 && apid <= 39) {
+			apid33U39 = new Apid6U12(dis);
+		} else if (apid == 40) {
 			apid40 = new Apid40(dis);
-		} else if (APID == 42) {
+		} else if (apid == 42) {
 			apid42 = new Apid42(dis);
-		} else if (APID >= 43 && APID <= 50) {
-			apid43_50 = new Apid43U50(dis);
-		} else if (APID >= 51 && APID <= 54) {
-			apid51_54 = new Apid51U54(dis);
-		} else if (APID == 57) {
+		} else if (apid >= 43 && apid <= 50) {
+			apid43U50 = new Apid43U50(dis);
+		} else if (apid >= 51 && apid <= 54) {
+			apid51U54 = new Apid51U54(dis);
+		} else if (apid == 57) {
 			apid57 = new Apid57(dis);
-		} else if (APID == 58) {
+		} else if (apid == 58) {
 			apid58 = new Apid58(dis);
-		} else if (APID == 59) {
+		} else if (apid == 59) {
 			apid59 = new Apid59(dis);
-		} else if (APID >= 60 && APID <= 66) {
-			apid60_66 = new Apid6U12(dis);
+		} else if (apid >= 60 && apid <= 66) {
+			apid60U66 = new Apid6U12(dis);
 		} else {
-			LOG.error("unknown apid: {}", APID);
+			LOG.error("unknown apid: {}", apid);
 			dis.skipBytes(126);
 		}
 	}
@@ -146,12 +146,12 @@ public class SourcePacket {
 		this.apid5 = apid5;
 	}
 
-	public Apid6U12 getApid6_12() {
-		return apid6_12;
+	public Apid6U12 getApid6U12() {
+		return apid6U12;
 	}
 
-	public void setApid6_12(Apid6U12 apid6_12) {
-		this.apid6_12 = apid6_12;
+	public void setApid6U12(Apid6U12 apid6u12) {
+		apid6U12 = apid6u12;
 	}
 
 	public Apid13 getApid13() {
@@ -162,12 +162,12 @@ public class SourcePacket {
 		this.apid13 = apid13;
 	}
 
-	public Apid14U29 getApid14_29() {
-		return apid14_29;
+	public Apid14U29 getApid14U29() {
+		return apid14U29;
 	}
 
-	public void setApid14_29(Apid14U29 apid14_29) {
-		this.apid14_29 = apid14_29;
+	public void setApid14U29(Apid14U29 apid14u29) {
+		apid14U29 = apid14u29;
 	}
 
 	public Apid30 getApid30() {
@@ -194,12 +194,12 @@ public class SourcePacket {
 		this.apid32 = apid32;
 	}
 
-	public Apid6U12 getApid33_39() {
-		return apid33_39;
+	public Apid6U12 getApid33U39() {
+		return apid33U39;
 	}
 
-	public void setApid33_39(Apid6U12 apid33_39) {
-		this.apid33_39 = apid33_39;
+	public void setApid33U39(Apid6U12 apid33u39) {
+		apid33U39 = apid33u39;
 	}
 
 	public Apid40 getApid40() {
@@ -218,20 +218,20 @@ public class SourcePacket {
 		this.apid42 = apid42;
 	}
 
-	public Apid43U50 getApid43_50() {
-		return apid43_50;
+	public Apid43U50 getApid43U50() {
+		return apid43U50;
 	}
 
-	public void setApid43_50(Apid43U50 apid43_50) {
-		this.apid43_50 = apid43_50;
+	public void setApid43U50(Apid43U50 apid43u50) {
+		apid43U50 = apid43u50;
 	}
 
-	public Apid51U54 getApid51_54() {
-		return apid51_54;
+	public Apid51U54 getApid51U54() {
+		return apid51U54;
 	}
 
-	public void setApid51_54(Apid51U54 apid51_54) {
-		this.apid51_54 = apid51_54;
+	public void setApid51U54(Apid51U54 apid51u54) {
+		apid51U54 = apid51u54;
 	}
 
 	public Apid57 getApid57() {
@@ -258,68 +258,68 @@ public class SourcePacket {
 		this.apid59 = apid59;
 	}
 
-	public Apid6U12 getApid60_66() {
-		return apid60_66;
+	public Apid6U12 getApid60U66() {
+		return apid60U66;
 	}
 
-	public void setApid60_66(Apid6U12 apid60_66) {
-		this.apid60_66 = apid60_66;
+	public void setApid60U66(Apid6U12 apid60u66) {
+		apid60U66 = apid60u66;
 	}
 
-	public int getPVN() {
-		return PVN;
+	public int getPvn() {
+		return pvn;
 	}
 
-	public void setPVN(int pVN) {
-		PVN = pVN;
+	public void setPvn(int pvn) {
+		this.pvn = pvn;
 	}
 
-	public boolean isPT() {
-		return PT;
+	public boolean isPt() {
+		return pt;
 	}
 
-	public void setPT(boolean pT) {
-		PT = pT;
+	public void setPt(boolean pt) {
+		this.pt = pt;
 	}
 
-	public boolean isSHF() {
-		return SHF;
+	public boolean isShf() {
+		return shf;
 	}
 
-	public void setSHF(boolean sHF) {
-		SHF = sHF;
+	public void setShf(boolean shf) {
+		this.shf = shf;
 	}
 
-	public int getAPID() {
-		return APID;
+	public int getApid() {
+		return apid;
 	}
 
-	public void setAPID(int aPID) {
-		APID = aPID;
+	public void setApid(int apid) {
+		this.apid = apid;
 	}
 
-	public int getSEQFLAG() {
-		return SEQFLAG;
+	public int getSeqflag() {
+		return seqflag;
 	}
 
-	public void setSEQFLAG(int sEQFLAG) {
-		SEQFLAG = sEQFLAG;
+	public void setSeqflag(int seqflag) {
+		this.seqflag = seqflag;
 	}
 
-	public int getPSC() {
-		return PSC;
+	public int getPsc() {
+		return psc;
 	}
 
-	public void setPSC(int pSC) {
-		PSC = pSC;
+	public void setPsc(int psc) {
+		this.psc = psc;
 	}
 
-	public int getPDL() {
-		return PDL;
+	public int getPdl() {
+		return pdl;
 	}
 
-	public void setPDL(int pDL) {
-		PDL = pDL;
+	public void setPdl(int pdl) {
+		this.pdl = pdl;
 	}
 
 }
