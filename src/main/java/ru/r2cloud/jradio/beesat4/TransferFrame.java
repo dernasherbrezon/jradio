1d33f9581da5bc11b05d1f5cd8f7c6a45c014dae
dernasherbrezon <rodionovamp@mail.ru>
2019-05-25 10:12:45 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/beesat4/TransferFrame.java
+++ src/main/java/ru/r2cloud/jradio/beesat4/TransferFrame.java
@@ -5,152 +5,152 @@ import java.io.IOException;
 
 public class TransferFrame {
 
-	private String ASM;             //  Attached synchronisation marker
-	private int TFVN;               //  Transfer frame version number
-	private int SCID;               //  Spacecraft Identifier
-	private int VCID;               //  Virtual channel identifier
-	private boolean OCFF;           //  Operational control field flag
-	private int MCFC;               //  Master channel frame count
-	private int VCFC;               //  Virtual channel frame count
-	private boolean TF_SHF;         //  TF secondary header flag
-	private boolean SYNCHFLAG;      //  Synchronisation flag
-	private boolean POF;            //  Packet order flag
-	private int SLID;               //  Segment length identifier
-	private int FHP;                //  First header pointer
-	private int FECFD;              //  Frame Error Control Field Data
-	
+	private String asm; // Attached synchronisation marker
+	private int tfvn; // Transfer frame version number
+	private int scid; // Spacecraft Identifier
+	private int vcid; // Virtual channel identifier
+	private boolean ocff; // Operational control field flag
+	private int mcfc; // Master channel frame count
+	private int vcfc; // Virtual channel frame count
+	private boolean tfShf; // TF secondary header flag
+	private boolean synchflag; // Synchronisation flag
+	private boolean pof; // Packet order flag
+	private int slid; // Segment length identifier
+	private int fhp; // First header pointer
+	private int fecfd; // Frame Error Control Field Data
+
 	private SourcePacket packet;
 
 	public void readExternal(DataInputStream dis) throws IOException {
-		ASM = Integer.toString(dis.readInt(), 16);
+		asm = Integer.toString(dis.readInt(), 16);
 		int raw = dis.readUnsignedShort();
-		TFVN = ((raw >> 14) & 0b11);
-		SCID = ((raw >> 4) & 0b1111111111);
-		VCID = ((raw >> 1) & 0b111);
-		OCFF = (raw & 0x1) > 0;
-		MCFC = dis.readUnsignedByte();
-		VCFC = dis.readUnsignedByte();
+		tfvn = ((raw >> 14) & 0b11);
+		scid = ((raw >> 4) & 0b1111111111);
+		vcid = ((raw >> 1) & 0b111);
+		ocff = (raw & 0x1) > 0;
+		mcfc = dis.readUnsignedByte();
+		vcfc = dis.readUnsignedByte();
 		raw = dis.readUnsignedShort();
-		TF_SHF = ((raw >> 15) & 0x1) > 0;
-		SYNCHFLAG = ((raw >> 14) & 0x1) > 0;
-		POF = ((raw >> 13) & 0x1) > 0;
-		SLID = ((raw >> 11) & 0b11);
-		FHP = (raw & 0b11111111111);
+		tfShf = ((raw >> 15) & 0x1) > 0;
+		synchflag = ((raw >> 14) & 0x1) > 0;
+		pof = ((raw >> 13) & 0x1) > 0;
+		slid = ((raw >> 11) & 0b11);
+		fhp = (raw & 0b11111111111);
 		packet = new SourcePacket();
 		packet.readExternal(dis);
-		FECFD = dis.readUnsignedShort();
+		fecfd = dis.readUnsignedShort();
 	}
-	
-	public int getFECFD() {
-		return FECFD;
+
+	public String getAsm() {
+		return asm;
 	}
-	
-	public void setFECFD(int fECFD) {
-		FECFD = fECFD;
+
+	public void setAsm(String asm) {
+		this.asm = asm;
 	}
-	
+
+	public int getTfvn() {
+		return tfvn;
+	}
+
+	public void setTfvn(int tfvn) {
+		this.tfvn = tfvn;
+	}
+
+	public int getScid() {
+		return scid;
+	}
+
+	public void setScid(int scid) {
+		this.scid = scid;
+	}
+
+	public int getVcid() {
+		return vcid;
+	}
+
+	public void setVcid(int vcid) {
+		this.vcid = vcid;
+	}
+
+	public boolean isOcff() {
+		return ocff;
+	}
+
+	public void setOcff(boolean ocff) {
+		this.ocff = ocff;
+	}
+
+	public int getMcfc() {
+		return mcfc;
+	}
+
+	public void setMcfc(int mcfc) {
+		this.mcfc = mcfc;
+	}
+
+	public int getVcfc() {
+		return vcfc;
+	}
+
+	public void setVcfc(int vcfc) {
+		this.vcfc = vcfc;
+	}
+
+	public boolean isTfShf() {
+		return tfShf;
+	}
+
+	public void setTfShf(boolean tfShf) {
+		this.tfShf = tfShf;
+	}
+
+	public boolean isSynchflag() {
+		return synchflag;
+	}
+
+	public void setSynchflag(boolean synchflag) {
+		this.synchflag = synchflag;
+	}
+
+	public boolean isPof() {
+		return pof;
+	}
+
+	public void setPof(boolean pof) {
+		this.pof = pof;
+	}
+
+	public int getSlid() {
+		return slid;
+	}
+
+	public void setSlid(int slid) {
+		this.slid = slid;
+	}
+
+	public int getFhp() {
+		return fhp;
+	}
+
+	public void setFhp(int fhp) {
+		this.fhp = fhp;
+	}
+
+	public int getFecfd() {
+		return fecfd;
+	}
+
+	public void setFecfd(int fecfd) {
+		this.fecfd = fecfd;
+	}
+
 	public SourcePacket getPacket() {
 		return packet;
 	}
-	
+
 	public void setPacket(SourcePacket packet) {
 		this.packet = packet;
-	}
-
-	public String getASM() {
-		return ASM;
-	}
-
-	public void setASM(String aSM) {
-		ASM = aSM;
-	}
-
-	public int getTFVN() {
-		return TFVN;
-	}
-
-	public void setTFVN(int tFVN) {
-		TFVN = tFVN;
-	}
-
-	public int getSCID() {
-		return SCID;
-	}
-
-	public void setSCID(int sCID) {
-		SCID = sCID;
-	}
-
-	public int getVCID() {
-		return VCID;
-	}
-
-	public void setVCID(int vCID) {
-		VCID = vCID;
-	}
-
-	public boolean isOCFF() {
-		return OCFF;
-	}
-
-	public void setOCFF(boolean oCFF) {
-		OCFF = oCFF;
-	}
-
-	public int getMCFC() {
-		return MCFC;
-	}
-
-	public void setMCFC(int mCFC) {
-		MCFC = mCFC;
-	}
-
-	public int getVCFC() {
-		return VCFC;
-	}
-
-	public void setVCFC(int vCFC) {
-		VCFC = vCFC;
-	}
-
-	public boolean isTF_SHF() {
-		return TF_SHF;
-	}
-
-	public void setTF_SHF(boolean tF_SHF) {
-		TF_SHF = tF_SHF;
-	}
-
-	public boolean isSYNCHFLAG() {
-		return SYNCHFLAG;
-	}
-
-	public void setSYNCHFLAG(boolean sYNCHFLAG) {
-		SYNCHFLAG = sYNCHFLAG;
-	}
-
-	public boolean isPOF() {
-		return POF;
-	}
-
-	public void setPOF(boolean pOF) {
-		POF = pOF;
-	}
-
-	public int getSLID() {
-		return SLID;
-	}
-
-	public void setSLID(int sLID) {
-		SLID = sLID;
-	}
-
-	public int getFHP() {
-		return FHP;
-	}
-
-	public void setFHP(int fHP) {
-		FHP = fHP;
 	}
 
 }
