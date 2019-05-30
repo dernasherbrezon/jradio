88eb9fdba4e1b56ba6f7bf093c6c9005354d4bbc
dernasherbrezon <rodionovamp@mail.ru>
2019-05-30 07:46:55 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/blocks/FLLBandEdge.java
+++ src/main/java/ru/r2cloud/jradio/blocks/FLLBandEdge.java
@@ -10,8 +10,8 @@ import ru.r2cloud.jradio.util.MathUtils;
 public class FLLBandEdge implements FloatInput {
 
 	private final FloatInput source;
-	private ComplexFIRFilter d_filter_upper;
-	private ComplexFIRFilter d_filter_lower;
+	private ComplexFIRFilter filterUpper;
+	private ComplexFIRFilter filterLower;
 	private ControlLoop controlLoop;
 	private final CircularComplexArray array;
 
@@ -36,7 +36,7 @@ public class FLLBandEdge implements FloatInput {
 		this.source = source;
 		this.array = new CircularComplexArray(filterSize);
 		this.controlLoop = new ControlLoop(bandwidth, (float) ((2 * Math.PI) * (2.0 / samplesPerSymbol)), (float) (-(2 * Math.PI) * (2.0 / samplesPerSymbol)));
-		design_filter(samplesPerSymbol, rolloff, filterSize);
+		designFilter(samplesPerSymbol, rolloff, filterSize);
 	}
 
 	@Override
@@ -52,8 +52,8 @@ public class FLLBandEdge implements FloatInput {
 
 			array.add(currentComplex[0], currentComplex[1]);
 			// Perform the dot product of the output with the filters
-			d_filter_lower.filterComplex(currentUpperComplex, array);
-			d_filter_upper.filterComplex(currentLowerComplex, array);
+			filterLower.filterComplex(currentUpperComplex, array);
+			filterUpper.filterComplex(currentLowerComplex, array);
 
 			float error = MathUtils.norm(currentLowerComplex) - MathUtils.norm(currentUpperComplex);
 
@@ -66,50 +66,50 @@ public class FLLBandEdge implements FloatInput {
 		return currentComplex[1];
 	}
 
-	private void design_filter(float samps_per_sym, float rolloff, int filter_size) {
-		int M = (int) Math.rint(filter_size / samps_per_sym);
+	private void designFilter(float samplesPerSymbol, float rolloff, int filterSize) {
+		int m = (int) Math.rint(filterSize / samplesPerSymbol);
 		float power = 0;
 
 		// Create the baseband filter by adding two sincs together
-		float[] bb_taps = new float[filter_size];
-		for (int i = 0; i < bb_taps.length; i++) {
-			float k = -M + i * 2.0f / samps_per_sym;
+		float[] basebandTaps = new float[filterSize];
+		for (int i = 0; i < basebandTaps.length; i++) {
+			float k = -m + i * 2.0f / samplesPerSymbol;
 			float tap = MathUtils.sinc(rolloff * k - 0.5f) + MathUtils.sinc(rolloff * k + 0.5f);
 			power += tap;
-			bb_taps[i] = tap;
+			basebandTaps[i] = tap;
 		}
 		
 		if (power == 0.0f) {
 			throw new IllegalArgumentException("invalid power: " + power);
 		}
 
-		float[] d_taps_lowerReal = new float[filter_size];
-		float[] d_taps_lowerImg = new float[filter_size];
-		float[] d_taps_upperReal = new float[filter_size];
-		float[] d_taps_upperImg = new float[filter_size];
+		float[] tapsLowerReal = new float[filterSize];
+		float[] tapsLowerImg = new float[filterSize];
+		float[] tapsUpperReal = new float[filterSize];
+		float[] tapsUpperImg = new float[filterSize];
 
 		// Create the band edge filters by spinning the baseband
 		// filter up and down to the right places in frequency.
 		// Also, normalize the power in the filters
 		float[] tempComplex = new float[2];
-		int N = (int) ((bb_taps.length - 1.0) / 2.0);
-		for (int i = 0; i < filter_size; i++) {
-			float tap = bb_taps[i] / power;
+		int n = (int) ((basebandTaps.length - 1.0) / 2.0);
+		for (int i = 0; i < filterSize; i++) {
+			float tap = basebandTaps[i] / power;
 
-			float k = (-N + i) / (2.0f * samps_per_sym);
+			float k = (-n + i) / (2.0f * samplesPerSymbol);
 
 			MathUtils.expj(tempComplex, -(2 * (float) Math.PI) * (1 + rolloff) * k);
 			MathUtils.multiply(tap, tempComplex);
-			d_taps_lowerReal[filter_size - i - 1] = tempComplex[0];
-			d_taps_lowerImg[filter_size - i - 1] = tempComplex[1];
+			tapsLowerReal[filterSize - i - 1] = tempComplex[0];
+			tapsLowerImg[filterSize - i - 1] = tempComplex[1];
 			MathUtils.expj(tempComplex, (2 * (float) Math.PI) * (1 + rolloff) * k);
 			MathUtils.multiply(tap, tempComplex);
-			d_taps_upperReal[filter_size - i - 1] = tempComplex[0];
-			d_taps_upperImg[filter_size - i - 1] = tempComplex[1];
+			tapsUpperReal[filterSize - i - 1] = tempComplex[0];
+			tapsUpperImg[filterSize - i - 1] = tempComplex[1];
 		}
 
-		d_filter_upper = new ComplexFIRFilter(d_taps_upperReal, d_taps_upperImg);
-		d_filter_lower = new ComplexFIRFilter(d_taps_lowerReal, d_taps_lowerImg);
+		filterUpper = new ComplexFIRFilter(tapsUpperReal, tapsUpperImg);
+		filterLower = new ComplexFIRFilter(tapsLowerReal, tapsLowerImg);
 	}
 
 	@Override
