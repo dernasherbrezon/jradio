53359ca6de303f4e1e9f03f05672095e21da822f
dernasherbrezon <rodionovamp@mail.ru>
2019-06-30 11:34:24 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/eseo/HSTXCommunicationCondition.java
+++ src/main/java/ru/r2cloud/jradio/eseo/HSTXCommunicationCondition.java
@@ -6,92 +6,92 @@ import ru.r2cloud.jradio.util.LittleEndianDataInputStream;
 
 public class HSTXCommunicationCondition {
 
-	private int CAN_TEC; // Can Controller Transmit Error Counter.
-	private int CAN_REC; // CAN Controller Received Error Counter
-	private boolean HRB; // Detection of the heartbeat messages on both CAN busses
-	private boolean PLTC_INT; // Error in interpreting data from HSTX
-	private boolean OBTC_INT; // Error in interpreting TC from OBDH (address out of range)
-	private boolean TC_SEQ; // Error in interpreting TC from OBDH (sequence error)
-	private int LEC0; // CAN Controller Last Error Code.
-	private boolean COMERR; // One or more errors in STX_COM_ERROR occurred.
+	private int canTEC; // Can Controller Transmit Error Counter.
+	private int canREC; // CAN Controller Received Error Counter
+	private boolean hrb; // Detection of the heartbeat messages on both CAN busses
+	private boolean pltcINT; // Error in interpreting data from HSTX
+	private boolean obtcINT; // Error in interpreting TC from OBDH (address out of range)
+	private boolean tcSEQ; // Error in interpreting TC from OBDH (sequence error)
+	private int lec0; // CAN Controller Last Error Code.
+	private boolean comerr; // One or more errors in STX_COM_ERROR occurred.
 
 	public HSTXCommunicationCondition(LittleEndianDataInputStream dis) throws IOException {
-		CAN_TEC = dis.readUnsignedByte();
-		CAN_REC = dis.readUnsignedByte();
+		canTEC = dis.readUnsignedByte();
+		canREC = dis.readUnsignedByte();
 
 		int raw = dis.readUnsignedByte();
-		HRB = ((raw >> 6) & 0x1) > 0;
-		PLTC_INT = ((raw >> 5) & 0x1) > 0;
-		OBTC_INT = ((raw >> 4) & 0x1) > 0;
-		TC_SEQ = ((raw >> 3) & 0x1) > 0;
-		LEC0 = (raw & 0b111);
+		hrb = ((raw >> 6) & 0x1) > 0;
+		pltcINT = ((raw >> 5) & 0x1) > 0;
+		obtcINT = ((raw >> 4) & 0x1) > 0;
+		tcSEQ = ((raw >> 3) & 0x1) > 0;
+		lec0 = (raw & 0b111);
 
 		raw = dis.readUnsignedByte();
-		COMERR = ((raw >> 7) & 0x1) > 0;
+		comerr = ((raw >> 7) & 0x1) > 0;
 	}
 
-	public int getCAN_TEC() {
-		return CAN_TEC;
+	public int getCanTEC() {
+		return canTEC;
 	}
 
-	public void setCAN_TEC(int cAN_TEC) {
-		CAN_TEC = cAN_TEC;
+	public void setCanTEC(int canTEC) {
+		this.canTEC = canTEC;
 	}
 
-	public int getCAN_REC() {
-		return CAN_REC;
+	public int getCanREC() {
+		return canREC;
 	}
 
-	public void setCAN_REC(int cAN_REC) {
-		CAN_REC = cAN_REC;
+	public void setCanREC(int canREC) {
+		this.canREC = canREC;
 	}
 
-	public boolean isHRB() {
-		return HRB;
+	public boolean isHrb() {
+		return hrb;
 	}
 
-	public void setHRB(boolean hRB) {
-		HRB = hRB;
+	public void setHrb(boolean hrb) {
+		this.hrb = hrb;
 	}
 
-	public boolean isPLTC_INT() {
-		return PLTC_INT;
+	public boolean isPltcINT() {
+		return pltcINT;
 	}
 
-	public void setPLTC_INT(boolean pLTC_INT) {
-		PLTC_INT = pLTC_INT;
+	public void setPltcINT(boolean pltcINT) {
+		this.pltcINT = pltcINT;
 	}
 
-	public boolean isOBTC_INT() {
-		return OBTC_INT;
+	public boolean isObtcINT() {
+		return obtcINT;
 	}
 
-	public void setOBTC_INT(boolean oBTC_INT) {
-		OBTC_INT = oBTC_INT;
+	public void setObtcINT(boolean obtcINT) {
+		this.obtcINT = obtcINT;
 	}
 
-	public boolean isTC_SEQ() {
-		return TC_SEQ;
+	public boolean isTcSEQ() {
+		return tcSEQ;
 	}
 
-	public void setTC_SEQ(boolean tC_SEQ) {
-		TC_SEQ = tC_SEQ;
+	public void setTcSEQ(boolean tcSEQ) {
+		this.tcSEQ = tcSEQ;
 	}
 
-	public int getLEC0() {
-		return LEC0;
+	public int getLec0() {
+		return lec0;
 	}
 
-	public void setLEC0(int lEC0) {
-		LEC0 = lEC0;
+	public void setLec0(int lec0) {
+		this.lec0 = lec0;
 	}
 
-	public boolean isCOMERR() {
-		return COMERR;
+	public boolean isComerr() {
+		return comerr;
 	}
 
-	public void setCOMERR(boolean cOMERR) {
-		COMERR = cOMERR;
+	public void setComerr(boolean comerr) {
+		this.comerr = comerr;
 	}
 
 }
