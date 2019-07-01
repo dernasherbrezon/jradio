c4e1cc558b78393719f8faa8a8d41ee388b34048
dernasherbrezon <rodionovamp@mail.ru>
2019-07-01 07:24:48 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/eseo/HSTXMemoryCondition.java
+++ src/main/java/ru/r2cloud/jradio/eseo/HSTXMemoryCondition.java
@@ -6,153 +6,153 @@ import ru.r2cloud.jradio.util.LittleEndianDataInputStream;
 
 public class HSTXMemoryCondition {
 
-	private int MBFAIL; // Number of death cells (def.: 00).
-	private boolean M5AFULL;
-	private boolean M5FULL;
-	private boolean M4AFULL;
-	private boolean M4FULL;
-	private boolean M3AFULL;
-	private boolean M3FULL;
-	private boolean M1AFULL; // Memory buffer for virtual channel VC=1 is almost full. (20% spare left).
-	private boolean M1FULL; // Memory buffer for virtual channel VC=1 is full.
-	private boolean MEMERR;
-	private boolean M7AFULL;
-	private boolean M7FULL;
-	private boolean M6AFULL;
-	private boolean M6FULL;
+	private int mbfail; // number of death cells (def.: 00).
+	private boolean m5afull;
+	private boolean m5full;
+	private boolean m4afull;
+	private boolean m4full;
+	private boolean m3afull;
+	private boolean m3full;
+	private boolean m1afull; // Memory buffer for virtual channel VC=1 is almost full. (20% spare left).
+	private boolean m1full; // memory buffer for virtual channel VC=1 is full.
+	private boolean memerr;
+	private boolean m7afull;
+	private boolean m7full;
+	private boolean m6afull;
+	private boolean m6full;
 
 	public HSTXMemoryCondition(LittleEndianDataInputStream dis) throws IOException {
-		MBFAIL = dis.readUnsignedByte();
+		mbfail = dis.readUnsignedByte();
 		dis.skipBytes(1);
 
 		int raw = dis.readUnsignedByte();
-		M5AFULL = ((raw >> 7) & 0x1) > 0;
-		M5FULL = ((raw >> 6) & 0x1) > 0;
-		M4AFULL = ((raw >> 5) & 0x1) > 0;
-		M4FULL = ((raw >> 4) & 0x1) > 0;
-		M3AFULL = ((raw >> 3) & 0x1) > 0;
-		M3FULL = ((raw >> 2) & 0x1) > 0;
-		M1AFULL = ((raw >> 1) & 0x1) > 0;
-		M1FULL = (raw & 0x1) > 0;
+		m5afull = ((raw >> 7) & 0x1) > 0;
+		m5full = ((raw >> 6) & 0x1) > 0;
+		m4afull = ((raw >> 5) & 0x1) > 0;
+		m4full = ((raw >> 4) & 0x1) > 0;
+		m3afull = ((raw >> 3) & 0x1) > 0;
+		m3full = ((raw >> 2) & 0x1) > 0;
+		m1afull = ((raw >> 1) & 0x1) > 0;
+		m1full = (raw & 0x1) > 0;
 
 		raw = dis.readUnsignedByte();
-		MEMERR = ((raw >> 7) & 0x1) > 0;
-		M7AFULL = ((raw >> 3) & 0x1) > 0;
-		M7FULL = ((raw >> 2) & 0x1) > 0;
-		M6AFULL = ((raw >> 1) & 0x1) > 0;
-		M6FULL = (raw & 0x1) > 0;
+		memerr = ((raw >> 7) & 0x1) > 0;
+		m7afull = ((raw >> 3) & 0x1) > 0;
+		m7full = ((raw >> 2) & 0x1) > 0;
+		m6afull = ((raw >> 1) & 0x1) > 0;
+		m6full = (raw & 0x1) > 0;
 	}
 
-	public int getMBFAIL() {
-		return MBFAIL;
+	public int getMbfail() {
+		return mbfail;
 	}
 
-	public void setMBFAIL(int mBFAIL) {
-		MBFAIL = mBFAIL;
+	public void setMbfail(int mbfail) {
+		this.mbfail = mbfail;
 	}
 
-	public boolean isM5AFULL() {
-		return M5AFULL;
+	public boolean isM5afull() {
+		return m5afull;
 	}
 
-	public void setM5AFULL(boolean m5afull) {
-		M5AFULL = m5afull;
+	public void setM5afull(boolean m5afull) {
+		this.m5afull = m5afull;
 	}
 
-	public boolean isM5FULL() {
-		return M5FULL;
+	public boolean isM5full() {
+		return m5full;
 	}
 
-	public void setM5FULL(boolean m5full) {
-		M5FULL = m5full;
+	public void setM5full(boolean m5full) {
+		this.m5full = m5full;
 	}
 
-	public boolean isM4AFULL() {
-		return M4AFULL;
+	public boolean isM4afull() {
+		return m4afull;
 	}
 
-	public void setM4AFULL(boolean m4afull) {
-		M4AFULL = m4afull;
+	public void setM4afull(boolean m4afull) {
+		this.m4afull = m4afull;
 	}
 
-	public boolean isM4FULL() {
-		return M4FULL;
+	public boolean isM4full() {
+		return m4full;
 	}
 
-	public void setM4FULL(boolean m4full) {
-		M4FULL = m4full;
+	public void setM4full(boolean m4full) {
+		this.m4full = m4full;
 	}
 
-	public boolean isM3AFULL() {
-		return M3AFULL;
+	public boolean isM3afull() {
+		return m3afull;
 	}
 
-	public void setM3AFULL(boolean m3afull) {
-		M3AFULL = m3afull;
+	public void setM3afull(boolean m3afull) {
+		this.m3afull = m3afull;
 	}
 
-	public boolean isM3FULL() {
-		return M3FULL;
+	public boolean isM3full() {
+		return m3full;
 	}
 
-	public void setM3FULL(boolean m3full) {
-		M3FULL = m3full;
+	public void setM3full(boolean m3full) {
+		this.m3full = m3full;
 	}
 
-	public boolean isM1AFULL() {
-		return M1AFULL;
+	public boolean isM1afull() {
+		return m1afull;
 	}
 
-	public void setM1AFULL(boolean m1afull) {
-		M1AFULL = m1afull;
+	public void setM1afull(boolean m1afull) {
+		this.m1afull = m1afull;
 	}
 
-	public boolean isM1FULL() {
-		return M1FULL;
+	public boolean isM1full() {
+		return m1full;
 	}
 
-	public void setM1FULL(boolean m1full) {
-		M1FULL = m1full;
+	public void setM1full(boolean m1full) {
+		this.m1full = m1full;
 	}
 
-	public boolean isMEMERR() {
-		return MEMERR;
+	public boolean isMemerr() {
+		return memerr;
 	}
 
-	public void setMEMERR(boolean mEMERR) {
-		MEMERR = mEMERR;
+	public void setMemerr(boolean memerr) {
+		this.memerr = memerr;
 	}
 
-	public boolean isM7AFULL() {
-		return M7AFULL;
+	public boolean isM7afull() {
+		return m7afull;
 	}
 
-	public void setM7AFULL(boolean m7afull) {
-		M7AFULL = m7afull;
+	public void setM7afull(boolean m7afull) {
+		this.m7afull = m7afull;
 	}
 
-	public boolean isM7FULL() {
-		return M7FULL;
+	public boolean isM7full() {
+		return m7full;
 	}
 
-	public void setM7FULL(boolean m7full) {
-		M7FULL = m7full;
+	public void setM7full(boolean m7full) {
+		this.m7full = m7full;
 	}
 
-	public boolean isM6AFULL() {
-		return M6AFULL;
+	public boolean isM6afull() {
+		return m6afull;
 	}
 
-	public void setM6AFULL(boolean m6afull) {
-		M6AFULL = m6afull;
+	public void setM6afull(boolean m6afull) {
+		this.m6afull = m6afull;
 	}
 
-	public boolean isM6FULL() {
-		return M6FULL;
+	public boolean isM6full() {
+		return m6full;
 	}
 
-	public void setM6FULL(boolean m6full) {
-		M6FULL = m6full;
+	public void setM6full(boolean m6full) {
+		this.m6full = m6full;
 	}
 
 }
