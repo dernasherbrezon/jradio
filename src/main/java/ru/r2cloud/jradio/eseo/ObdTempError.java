c893fc9a29ec267d33079aaa127a3ccb9cf7ba3e
dernasherbrezon <rodionovamp@mail.ru>
2019-07-09 07:26:26 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/eseo/ObdTempError.java
+++ src/main/java/ru/r2cloud/jradio/eseo/ObdTempError.java
@@ -6,121 +6,121 @@ import ru.r2cloud.jradio.util.LittleEndianDataInputStream;
 
 public class ObdTempError {
 
-	private boolean TEMP1_PDU1;
-	private boolean TEMP2_BAT1;
-	private boolean TEMP3_PMB;
-	private boolean TEMP4_HPA2;
-	private boolean TEMP8_HPA1;
-	private boolean TEMP10_TNK;
-	private boolean TEMP11_BAT2;
-	private boolean TEMP12_MWM;
-	private boolean TEMP13_MWR;
-	private boolean TEMP14_MMM;
-	private boolean TEMP15_MMR;
+	private boolean temp1PDU1;
+	private boolean temp2BAT1;
+	private boolean temp3PMB;
+	private boolean temp4HPA2;
+	private boolean temp8HPA1;
+	private boolean temp10TNK;
+	private boolean temp11BAT2;
+	private boolean temp12MWM;
+	private boolean temp13MWR;
+	private boolean temp14MMM;
+	private boolean temp15MMR;
 
 	public ObdTempError(LittleEndianDataInputStream dis) throws IOException {
 		int raw = dis.readUnsignedByte();
-		TEMP1_PDU1 = ((raw >> 7) & 0x1) > 0;
-		TEMP2_BAT1 = ((raw >> 6) & 0x1) > 0;
-		TEMP3_PMB = ((raw >> 5) & 0x1) > 0;
-		TEMP4_HPA2 = ((raw >> 4) & 0x1) > 0;
-		TEMP8_HPA1 = ((raw >> 3) & 0x1) > 0;
-		TEMP10_TNK = ((raw >> 2) & 0x1) > 0;
-		TEMP11_BAT2 = ((raw >> 1) & 0x1) > 0;
-		TEMP12_MWM = (raw & 0x1) > 0;
+		temp1PDU1 = ((raw >> 7) & 0x1) > 0;
+		temp2BAT1 = ((raw >> 6) & 0x1) > 0;
+		temp3PMB = ((raw >> 5) & 0x1) > 0;
+		temp4HPA2 = ((raw >> 4) & 0x1) > 0;
+		temp8HPA1 = ((raw >> 3) & 0x1) > 0;
+		temp10TNK = ((raw >> 2) & 0x1) > 0;
+		temp11BAT2 = ((raw >> 1) & 0x1) > 0;
+		temp12MWM = (raw & 0x1) > 0;
 
 		raw = dis.readUnsignedByte();
-		TEMP13_MWR = ((raw >> 7) & 0x1) > 0;
-		TEMP14_MMM = ((raw >> 6) & 0x1) > 0;
-		TEMP15_MMR = ((raw >> 5) & 0x1) > 0;
+		temp13MWR = ((raw >> 7) & 0x1) > 0;
+		temp14MMM = ((raw >> 6) & 0x1) > 0;
+		temp15MMR = ((raw >> 5) & 0x1) > 0;
 	}
 
-	public boolean isTEMP1_PDU1() {
-		return TEMP1_PDU1;
+	public boolean isTemp1PDU1() {
+		return temp1PDU1;
 	}
 
-	public void setTEMP1_PDU1(boolean tEMP1_PDU1) {
-		TEMP1_PDU1 = tEMP1_PDU1;
+	public void setTemp1PDU1(boolean temp1pdu1) {
+		temp1PDU1 = temp1pdu1;
 	}
 
-	public boolean isTEMP2_BAT1() {
-		return TEMP2_BAT1;
+	public boolean isTemp2BAT1() {
+		return temp2BAT1;
 	}
 
-	public void setTEMP2_BAT1(boolean tEMP2_BAT1) {
-		TEMP2_BAT1 = tEMP2_BAT1;
+	public void setTemp2BAT1(boolean temp2bat1) {
+		temp2BAT1 = temp2bat1;
 	}
 
-	public boolean isTEMP3_PMB() {
-		return TEMP3_PMB;
+	public boolean isTemp3PMB() {
+		return temp3PMB;
 	}
 
-	public void setTEMP3_PMB(boolean tEMP3_PMB) {
-		TEMP3_PMB = tEMP3_PMB;
+	public void setTemp3PMB(boolean temp3pmb) {
+		temp3PMB = temp3pmb;
 	}
 
-	public boolean isTEMP4_HPA2() {
-		return TEMP4_HPA2;
+	public boolean isTemp4HPA2() {
+		return temp4HPA2;
 	}
 
-	public void setTEMP4_HPA2(boolean tEMP4_HPA2) {
-		TEMP4_HPA2 = tEMP4_HPA2;
+	public void setTemp4HPA2(boolean temp4hpa2) {
+		temp4HPA2 = temp4hpa2;
 	}
 
-	public boolean isTEMP8_HPA1() {
-		return TEMP8_HPA1;
+	public boolean isTemp8HPA1() {
+		return temp8HPA1;
 	}
 
-	public void setTEMP8_HPA1(boolean tEMP8_HPA1) {
-		TEMP8_HPA1 = tEMP8_HPA1;
+	public void setTemp8HPA1(boolean temp8hpa1) {
+		temp8HPA1 = temp8hpa1;
 	}
 
-	public boolean isTEMP10_TNK() {
-		return TEMP10_TNK;
+	public boolean isTemp10TNK() {
+		return temp10TNK;
 	}
 
-	public void setTEMP10_TNK(boolean tEMP10_TNK) {
-		TEMP10_TNK = tEMP10_TNK;
+	public void setTemp10TNK(boolean temp10tnk) {
+		temp10TNK = temp10tnk;
 	}
 
-	public boolean isTEMP11_BAT2() {
-		return TEMP11_BAT2;
+	public boolean isTemp11BAT2() {
+		return temp11BAT2;
 	}
 
-	public void setTEMP11_BAT2(boolean tEMP11_BAT2) {
-		TEMP11_BAT2 = tEMP11_BAT2;
+	public void setTemp11BAT2(boolean temp11bat2) {
+		temp11BAT2 = temp11bat2;
 	}
 
-	public boolean isTEMP12_MWM() {
-		return TEMP12_MWM;
+	public boolean isTemp12MWM() {
+		return temp12MWM;
 	}
 
-	public void setTEMP12_MWM(boolean tEMP12_MWM) {
-		TEMP12_MWM = tEMP12_MWM;
+	public void setTemp12MWM(boolean temp12mwm) {
+		temp12MWM = temp12mwm;
 	}
 
-	public boolean isTEMP13_MWR() {
-		return TEMP13_MWR;
+	public boolean isTemp13MWR() {
+		return temp13MWR;
 	}
 
-	public void setTEMP13_MWR(boolean tEMP13_MWR) {
-		TEMP13_MWR = tEMP13_MWR;
+	public void setTemp13MWR(boolean temp13mwr) {
+		temp13MWR = temp13mwr;
 	}
 
-	public boolean isTEMP14_MMM() {
-		return TEMP14_MMM;
+	public boolean isTemp14MMM() {
+		return temp14MMM;
 	}
 
-	public void setTEMP14_MMM(boolean tEMP14_MMM) {
-		TEMP14_MMM = tEMP14_MMM;
+	public void setTemp14MMM(boolean temp14mmm) {
+		temp14MMM = temp14mmm;
 	}
 
-	public boolean isTEMP15_MMR() {
-		return TEMP15_MMR;
+	public boolean isTemp15MMR() {
+		return temp15MMR;
 	}
 
-	public void setTEMP15_MMR(boolean tEMP15_MMR) {
-		TEMP15_MMR = tEMP15_MMR;
+	public void setTemp15MMR(boolean temp15mmr) {
+		temp15MMR = temp15mmr;
 	}
 
 }
