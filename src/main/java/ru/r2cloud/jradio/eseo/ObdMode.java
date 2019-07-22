32815be020ad180e3e3efbb2cb313c62e65088aa
dernasherbrezon <rodionovamp@mail.ru>
2019-07-22 08:34:36 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/eseo/ObdMode.java
+++ src/main/java/ru/r2cloud/jradio/eseo/ObdMode.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public enum ObdMode {
 
-	EGSE(0x00), OBDHPowerUp(0x01), AOCSInitialization(0x02), AOCSDamping(0x03), AOCSNominal(0x04), ReEntry(0xF0), SafeModeS1(0xF1), SafeModeS2(0xF2), SafeModeS3(0xF3), SafeModeS4(0xF4);
+	EGSE(0x00), OBDH_POWER_UP(0x01), AOCS_INITIALIZATION(0x02), AOCS_DAMPING(0x03), AOCS_NOMINAL(0x04), REENTRY(0xF0), SAFE_MODE_S1(0xF1), SAFE_MODE_S2(0xF2), SAFE_MODE_S3(0xF3), SAFE_MODE_S4(0xF4);
 
 	private final int code;
 	private static final Map<Integer, ObdMode> typeByCode = new HashMap<>();
