f2120203c902539050784702f61604bd185d9d16
dernasherbrezon <rodionovamp@mail.ru>
2019-08-14 10:42:01 +0100

    properly estimate number of samples during unpacked -> packed
    + handle non 8 number of unpacked bits

--- src/main/java/ru/r2cloud/jradio/blocks/UnpackedToPacked.java
+++ src/main/java/ru/r2cloud/jradio/blocks/UnpackedToPacked.java
@@ -1,5 +1,6 @@
 package ru.r2cloud.jradio.blocks;
 
+import java.io.EOFException;
 import java.io.IOException;
 
 import ru.r2cloud.jradio.ByteInput;
@@ -13,6 +14,8 @@ public class UnpackedToPacked implements ByteInput {
 	private final int bitsPerChunk;
 	private int index = 0;
 	private final Endianness endianness;
+	private final Context context;
+	private EOFException eofStreamReached = null;
 
 	public UnpackedToPacked(ByteInput input, int bitsPerChunk, Endianness endianness, Class<?> outputType) {
 		this.input = input;
@@ -24,6 +27,11 @@ public class UnpackedToPacked implements ByteInput {
 			throw new IllegalArgumentException("invalid type: " + outputType);
 		}
 		this.endianness = endianness;
+		this.context = new Context(input.getContext());
+		this.context.setSampleRate(this.context.getSampleRate() / 8);
+		if (this.context.getTotalSamples() != null) {
+			this.context.setTotalSamples((long) Math.ceil(this.context.getTotalSamples().doubleValue() / 8));
+		}
 	}
 
 	@Override
@@ -31,7 +39,18 @@ public class UnpackedToPacked implements ByteInput {
 		input.close();
 	}
 
-	private static int getBitBe1(byte x, int bitAddr, int bitsPerChunk) {
+	private int getBitBe1(int bitAddr, int bitsPerChunk) throws IOException {
+		byte x;
+		if (eofStreamReached == null) {
+			try {
+				x = input.readByte();
+			} catch (EOFException e) {
+				eofStreamReached = e;
+				x = 0;
+			}
+		} else {
+			x = 0;
+		}
 		int byteAddr = bitAddr / bitsPerChunk;
 		int residue = bitAddr - byteAddr * bitsPerChunk;
 		return (x >> (bitsPerChunk - 1 - residue)) & 1;
@@ -39,6 +58,9 @@ public class UnpackedToPacked implements ByteInput {
 
 	@Override
 	public byte readByte() throws IOException {
+		if (eofStreamReached != null) {
+			throw eofStreamReached;
+		}
 		int indexTmp = index;
 		byte result = 0;
 		Tag tag = null;
@@ -46,11 +68,11 @@ public class UnpackedToPacked implements ByteInput {
 		case GR_MSB_FIRST:
 			byte tmp = 0;
 			for (int j = 0; j < 8; j++) {
-				tmp = (byte) ((tmp << 1) | getBitBe1(input.readByte(), indexTmp, bitsPerChunk));
+				tmp = (byte) ((tmp << 1) | getBitBe1(indexTmp, bitsPerChunk));
 				indexTmp++;
 				// reduce length of message
 				if (j == 0) {
-					tag = getContext().getCurrent();
+					tag = input.getContext().getCurrent();
 					if (tag != null) {
 						tag.put(FixedLengthTagger.LENGTH, (Integer) tag.get(FixedLengthTagger.LENGTH) / 8);
 					}
@@ -61,11 +83,11 @@ public class UnpackedToPacked implements ByteInput {
 		case GR_LSB_FIRST:
 			long tmp2 = 0;
 			for (int j = 0; j < 8; j++) {
-				tmp2 = (tmp2 >> 1) | (getBitBe1(input.readByte(), indexTmp, bitsPerChunk) << (8 - 1));
+				tmp2 = (tmp2 >> 1) | (getBitBe1(indexTmp, bitsPerChunk) << (8 - 1));
 				indexTmp++;
 				// reduce length of message
 				if (j == 0) {
-					tag = getContext().getCurrent();
+					tag = input.getContext().getCurrent();
 					if (tag != null) {
 						tag.put(FixedLengthTagger.LENGTH, (Integer) tag.get(FixedLengthTagger.LENGTH) / 8);
 					}
@@ -76,14 +98,17 @@ public class UnpackedToPacked implements ByteInput {
 		default:
 			throw new IllegalArgumentException("unsupported endianness: " + endianness);
 		}
-		getContext().setCurrent(tag);
+		if (tag != null) {
+			context.put(tag.getId(), tag);
+		}
+		context.setCurrent(tag);
 		index = indexTmp;
 		return result;
 	}
 
 	@Override
 	public Context getContext() {
-		return input.getContext();
+		return context;
 	}
 
 	public static byte[] pack(byte[] hardDecisionBits, int offsetBits, int lengthBytes) {
@@ -95,7 +120,7 @@ public class UnpackedToPacked implements ByteInput {
 		}
 		return result;
 	}
-	
+
 	public static byte[] packSoft(byte[] softDecisionBits, int offsetBits, int lengthBytes) {
 		byte[] result = new byte[lengthBytes];
 		int max = offsetBits + lengthBytes * 8;
--- src/test/java/ru/r2cloud/jradio/UnpackedToPackedTest.java
+++ src/test/java/ru/r2cloud/jradio/UnpackedToPackedTest.java
@@ -1,7 +1,9 @@
 package ru.r2cloud.jradio;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import java.io.EOFException;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -25,6 +27,22 @@ public class UnpackedToPackedTest {
 			Tag tag = getFirst(source.getContext());
 			assertEquals(251, tag.get(FixedLengthTagger.LENGTH));
 		}
+	}
+
+	@Test
+	public void testTotalNumberOfSamples() throws Exception {
+		ArrayByteInput data = new ArrayByteInput(1, 0, 0, 0, 1, 1, 0, 1, 1);
+		UnpackedToPacked u2p = new UnpackedToPacked(data, 1, Endianness.GR_MSB_FIRST, Byte.class);
+		assertEquals((byte) 0b10001101, u2p.readByte());
+		assertEquals((byte) 0b10000000, u2p.readByte());
+		try {
+			u2p.readByte();
+			fail("expected eof");
+		} catch (EOFException e) {
+			// do nothing
+		}
+		assertEquals(2L, u2p.getContext().getTotalSamples().longValue());
+		u2p.close();
 	}
 
 	public static Tag getFirst(Context context) {
