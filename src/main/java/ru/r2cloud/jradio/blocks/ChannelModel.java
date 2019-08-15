770ed2e041331a4ec4f83554cba47393429e9276
dernasherbrezon <rodionovamp@mail.ru>
2019-08-15 19:27:29 +0100

    no-op channel model
    do not include blocks if they are no-op

--- src/main/java/ru/r2cloud/jradio/blocks/ChannelModel.java
+++ src/main/java/ru/r2cloud/jradio/blocks/ChannelModel.java
@@ -12,15 +12,22 @@ public class ChannelModel implements FloatInput {
 	private final FloatInput input;
 
 	public ChannelModel(FloatInput input, float noiseVoltage, float frequencyOffsetValue, float epsilon, float[] taps, long noiseSeed) {
-		FloatInput next = new FractionalResampler(input, 0.0f, epsilon);
-		if (taps != null) {
+		FloatInput next = input;
+		if (epsilon != 1.0) {
+			next = new FractionalResampler(next, 0.0f, epsilon);
+		}
+		if (taps != null && taps.length != 0) {
 			next = new FIRFilterBlockComplex(next, taps);
 		}
-		SigSource frequencyOffset = new SigSource(Waveform.COMPLEX, 1, frequencyOffsetValue, 1.0);
-		Multiply multiply = new Multiply(next, frequencyOffset);
-
-		FastNoiseSourceComplex noise = new FastNoiseSourceComplex(noiseVoltage, noiseSeed, 1024 * 16);
-		this.input = new Add(multiply, noise);
+		if (frequencyOffsetValue != 0.0) {
+			SigSource frequencyOffset = new SigSource(Waveform.COMPLEX, 1, frequencyOffsetValue, 1.0);
+			next = new Multiply(next, frequencyOffset);
+		}
+		if (noiseVoltage != 0.0) {
+			FastNoiseSourceComplex noise = new FastNoiseSourceComplex(noiseVoltage, noiseSeed, 1024 * 16);
+			next = new Add(next, noise);
+		}
+		this.input = next;
 	}
 
 	@Override
--- src/test/java/ru/r2cloud/jradio/blocks/ChannelModelTest.java
+++ src/test/java/ru/r2cloud/jradio/blocks/ChannelModelTest.java
@@ -1,7 +1,10 @@
 package ru.r2cloud.jradio.blocks;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
+import ru.r2cloud.jradio.ArrayFloatInput;
 import ru.r2cloud.jradio.Context;
 import ru.r2cloud.jradio.FloatInput;
 import ru.r2cloud.jradio.TestUtil;
@@ -16,5 +19,14 @@ public class ChannelModelTest {
 		try (FloatInput source = new ChannelModel(new InputStreamSource(ChannelModelTest.class.getClassLoader().getResourceAsStream("constellation_modulator_diff.bin"), context), 1.1f, 2.3f, 1.4f, new float[] { 1.2f, 1.1f, 0.0f, 0.0f }, 42L)) {
 			TestUtil.assertFloatInput("channel_model.bin", source);
 		}
+	}
+
+	@Test
+	public void testNoOpChannel() throws Exception {
+		ChannelModel model = new ChannelModel(new ArrayFloatInput(1.0f, 2.0f, 3.0f), 0.0f, 0.0f, 1.0f, null, 42);
+		assertEquals(1.0, model.readFloat(), 0.0);
+		assertEquals(2.0, model.readFloat(), 0.0);
+		assertEquals(3.0, model.readFloat(), 0.0);
+		model.close();
 	}
 }
