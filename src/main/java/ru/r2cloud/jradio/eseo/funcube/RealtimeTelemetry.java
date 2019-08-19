86ee8231068ded60b0d81d430d6202399c4623d2
dernasherbrezon <rodionovamp@mail.ru>
2019-08-19 12:55:11 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/eseo/funcube/RealtimeTelemetry.java
+++ src/main/java/ru/r2cloud/jradio/eseo/funcube/RealtimeTelemetry.java
@@ -6,72 +6,72 @@ import ru.r2cloud.jradio.util.BitInputStream;
 
 public class RealtimeTelemetry {
 
-	private float DCDCConverterOutputVoltage;
-	private float DCDCConverterOutputCurrent;
-	private float DCDCConverterTemperature;
+	private float dcdcConverterOutputVoltage;
+	private float dcdcConverterOutputCurrent;
+	private float dcdcConverterTemperature;
 	private float enclosureTemperature;
 	private float processorTemperature;
 	private float voltage33V;
 	private float current33V;
-	private float Transponder69VVoltage;
-	private float Transponder69VCurrent;
+	private float transponder69VVoltage;
+	private float transponder69VCurrent;
 	private float voltage9V;
 	private float current9V;
 	private float transmitterForwardPower;
 	private float transmitterReflectedPower;
-	private float FMPowerAmplifierTemperature;
-	private float BPSKpowerAmplifierTemperature;
-	private float BPSKpowerAmplifierCurrent;
+	private float fmPowerAmplifierTemperature;
+	private float bpskPowerAmplifierTemperature;
+	private float bpskPowerAmplifierCurrent;
 	private float supplyCurrent33V;
 	private float transponderReceiverRSSI;
 	private float commandReceiverRSSI;
 	private float commandReceiverDoppler;
-	private float CMDReceiverOscillatorTemperature;
+	private float cmdReceiverOscillatorTemperature;
 	private int sequenceNumber;
 	private int lastCommand;
 	private RFMode mode;
 	private DataMode dataMode;
 	private PayloadTransferStatus transferStatus;
-	private boolean ESEOEclipseState;
+	private boolean eseoEclipseState;
 	private boolean autonomousModeState;
-	private boolean CTCSSDetectState;
+	private boolean ctcssDetectState;
 	private boolean safeModeState;
 	private boolean inSafeMode;
 	private NonPayloadModeOBC nonPayloadObc;
 	private PayloadModeOBC payloadObc;
 
 	public RealtimeTelemetry(BitInputStream dis) throws IOException {
-		DCDCConverterOutputVoltage = 0.100f * dis.readUnsignedByte();
-		DCDCConverterOutputCurrent = 5.131579f * dis.readUnsignedByte();
-		DCDCConverterTemperature = -0.7796212f * dis.readUnsignedByte() + 98.19402f;
+		dcdcConverterOutputVoltage = 0.100f * dis.readUnsignedByte();
+		dcdcConverterOutputCurrent = 5.131579f * dis.readUnsignedByte();
+		dcdcConverterTemperature = -0.7796212f * dis.readUnsignedByte() + 98.19402f;
 		enclosureTemperature = -0.7385868f * dis.readUnsignedByte() + 97.74249f;
 		processorTemperature = -0.7725984f * dis.readUnsignedByte() + 94.95152f;
 		voltage33V = 0.031141509f * dis.readUnsignedByte();
 		current33V = 3.75f * dis.readUnsignedByte();
-		Transponder69VVoltage = 0.0885753425f * dis.readUnsignedByte();
-		Transponder69VCurrent = (dis.readUnsignedByte() - 2) * 2.5941175f;
+		transponder69VVoltage = 0.0885753425f * dis.readUnsignedByte();
+		transponder69VCurrent = (dis.readUnsignedByte() - 2) * 2.5941175f;
 		voltage9V = 0.088171498f * dis.readUnsignedByte();
 		current9V = 2.52778f * dis.readUnsignedByte();
 		int raw = dis.readUnsignedByte();
 		transmitterForwardPower = 0.0136f * raw * raw + 0.4995f * raw + 1E-12f;
 		raw = dis.readUnsignedByte();
 		transmitterReflectedPower = 0.0136f * raw * raw + 0.4995f * raw + 1E-12f;
-		FMPowerAmplifierTemperature = -0.789929f * dis.readUnsignedByte() + 97.5934f;
-		BPSKpowerAmplifierTemperature = -0.8104347f * dis.readUnsignedByte() + 91.93637f;
-		BPSKpowerAmplifierCurrent = 2.18f * dis.readUnsignedByte();
+		fmPowerAmplifierTemperature = -0.789929f * dis.readUnsignedByte() + 97.5934f;
+		bpskPowerAmplifierTemperature = -0.8104347f * dis.readUnsignedByte() + 91.93637f;
+		bpskPowerAmplifierCurrent = 2.18f * dis.readUnsignedByte();
 		supplyCurrent33V = 0.8f * dis.readUnsignedByte();
 		transponderReceiverRSSI = LookupTableConverter.getTransponderRSSI(dis.readUnsignedByte());
 		commandReceiverRSSI = LookupTableConverter.getCommandRSSI(dis.readUnsignedByte());
 		commandReceiverDoppler = LookupTableConverter.getCommandDoppler(dis.readUnsignedByte());
-		CMDReceiverOscillatorTemperature = -0.8592393f * dis.readUnsignedByte() + 94.30121f;
+		cmdReceiverOscillatorTemperature = -0.8592393f * dis.readUnsignedByte() + 94.30121f;
 		sequenceNumber = dis.readUnsignedInt(24);
 		lastCommand = dis.readUnsignedByte();
 		mode = RFMode.valueOfCode(dis.readUnsignedInt(3));
 		dataMode = DataMode.valueOfCode(dis.readUnsignedInt(2));
 		transferStatus = PayloadTransferStatus.valueOfCode(dis.readUnsignedInt(1));
-		ESEOEclipseState = dis.readBoolean();
+		eseoEclipseState = dis.readBoolean();
 		autonomousModeState = dis.readBoolean();
-		CTCSSDetectState = dis.readBoolean();
+		ctcssDetectState = dis.readBoolean();
 		safeModeState = dis.readBoolean();
 		inSafeMode = dis.readBoolean();
 		switch (dataMode) {
@@ -87,27 +87,27 @@ public class RealtimeTelemetry {
 	}
 
 	public float getDCDCConverterOutputVoltage() {
-		return DCDCConverterOutputVoltage;
+		return dcdcConverterOutputVoltage;
 	}
 
 	public void setDCDCConverterOutputVoltage(float dCDCConverterOutputVoltage) {
-		DCDCConverterOutputVoltage = dCDCConverterOutputVoltage;
+		dcdcConverterOutputVoltage = dCDCConverterOutputVoltage;
 	}
 
 	public float getDCDCConverterOutputCurrent() {
-		return DCDCConverterOutputCurrent;
+		return dcdcConverterOutputCurrent;
 	}
 
 	public void setDCDCConverterOutputCurrent(float dCDCConverterOutputCurrent) {
-		DCDCConverterOutputCurrent = dCDCConverterOutputCurrent;
+		dcdcConverterOutputCurrent = dCDCConverterOutputCurrent;
 	}
 
 	public float getDCDCConverterTemperature() {
-		return DCDCConverterTemperature;
+		return dcdcConverterTemperature;
 	}
 
 	public void setDCDCConverterTemperature(float dCDCConverterTemperature) {
-		DCDCConverterTemperature = dCDCConverterTemperature;
+		dcdcConverterTemperature = dCDCConverterTemperature;
 	}
 
 	public float getEnclosureTemperature() {
@@ -143,19 +143,19 @@ public class RealtimeTelemetry {
 	}
 
 	public float getTransponder69VVoltage() {
-		return Transponder69VVoltage;
+		return transponder69VVoltage;
 	}
 
 	public void setTransponder69VVoltage(float transponder69vVoltage) {
-		Transponder69VVoltage = transponder69vVoltage;
+		transponder69VVoltage = transponder69vVoltage;
 	}
 
 	public float getTransponder69VCurrent() {
-		return Transponder69VCurrent;
+		return transponder69VCurrent;
 	}
 
 	public void setTransponder69VCurrent(float transponder69vCurrent) {
-		Transponder69VCurrent = transponder69vCurrent;
+		transponder69VCurrent = transponder69vCurrent;
 	}
 
 	public float getVoltage9V() {
@@ -191,27 +191,27 @@ public class RealtimeTelemetry {
 	}
 
 	public float getFMPowerAmplifierTemperature() {
-		return FMPowerAmplifierTemperature;
+		return fmPowerAmplifierTemperature;
 	}
 
 	public void setFMPowerAmplifierTemperature(float fMPowerAmplifierTemperature) {
-		FMPowerAmplifierTemperature = fMPowerAmplifierTemperature;
+		fmPowerAmplifierTemperature = fMPowerAmplifierTemperature;
 	}
 
 	public float getBPSKpowerAmplifierTemperature() {
-		return BPSKpowerAmplifierTemperature;
+		return bpskPowerAmplifierTemperature;
 	}
 
 	public void setBPSKpowerAmplifierTemperature(float bPSKpowerAmplifierTemperature) {
-		BPSKpowerAmplifierTemperature = bPSKpowerAmplifierTemperature;
+		bpskPowerAmplifierTemperature = bPSKpowerAmplifierTemperature;
 	}
 
 	public float getBPSKpowerAmplifierCurrent() {
-		return BPSKpowerAmplifierCurrent;
+		return bpskPowerAmplifierCurrent;
 	}
 
 	public void setBPSKpowerAmplifierCurrent(float bPSKpowerAmplifierCurrent) {
-		BPSKpowerAmplifierCurrent = bPSKpowerAmplifierCurrent;
+		bpskPowerAmplifierCurrent = bPSKpowerAmplifierCurrent;
 	}
 
 	public float getSupplyCurrent33V() {
@@ -247,11 +247,11 @@ public class RealtimeTelemetry {
 	}
 
 	public float getCMDReceiverOscillatorTemperature() {
-		return CMDReceiverOscillatorTemperature;
+		return cmdReceiverOscillatorTemperature;
 	}
 
 	public void setCMDReceiverOscillatorTemperature(float cMDReceiverOscillatorTemperature) {
-		CMDReceiverOscillatorTemperature = cMDReceiverOscillatorTemperature;
+		cmdReceiverOscillatorTemperature = cMDReceiverOscillatorTemperature;
 	}
 
 	public int getSequenceNumber() {
@@ -295,11 +295,11 @@ public class RealtimeTelemetry {
 	}
 
 	public boolean isESEOEclipseState() {
-		return ESEOEclipseState;
+		return eseoEclipseState;
 	}
 
 	public void setESEOEclipseState(boolean eSEOEclipseState) {
-		ESEOEclipseState = eSEOEclipseState;
+		eseoEclipseState = eSEOEclipseState;
 	}
 
 	public boolean isAutonomousModeState() {
@@ -311,11 +311,11 @@ public class RealtimeTelemetry {
 	}
 
 	public boolean isCTCSSDetectState() {
-		return CTCSSDetectState;
+		return ctcssDetectState;
 	}
 
 	public void setCTCSSDetectState(boolean cTCSSDetectState) {
-		CTCSSDetectState = cTCSSDetectState;
+		ctcssDetectState = cTCSSDetectState;
 	}
 
 	public boolean isSafeModeState() {
