62a1cd57454c1b6ec9c6a4733f341d13a83fe2f4
dernasherbrezon <rodionovamp@mail.ru>
2019-08-26 14:32:57 +0100

    skip xlating filter if there is no offset

--- src/main/java/ru/r2cloud/jradio/demod/BpskDemodulator.java
+++ src/main/java/ru/r2cloud/jradio/demod/BpskDemodulator.java
@@ -25,16 +25,19 @@ public class BpskDemodulator implements ByteInput {
 
 	// produces soft stream of bytes
 	public BpskDemodulator(FloatInput input, int symbolRate, int decimation, double centerFrequency, float bandwidth, boolean differential) {
-		float[] taps = Firdes.lowPass(1.0, input.getContext().getSampleRate(), symbolRate * LOW_PASS_THRESHOLD, 1000, Window.WIN_HAMMING, 6.76);
-		FrequencyXlatingFIRFilter xlating = new FrequencyXlatingFIRFilter(input, taps, decimation, centerFrequency);
-		RmsAgc agc = new RmsAgc(xlating, 1e-2f, 0.5f);
+		FloatInput next = input;
+		if (centerFrequency != 0.0) {
+			float[] taps = Firdes.lowPass(1.0, next.getContext().getSampleRate(), symbolRate * LOW_PASS_THRESHOLD, 1000, Window.WIN_HAMMING, 6.76);
+			next = new FrequencyXlatingFIRFilter(next, taps, decimation, centerFrequency);
+		}
+		RmsAgc agc = new RmsAgc(next, 1e-2f, 0.5f);
 		float samplesPerSymbol = agc.getContext().getSampleRate() / symbolRate;
 		FLLBandEdge fll = new FLLBandEdge(agc, samplesPerSymbol, 0.35f, 100, 0.01f);
 		LowPassFilterComplex lpf = new LowPassFilterComplex(fll, 1.0, bandwidth, 500, Window.WIN_HAMMING, 6.76);
 		int nfilts = 16;
 		float[] rrcTaps = Firdes.rootRaisedCosine(nfilts, nfilts, 1.0f / samplesPerSymbol, 0.35f, (int) (11 * samplesPerSymbol * nfilts));
 		PolyphaseClockSyncComplex clock = new PolyphaseClockSyncComplex(lpf, samplesPerSymbol, 0.1f, rrcTaps, nfilts, nfilts / 2, 1.5f, 1);
-		FloatInput next = new CostasLoop(clock, 0.1f, 2, false);
+		next = new CostasLoop(clock, 0.1f, 2, false);
 		if (differential) {
 			next = new DelayOne(next);
 		}
