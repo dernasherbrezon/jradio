79d0b2746c2733e6ae9c56f504d8df67a917e730
dernasherbrezon <rodionovamp@mail.ru>
2019-09-02 07:46:22 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/fec/rs/bch/ReedSolomon.java
+++ src/main/java/ru/r2cloud/jradio/fec/rs/bch/ReedSolomon.java
@@ -13,81 +13,80 @@ public class ReedSolomon {
 	private int[] gexp = new int[512];
 	private int[] glog = new int[256];
 
-	private int MAXDEG;
+	private int maxdeg;
 	private int[] genPoly;
-	private int NPAR;
+	private int npar;
 
 	public ReedSolomon(int parityBytes) {
 		initExpTable();
-		this.NPAR = parityBytes;
-		MAXDEG = NPAR * 2;
-		genPoly = new int[MAXDEG * 2];
-		compute_genpoly();
+		this.npar = parityBytes;
+		maxdeg = npar * 2;
+		genPoly = new int[maxdeg * 2];
+		computeGenpoly();
 	}
 
-	private void compute_genpoly() {
+	private void computeGenpoly() {
 		int i = 0;
 		int[] tp = new int[256];
 		int[] tp1 = new int[256];
 
 		/* multiply (x + a^n) for n = 1 to nbytes */
 
-		zero_poly(tp1);
+		zeroPoly(tp1);
 		tp1[0] = 1;
 
-		for (i = 1; i <= NPAR; i++) {
-			zero_poly(tp);
+		for (i = 1; i <= npar; i++) {
+			zeroPoly(tp);
 			tp[0] = gexp[i]; /* set up x+a^n */
 			tp[1] = 1;
 
-			mult_polys(genPoly, tp, tp1);
-			copy_poly(tp1, genPoly);
+			multPolys(genPoly, tp, tp1);
+			copyPoly(tp1, genPoly);
 		}
 	}
 
-	void zero_poly(int poly[]) {
+	void zeroPoly(int[] poly) {
 		int i;
-		for (i = 0; i < MAXDEG; i++) {
+		for (i = 0; i < maxdeg; i++) {
 			poly[i] = 0;
 		}
 	}
 
-	void copy_poly(int dst[], int src[]) {
-		int i;
-		for (i = 0; i < MAXDEG; i++) {
-			dst[i] = src[i];
-		}
+	void copyPoly(int[] dst, int[] src) {
+		System.arraycopy(src, 0, dst, 0, maxdeg);
 	}
 
 	/* polynomial multiplication */
-	void mult_polys(int dst[], int p1[], int p2[]) {
-		int i, j;
-		int[] tmp1 = new int[MAXDEG * 2];
+	void multPolys(int[] dst, int[] p1, int[] p2) {
+		int i;
+		int j;
+		int[] tmp1 = new int[maxdeg * 2];
 
-		for (i = 0; i < (MAXDEG * 2); i++)
+		for (i = 0; i < (maxdeg * 2); i++)
 			dst[i] = 0;
 
-		for (i = 0; i < MAXDEG; i++) {
-			for (j = MAXDEG; j < (MAXDEG * 2); j++)
+		for (i = 0; i < maxdeg; i++) {
+			for (j = maxdeg; j < (maxdeg * 2); j++)
 				tmp1[j] = 0;
 
 			/* scale tmp1 by p1[i] */
-			for (j = 0; j < MAXDEG; j++)
+			for (j = 0; j < maxdeg; j++)
 				tmp1[j] = gmult(p2[j], p1[i]);
 			/* and mult (shift) tmp1 right by i */
-			for (j = (MAXDEG * 2) - 1; j >= i; j--)
+			for (j = (maxdeg * 2) - 1; j >= i; j--)
 				tmp1[j] = tmp1[j - i];
 			for (j = 0; j < i; j++)
 				tmp1[j] = 0;
 
 			/* add into partial product */
-			for (j = 0; j < (MAXDEG * 2); j++)
+			for (j = 0; j < (maxdeg * 2); j++)
 				dst[j] ^= tmp1[j];
 		}
 	}
 
 	int gmult(int a, int b) {
-		int i, j;
+		int i;
+		int j;
 		if (a == 0 || b == 0) {
 			return (0);
 		}
@@ -97,11 +96,17 @@ public class ReedSolomon {
 	}
 
 	private void initExpTable() {
-		int i, z;
-		int pinit, p1, p2, p3, p4, p5, p6, p7, p8;
-
-		pinit = p2 = p3 = p4 = p5 = p6 = p7 = p8 = 0;
-		p1 = 1;
+		int i;
+		int z;
+		int pinit = 0;
+		int p1 = 1;
+		int p2 = 0;
+		int p3 = 0;
+		int p4 = 0;
+		int p5 = 0;
+		int p6 = 0;
+		int p7 = 0;
+		int p8 = 0;
 
 		gexp[0] = 1;
 		gexp[255] = gexp[0];
@@ -133,91 +138,89 @@ public class ReedSolomon {
 
 	public byte[] encode(byte[] data) {
 		int i = 0;
-		int[] LFSR = new int[NPAR + 1];
+		int[] lfsr = new int[npar + 1];
 		int dbyte = 0;
 		int j = 0;
 
-		for (i = 0; i < NPAR + 1; i++) {
-			LFSR[i] = 0;
+		for (i = 0; i < npar + 1; i++) {
+			lfsr[i] = 0;
 		}
 
 		for (i = 0; i < data.length; i++) {
-			dbyte = data[i] ^ LFSR[NPAR - 1];
-			for (j = NPAR - 1; j > 0; j--) {
-				LFSR[j] = LFSR[j - 1] ^ gmult(genPoly[j], dbyte);
+			dbyte = data[i] ^ lfsr[npar - 1];
+			for (j = npar - 1; j > 0; j--) {
+				lfsr[j] = lfsr[j - 1] ^ gmult(genPoly[j], dbyte);
 			}
-			LFSR[0] = gmult(genPoly[0], dbyte);
+			lfsr[0] = gmult(genPoly[0], dbyte);
 		}
 
-		int[] pBytes = new int[MAXDEG];
-		for (i = 0; i < NPAR; i++) {
-			pBytes[i] = LFSR[i];
-		}
-
+		int[] pBytes = new int[maxdeg];
+		System.arraycopy(lfsr, 0, pBytes, 0, npar);
 		return buildCodeword(data, pBytes);
 	}
 
 	private byte[] buildCodeword(byte[] data, int[] pBytes) {
 		int i;
-		byte[] result = new byte[data.length + NPAR];
+		byte[] result = new byte[data.length + npar];
+		System.arraycopy(data, 0, result, 0, data.length);
 
-		for (i = 0; i < data.length; i++) {
-			result[i] = data[i];
-		}
-
-		for (i = 0; i < NPAR; i++) {
-			result[i + data.length] = (byte) pBytes[NPAR - 1 - i];
+		for (i = 0; i < npar; i++) {
+			result[i + data.length] = (byte) pBytes[npar - 1 - i];
 		}
 		return result;
 	}
 
 	public byte[] decode(byte[] data) throws UncorrectableException {
-		int[] synBytes = new int[MAXDEG];
-		int i, j, sum;
-		for (j = 0; j < NPAR; j++) {
+		int[] synBytes = new int[maxdeg];
+		int i;
+		int j;
+		int sum;
+		for (j = 0; j < npar; j++) {
 			sum = 0;
 			for (i = 0; i < data.length; i++) {
 				sum = (data[i] & 0xFF) ^ gmult(gexp[j + 1], sum);
 			}
 			synBytes[j] = sum;
 		}
-		if (check_syndrome(synBytes) != 0) {
-			if (correct_errors_erasures(data, synBytes) != 1) {
-				throw new UncorrectableException("uncorrectable");
-			}
+		if (checkSyndrome(synBytes) != 0 && correctErrorsErasures(data, synBytes) != 1) {
+			throw new UncorrectableException("uncorrectable");
 		}
-		return Arrays.copyOfRange(data, 0, data.length - NPAR);
+		return Arrays.copyOfRange(data, 0, data.length - npar);
 	}
 
-	private int correct_errors_erasures(byte[] codeword, int[] synBytes) {
-		int r, i, j, err;
-		int[] Lambda = new int[MAXDEG];
-		int[] Omega = new int[MAXDEG];
-		int[] ErrorLocs = new int[256];
-		Modified_Berlekamp_Massey(Lambda, Omega, synBytes);
-		int NErrors = Find_Roots(Lambda, ErrorLocs);
-		if ((NErrors <= NPAR) && NErrors > 0) {
+	private int correctErrorsErasures(byte[] codeword, int[] synBytes) {
+		int r;
+		int i;
+		int j;
+		int err;
+		int[] lambda = new int[maxdeg];
+		int[] omega = new int[maxdeg];
+		int[] errorLocs = new int[256];
+		modifiedBerlekampMassey(lambda, omega, synBytes);
+		int nerrors = findRoots(lambda, errorLocs);
+		if ((nerrors <= npar) && nerrors > 0) {
 
 			/* first check for illegal error locs */
-			for (r = 0; r < NErrors; r++) {
-				if (ErrorLocs[r] >= codeword.length) {
+			for (r = 0; r < nerrors; r++) {
+				if (errorLocs[r] >= codeword.length) {
 					return (0);
 				}
 			}
 
-			for (r = 0; r < NErrors; r++) {
-				int num, denom;
-				i = ErrorLocs[r];
+			for (r = 0; r < nerrors; r++) {
+				int num;
+				int denom;
+				i = errorLocs[r];
 				/* evaluate Omega at alpha^(-i) */
 
 				num = 0;
-				for (j = 0; j < MAXDEG; j++)
-					num ^= gmult(Omega[j], gexp[((255 - i) * j) % 255]);
+				for (j = 0; j < maxdeg; j++)
+					num ^= gmult(omega[j], gexp[((255 - i) * j) % 255]);
 
 				/* evaluate Lambda' (derivative) at alpha^(-i) ; all odd powers disappear */
 				denom = 0;
-				for (j = 1; j < MAXDEG; j += 2) {
-					denom ^= gmult(Lambda[j], gexp[((255 - i) * (j - 1)) % 255]);
+				for (j = 1; j < maxdeg; j += 2) {
+					denom ^= gmult(lambda[j], gexp[((255 - i) * (j - 1)) % 255]);
 				}
 
 				err = gmult(num, ginv(denom));
@@ -229,88 +232,91 @@ public class ReedSolomon {
 		}
 	}
 
-	private void Modified_Berlekamp_Massey(int[] Lambda, int[] Omega, int[] synBytes) {
-		int n, L, L2, k, d, i;
-		int[] psi = new int[MAXDEG];
-		int[] psi2 = new int[MAXDEG];
-		int[] D = new int[MAXDEG];
-		int[] gamma = new int[MAXDEG];
+	private void modifiedBerlekampMassey(int[] lambda, int[] omega, int[] synBytes) {
+		int n;
+		int L;
+		int L2;
+		int k;
+		int d;
+		int i;
+		int[] psi = new int[maxdeg];
+		int[] psi2 = new int[maxdeg];
+		int[] localD = new int[maxdeg];
+		int[] gamma = new int[maxdeg];
 
 		/* initialize Gamma, the erasure locator polynomial */
-		init_gamma(gamma);
+		initGamma(gamma);
 
 		/* initialize to z */
-		copy_poly(D, gamma);
-		mul_z_poly(D);
+		copyPoly(localD, gamma);
+		mulZPoly(localD);
 
-		copy_poly(psi, gamma);
+		copyPoly(psi, gamma);
 		k = -1;
 		L = 0;
 
-		for (n = 0; n < NPAR; n++) {
+		for (n = 0; n < npar; n++) {
 
-			d = compute_discrepancy(psi, synBytes, L, n);
+			d = computeDiscrepancy(psi, synBytes, L, n);
 
 			if (d != 0) {
 
 				/* psi2 = psi - d*D */
-				for (i = 0; i < MAXDEG; i++)
-					psi2[i] = psi[i] ^ gmult(d, D[i]);
+				for (i = 0; i < maxdeg; i++) {
+					psi2[i] = psi[i] ^ gmult(d, localD[i]);
+				}
 
 				if (L < (n - k)) {
 					L2 = n - k;
 					k = n - L;
 					/* D = scale_poly(ginv(d), psi); */
-					for (i = 0; i < MAXDEG; i++)
-						D[i] = gmult(psi[i], ginv(d));
+					for (i = 0; i < maxdeg; i++) {
+						localD[i] = gmult(psi[i], ginv(d));
+					}
 					L = L2;
 				}
 
 				/* psi = psi2 */
-				for (i = 0; i < MAXDEG; i++)
-					psi[i] = psi2[i];
+				System.arraycopy(psi2, 0, psi, 0, maxdeg);
 			}
 
-			mul_z_poly(D);
+			mulZPoly(localD);
 		}
 
-		for (i = 0; i < MAXDEG; i++) {
-			Lambda[i] = psi[i];
-		}
-		compute_modified_omega(Lambda, Omega, synBytes);
+		System.arraycopy(psi, 0, lambda, 0, maxdeg);
+		computeModifiedOmega(lambda, omega, synBytes);
 
 	}
 
-	private int Find_Roots(int[] Lambda, int[] ErrorLocs) {
-		int sum, r, k;
-		int NErrors = 0;
+	private int findRoots(int[] lambda, int[] errorLocs) {
+		int sum;
+		int r;
+		int k;
+		int nerrors = 0;
 
 		for (r = 1; r < 256; r++) {
 			sum = 0;
 			/* evaluate lambda at r */
-			for (k = 0; k < NPAR + 1; k++) {
-				sum ^= gmult(gexp[(k * r) % 255], Lambda[k]);
+			for (k = 0; k < npar + 1; k++) {
+				sum ^= gmult(gexp[(k * r) % 255], lambda[k]);
 			}
 			if (sum == 0) {
-				ErrorLocs[NErrors] = (255 - r);
-				NErrors++;
+				errorLocs[nerrors] = (255 - r);
+				nerrors++;
 			}
 		}
-		return NErrors;
+		return nerrors;
 	}
 
 	/*
 	 * given Psi (called Lambda in Modified_Berlekamp_Massey) and synBytes, compute the combined erasure/error evaluator polynomial as Psi*S mod z^4
 	 */
-	private void compute_modified_omega(int[] Lambda, int[] Omega, int[] synBytes) {
-		int i;
-		int[] product = new int[MAXDEG * 2];
+	private void computeModifiedOmega(int[] lambda, int[] omega, int[] synBytes) {
+		int[] product = new int[maxdeg * 2];
 
-		mult_polys(product, Lambda, synBytes);
-		zero_poly(Omega);
-		for (i = 0; i < NPAR; i++) {
-			Omega[i] = product[i];
-		}
+		multPolys(product, lambda, synBytes);
+		zeroPoly(omega);
+		System.arraycopy(product, 0, omega, 0, npar);
 	}
 
 	private int ginv(int elt) {
@@ -318,33 +324,35 @@ public class ReedSolomon {
 	}
 
 	/* gamma = product (1-z*a^Ij) for erasure locs Ij */
-	private void init_gamma(int gamma[]) {
-		int[] tmp = new int[MAXDEG];
-		zero_poly(gamma);
-		zero_poly(tmp);
+	private void initGamma(int[] gamma) {
+		int[] tmp = new int[maxdeg];
+		zeroPoly(gamma);
+		zeroPoly(tmp);
 		gamma[0] = 1;
 	}
 
-	private int compute_discrepancy(int lambda[], int S[], int L, int n) {
-		int i, sum = 0;
+	private int computeDiscrepancy(int[] lambda, int[] s, int l, int n) {
+		int i;
+		int sum = 0;
 
-		for (i = 0; i <= L; i++) {
-			sum ^= gmult(lambda[i], S[n - i]);
+		for (i = 0; i <= l; i++) {
+			sum ^= gmult(lambda[i], s[n - i]);
 		}
 		return (sum);
 	}
 
-	private void mul_z_poly(int src[]) {
+	private void mulZPoly(int[] src) {
 		int i;
-		for (i = MAXDEG - 1; i > 0; i--) {
+		for (i = maxdeg - 1; i > 0; i--) {
 			src[i] = src[i - 1];
 		}
 		src[0] = 0;
 	}
 
-	private int check_syndrome(int[] synBytes) {
-		int i, nz = 0;
-		for (i = 0; i < NPAR; i++) {
+	private int checkSyndrome(int[] synBytes) {
+		int i;
+		int nz = 0;
+		for (i = 0; i < npar; i++) {
 			if (synBytes[i] != 0) {
 				nz = 1;
 				break;
