1866ae1f685ea1ab1f129419ea33747880ff3312
dernasherbrezon <rodionovamp@mail.ru>
2019-09-06 07:36:28 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/fec/ccsds/Randomize.java
+++ src/main/java/ru/r2cloud/jradio/fec/ccsds/Randomize.java
@@ -2,7 +2,7 @@ package ru.r2cloud.jradio.fec.ccsds;
 
 public class Randomize {
 
-	private static final int[] sequence = new int[255];
+	private static final int[] SEQUENCE = new int[255];
 
 	static {
 		int[] x = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
@@ -11,8 +11,8 @@ public class Randomize {
 		/*
 		 * The pseudo random sequence shall be generated using the polynomial h(x) = x8 + x7 + x5 + x3 + 1
 		 */
-		for (i = 0; i < sequence.length * 8; i++) {
-			sequence[i / 8] = sequence[i / 8] | x[1] << 7 >> (i % 8);
+		for (i = 0; i < SEQUENCE.length * 8; i++) {
+			SEQUENCE[i / 8] = SEQUENCE[i / 8] | x[1] << 7 >> (i % 8);
 			x[0] = (x[8] + x[6] + x[4] + x[1]) % 2;
 			x[1] = x[2];
 			x[2] = x[3];
@@ -27,8 +27,12 @@ public class Randomize {
 
 	public static void shuffle(byte[] data) {
 		for (int i = 0; i < data.length; i++) {
-			data[i] = (byte) (data[i] ^ sequence[i % sequence.length]);
+			data[i] = (byte) (data[i] ^ SEQUENCE[i % SEQUENCE.length]);
 		}
+	}
+
+	private Randomize() {
+		// do nothing
 	}
 
 }
