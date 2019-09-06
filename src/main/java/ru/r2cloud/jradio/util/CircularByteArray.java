7e95889a777da0ebf7ae6d235983cf05fd97e6c9
dernasherbrezon <rodionovamp@mail.ru>
2019-09-06 19:01:55 +0100

    circular byte array

---
+++ src/main/java/ru/r2cloud/jradio/util/CircularByteArray.java
@@ -0,0 +1,44 @@
+package ru.r2cloud.jradio.util;
+
+public class CircularByteArray {
+
+	private final byte[] array;
+	private int currentPos;
+
+	public CircularByteArray(int size) {
+		array = new byte[size];
+		currentPos = array.length;
+	}
+
+	public void add(byte value) {
+		currentPos--;
+		if (currentPos < 0) {
+			currentPos = array.length - 1;
+		}
+		array[currentPos] = value;
+	}
+
+	public byte get(int index) {
+		if (index >= array.length) {
+			throw new ArrayIndexOutOfBoundsException(index);
+		}
+		int resultIndex = currentPos - index - 1;
+		if (resultIndex < 0) {
+			resultIndex = resultIndex + array.length;
+		}
+		return array[resultIndex];
+	}
+
+	public byte[] getArray() {
+		return array;
+	}
+
+	public int getCurrentPos() {
+		return currentPos;
+	}
+
+	public int getSize() {
+		return array.length;
+	}
+
+}
---
+++ src/test/java/ru/r2cloud/jradio/util/CircularByteArrayTest.java
@@ -0,0 +1,33 @@
+package ru.r2cloud.jradio.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CircularByteArrayTest {
+
+	@Test
+	public void testGetByIndex() {
+		CircularByteArray array = new CircularByteArray(3);
+		array.add((byte) 1);
+		assertEquals(0, array.get(0));
+		assertEquals(0, array.get(1));
+		assertEquals(1, array.get(2));
+		array.add((byte) 2);
+		assertEquals(0, array.get(0));
+		assertEquals(1, array.get(1));
+		assertEquals(2, array.get(2));
+		array.add((byte) 3);
+		assertEquals(1, array.get(0));
+		assertEquals(2, array.get(1));
+		assertEquals(3, array.get(2));
+	}
+
+	@Test(expected = ArrayIndexOutOfBoundsException.class)
+	public void testArrayIndexOutOfBounds() {
+		CircularByteArray array = new CircularByteArray(3);
+		array.add((byte) 1);
+		array.get(3);
+	}
+
+}
