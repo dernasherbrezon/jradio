e0952f52568b0c291aa2d6452a820a4b5058f5dd
dernasherbrezon <rodionovamp@mail.ru>
2019-09-09 07:15:49 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/gomx1/TypeB.java
+++ src/main/java/ru/r2cloud/jradio/gomx1/TypeB.java
@@ -10,14 +10,14 @@ public class TypeB {
 	private float[] xest; // State vector [_i^cq ^c\omega b_mag b_gyro \tau] in total 16 states
 	private float[] zfilt; // Filt meas vector [v_sun v_mag omega_gyro]
 	private int enableVector; // Enable vector (bit mask) for meas vector [xsunsensor,3xmag,3xgyro]
-	private float[] ref_q; // Control ref
-	private float[] err_q; // control err
-	private float[] ierr_q; // Control ierr
-	private float[] err_rate; // Control err rate
-	private float[] sc_reci; // Ephem satellite pos
-	private float[] sun_eci; // Ephem sun pos
-	private float[] mag_eci; // Ephem mag
-	
+	private float[] refQ; // Control ref
+	private float[] errQ; // control err
+	private float[] ierrQ; // Control ierr
+	private float[] errRate; // Control err rate
+	private float[] scReci; // Ephem satellite pos
+	private float[] sunEci; // Ephem sun pos
+	private float[] magEci; // Ephem mag
+
 	public TypeB() {
 		// do nothing
 	}
@@ -31,13 +31,21 @@ public class TypeB {
 		xest = readFloat(dis, 16);
 		zfilt = readFloat(dis, 9);
 		enableVector = dis.readUnsignedShort();
-		ref_q = readFloat(dis, 4);
-		err_q = readFloat(dis, 4);
-		ierr_q = readFloat(dis, 3);
-		err_rate = readFloat(dis, 3);
-		sc_reci = readFloat(dis, 3);
-		sun_eci = readFloat(dis, 3);
-		mag_eci = readFloat(dis, 3);
+		refQ = readFloat(dis, 4);
+		errQ = readFloat(dis, 4);
+		ierrQ = readFloat(dis, 3);
+		errRate = readFloat(dis, 3);
+		scReci = readFloat(dis, 3);
+		sunEci = readFloat(dis, 3);
+		magEci = readFloat(dis, 3);
+	}
+
+	private static float[] readFloat(DataInputStream dis, int size) throws IOException {
+		float[] result = new float[size];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = dis.readFloat();
+		}
+		return result;
 	}
 
 	public int[] getSun() {
@@ -80,68 +88,60 @@ public class TypeB {
 		this.enableVector = enableVector;
 	}
 
-	public float[] getRef_q() {
-		return ref_q;
+	public float[] getRefQ() {
+		return refQ;
 	}
 
-	public void setRef_q(float[] ref_q) {
-		this.ref_q = ref_q;
+	public void setRefQ(float[] refQ) {
+		this.refQ = refQ;
 	}
 
-	public float[] getErr_q() {
-		return err_q;
+	public float[] getErrQ() {
+		return errQ;
 	}
 
-	public void setErr_q(float[] err_q) {
-		this.err_q = err_q;
+	public void setErrQ(float[] errQ) {
+		this.errQ = errQ;
 	}
 
-	public float[] getIerr_q() {
-		return ierr_q;
+	public float[] getIerrQ() {
+		return ierrQ;
 	}
 
-	public void setIerr_q(float[] ierr_q) {
-		this.ierr_q = ierr_q;
+	public void setIerrQ(float[] ierrQ) {
+		this.ierrQ = ierrQ;
 	}
 
-	public float[] getErr_rate() {
-		return err_rate;
+	public float[] getErrRate() {
+		return errRate;
 	}
 
-	public void setErr_rate(float[] err_rate) {
-		this.err_rate = err_rate;
+	public void setErrRate(float[] errRate) {
+		this.errRate = errRate;
 	}
 
-	public float[] getSc_reci() {
-		return sc_reci;
+	public float[] getScReci() {
+		return scReci;
 	}
 
-	public void setSc_reci(float[] sc_reci) {
-		this.sc_reci = sc_reci;
+	public void setScReci(float[] scReci) {
+		this.scReci = scReci;
 	}
 
-	public float[] getSun_eci() {
-		return sun_eci;
+	public float[] getSunEci() {
+		return sunEci;
 	}
 
-	public void setSun_eci(float[] sun_eci) {
-		this.sun_eci = sun_eci;
+	public void setSunEci(float[] sunEci) {
+		this.sunEci = sunEci;
 	}
 
-	public float[] getMag_eci() {
-		return mag_eci;
+	public float[] getMagEci() {
+		return magEci;
 	}
 
-	public void setMag_eci(float[] mag_eci) {
-		this.mag_eci = mag_eci;
-	}
-
-	private static float[] readFloat(DataInputStream dis, int size) throws IOException {
-		float[] result = new float[size];
-		for (int i = 0; i < result.length; i++) {
-			result[i] = dis.readFloat();
-		}
-		return result;
+	public void setMagEci(float[] magEci) {
+		this.magEci = magEci;
 	}
 
 }
