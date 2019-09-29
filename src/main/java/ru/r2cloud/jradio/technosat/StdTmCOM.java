cfaeef1d31fafb7de488524612a66a80e15a3557
dernasherbrezon <rodionovamp@mail.ru>
2019-09-29 09:33:32 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/technosat/StdTmCOM.java
+++ src/main/java/ru/r2cloud/jradio/technosat/StdTmCOM.java
@@ -5,101 +5,101 @@ import java.io.IOException;
 
 public class StdTmCOM {
 
-	private byte NODENO;           // redundant node number
-	private boolean RST_EN;        // the watchdog application is enabled to reset the node
-	private byte BOTSLT;           // currently running internal software slot
-	private boolean SYNPPS;        // shall the node synchronize with the PPS signal
-	private boolean DISUTC;        // shall the node distribute the UTC time at the next PPS signal
-	private boolean DULBSY;        // Indicates the state of the UploadManagers Flash Controller
+	private byte nodeNo; // redundant node number
+	private boolean rstEn; // the watchdog application is enabled to reset the node
+	private byte botSlt; // currently running internal software slot
+	private boolean synPps; // shall the node synchronize with the PPS signal
+	private boolean disUTC; // shall the node distribute the UTC time at the next PPS signal
+	private boolean dulBsy; // Indicates the state of the UploadManagers Flash Controller
 
-	private float COMPATMP;        // temperature underneathe both PAs of a COM side
-	private int COMRSSI0;          // RSSI of Transceiver 0
-	private int COMRSSI1;          // RSSI of Transceiver 1
+	private float comPaTmp; // temperature underneathe both PAs of a COM side
+	private int comRssi0; // RSSI of Transceiver 0
+	private int comRssi1; // RSSI of Transceiver 1
 
 	public StdTmCOM(DataInputStream dis) throws IOException {
 		int raw = dis.readUnsignedByte();
-		NODENO = (byte) (raw >> 7);
-		RST_EN = ((raw >> 6) & 0x1) > 0;
-		BOTSLT = (byte) ((raw >> 3) & 0x7);
-		SYNPPS = ((raw >> 2) & 0x1) > 0;
-		DISUTC = ((raw >> 1) & 0x1) > 0;
-		DULBSY = (raw & 0x1) > 0;
+		nodeNo = (byte) (raw >> 7);
+		rstEn = ((raw >> 6) & 0x1) > 0;
+		botSlt = (byte) ((raw >> 3) & 0x7);
+		synPps = ((raw >> 2) & 0x1) > 0;
+		disUTC = ((raw >> 1) & 0x1) > 0;
+		dulBsy = (raw & 0x1) > 0;
 
-		COMPATMP = dis.readUnsignedShort() * 0.1f;
-		COMRSSI0 = dis.readUnsignedByte();
-		COMRSSI1 = dis.readUnsignedByte();
+		comPaTmp = dis.readUnsignedShort() * 0.1f;
+		comRssi0 = dis.readUnsignedByte();
+		comRssi1 = dis.readUnsignedByte();
 	}
 
-	public byte getNODENO() {
-		return NODENO;
+	public byte getNodeNo() {
+		return nodeNo;
 	}
 
-	public void setNODENO(byte nODENO) {
-		NODENO = nODENO;
+	public void setNodeNo(byte nodeNo) {
+		this.nodeNo = nodeNo;
 	}
 
-	public boolean isRST_EN() {
-		return RST_EN;
+	public boolean isRstEn() {
+		return rstEn;
 	}
 
-	public void setRST_EN(boolean rST_EN) {
-		RST_EN = rST_EN;
+	public void setRstEn(boolean rstEn) {
+		this.rstEn = rstEn;
 	}
 
-	public byte getBOTSLT() {
-		return BOTSLT;
+	public byte getBotSlt() {
+		return botSlt;
 	}
 
-	public void setBOTSLT(byte bOTSLT) {
-		BOTSLT = bOTSLT;
+	public void setBotSlt(byte botSlt) {
+		this.botSlt = botSlt;
 	}
 
-	public boolean isSYNPPS() {
-		return SYNPPS;
+	public boolean isSynPps() {
+		return synPps;
 	}
 
-	public void setSYNPPS(boolean sYNPPS) {
-		SYNPPS = sYNPPS;
+	public void setSynPps(boolean synPps) {
+		this.synPps = synPps;
 	}
 
-	public boolean isDISUTC() {
-		return DISUTC;
+	public boolean isDisUTC() {
+		return disUTC;
 	}
 
-	public void setDISUTC(boolean dISUTC) {
-		DISUTC = dISUTC;
+	public void setDisUTC(boolean disUTC) {
+		this.disUTC = disUTC;
 	}
 
-	public boolean isDULBSY() {
-		return DULBSY;
+	public boolean isDulBsy() {
+		return dulBsy;
 	}
 
-	public void setDULBSY(boolean dULBSY) {
-		DULBSY = dULBSY;
+	public void setDulBsy(boolean dulBsy) {
+		this.dulBsy = dulBsy;
 	}
 
-	public float getCOMPATMP() {
-		return COMPATMP;
+	public float getComPaTmp() {
+		return comPaTmp;
 	}
 
-	public void setCOMPATMP(float cOMPATMP) {
-		COMPATMP = cOMPATMP;
+	public void setComPaTmp(float comPaTmp) {
+		this.comPaTmp = comPaTmp;
 	}
 
-	public int getCOMRSSI0() {
-		return COMRSSI0;
+	public int getComRssi0() {
+		return comRssi0;
 	}
 
-	public void setCOMRSSI0(int cOMRSSI0) {
-		COMRSSI0 = cOMRSSI0;
+	public void setComRssi0(int comRssi0) {
+		this.comRssi0 = comRssi0;
 	}
 
-	public int getCOMRSSI1() {
-		return COMRSSI1;
+	public int getComRssi1() {
+		return comRssi1;
 	}
 
-	public void setCOMRSSI1(int cOMRSSI1) {
-		COMRSSI1 = cOMRSSI1;
+	public void setComRssi1(int comRssi1) {
+		this.comRssi1 = comRssi1;
 	}
 
 }
