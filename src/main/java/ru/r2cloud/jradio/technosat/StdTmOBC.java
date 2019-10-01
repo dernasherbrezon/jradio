49575a171c1b6151dc30deb2f00a0b7e72942ed7
dernasherbrezon <rodionovamp@mail.ru>
2019-10-01 07:14:13 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/technosat/StdTmOBC.java
+++ src/main/java/ru/r2cloud/jradio/technosat/StdTmOBC.java
@@ -5,112 +5,112 @@ import java.io.IOException;
 
 public class StdTmOBC {
 
-	private byte NODENO;                    // redundant node number
-	private boolean RST_EN;                 // the watchdog application is enabled to reset the node
-	private byte BOTSLT;                    // currently running internal software slot
-	private boolean SYNPPS;                 // shall the node synchronize with the PPS signal
-	private boolean DISUTC;                 // shall the node distribute the UTC time at the next PPS signal
-	private boolean DULBSY;                 // Indicates the state of the UploadManagers Flash Controller
-	private HistoryBufferType TMMTFHIST;    // indicates the history buffer used to fill up transfer frames with history telemetry
-	private SatelliteMode SAT_MODE;         // the current mode of the satellite
-	private HistoryBufferType TMMHSHIST;    // indicates the history buffer used to fill up the high speed downlink
-	private HighSpeedState TMMHSSTAT;       // indicates the current state of highspeed telemetry transmissions
+	private byte nodeNo; // redundant node number
+	private boolean rstEn; // the watchdog application is enabled to reset the node
+	private byte botSlt; // currently running internal software slot
+	private boolean synPps; // shall the node synchronize with the PPS signal
+	private boolean disUTC; // shall the node distribute the UTC time at the next PPS signal
+	private boolean dulBsy; // Indicates the state of the UploadManagers Flash Controller
+	private HistoryBufferType tmmtfHist; // indicates the history buffer used to fill up transfer frames with history telemetry
+	private SatelliteMode satMode; // the current mode of the satellite
+	private HistoryBufferType tmmhsHist; // indicates the history buffer used to fill up the high speed downlink
+	private HighSpeedState tmmhsStat; // indicates the current state of highspeed telemetry transmissions
 
 	public StdTmOBC(DataInputStream dis) throws IOException {
 		int raw = dis.readUnsignedByte();
-		NODENO = (byte) (raw >> 7);
-		RST_EN = ((raw >> 6) & 0x1) > 0;
-		BOTSLT = (byte) ((raw >> 3) & 0x7);
-		SYNPPS = ((raw >> 2) & 0x1) > 0;
-		DISUTC = ((raw >> 1) & 0x1) > 0;
-		DULBSY = (raw & 0x1) > 0;
+		nodeNo = (byte) (raw >> 7);
+		rstEn = ((raw >> 6) & 0x1) > 0;
+		botSlt = (byte) ((raw >> 3) & 0x7);
+		synPps = ((raw >> 2) & 0x1) > 0;
+		disUTC = ((raw >> 1) & 0x1) > 0;
+		dulBsy = (raw & 0x1) > 0;
 		raw = dis.readUnsignedByte();
-		TMMTFHIST = HistoryBufferType.valueOfCode(raw >> 6);
-		SAT_MODE = SatelliteMode.valueOfCode((raw >> 4) & 0x3);
-		TMMHSHIST = HistoryBufferType.valueOfCode((raw >> 2) & 0x3);
-		TMMHSSTAT = HighSpeedState.valueOfCode(raw & 0x3);
+		tmmtfHist = HistoryBufferType.valueOfCode(raw >> 6);
+		satMode = SatelliteMode.valueOfCode((raw >> 4) & 0x3);
+		tmmhsHist = HistoryBufferType.valueOfCode((raw >> 2) & 0x3);
+		tmmhsStat = HighSpeedState.valueOfCode(raw & 0x3);
 
 		dis.skipBytes(11);
 	}
 
-	public byte getNODENO() {
-		return NODENO;
+	public byte getNodeNo() {
+		return nodeNo;
 	}
 
-	public void setNODENO(byte nODENO) {
-		NODENO = nODENO;
+	public void setNodeNo(byte nodeNo) {
+		this.nodeNo = nodeNo;
 	}
 
-	public boolean isRST_EN() {
-		return RST_EN;
+	public boolean isRstEn() {
+		return rstEn;
 	}
 
-	public void setRST_EN(boolean rST_EN) {
-		RST_EN = rST_EN;
+	public void setRstEn(boolean rstEn) {
+		this.rstEn = rstEn;
 	}
 
-	public byte getBOTSLT() {
-		return BOTSLT;
+	public byte getBotSlt() {
+		return botSlt;
 	}
 
-	public void setBOTSLT(byte bOTSLT) {
-		BOTSLT = bOTSLT;
+	public void setBotSlt(byte botSlt) {
+		this.botSlt = botSlt;
 	}
 
-	public boolean isSYNPPS() {
-		return SYNPPS;
+	public boolean isSynPps() {
+		return synPps;
 	}
 
-	public void setSYNPPS(boolean sYNPPS) {
-		SYNPPS = sYNPPS;
+	public void setSynPps(boolean synPps) {
+		this.synPps = synPps;
 	}
 
-	public boolean isDISUTC() {
-		return DISUTC;
+	public boolean isDisUTC() {
+		return disUTC;
 	}
 
-	public void setDISUTC(boolean dISUTC) {
-		DISUTC = dISUTC;
+	public void setDisUTC(boolean disUTC) {
+		this.disUTC = disUTC;
 	}
 
-	public boolean isDULBSY() {
-		return DULBSY;
+	public boolean isDulBsy() {
+		return dulBsy;
 	}
 
-	public void setDULBSY(boolean dULBSY) {
-		DULBSY = dULBSY;
+	public void setDulBsy(boolean dulBsy) {
+		this.dulBsy = dulBsy;
 	}
 
-	public HistoryBufferType getTMMTFHIST() {
-		return TMMTFHIST;
+	public HistoryBufferType getTmmtfHist() {
+		return tmmtfHist;
 	}
 
-	public void setTMMTFHIST(HistoryBufferType tMMTFHIST) {
-		TMMTFHIST = tMMTFHIST;
+	public void setTmmtfHist(HistoryBufferType tmmtfHist) {
+		this.tmmtfHist = tmmtfHist;
 	}
 
-	public SatelliteMode getSAT_MODE() {
-		return SAT_MODE;
+	public SatelliteMode getSatMode() {
+		return satMode;
 	}
 
-	public void setSAT_MODE(SatelliteMode sAT_MODE) {
-		SAT_MODE = sAT_MODE;
+	public void setSatMode(SatelliteMode satMode) {
+		this.satMode = satMode;
 	}
 
-	public HistoryBufferType getTMMHSHIST() {
-		return TMMHSHIST;
+	public HistoryBufferType getTmmhsHist() {
+		return tmmhsHist;
 	}
 
-	public void setTMMHSHIST(HistoryBufferType tMMHSHIST) {
-		TMMHSHIST = tMMHSHIST;
+	public void setTmmhsHist(HistoryBufferType tmmhsHist) {
+		this.tmmhsHist = tmmhsHist;
 	}
 
-	public HighSpeedState getTMMHSSTAT() {
-		return TMMHSSTAT;
+	public HighSpeedState getTmmhsStat() {
+		return tmmhsStat;
 	}
 
-	public void setTMMHSSTAT(HighSpeedState tMMHSSTAT) {
-		TMMHSSTAT = tMMHSSTAT;
+	public void setTmmhsStat(HighSpeedState tmmhsStat) {
+		this.tmmhsStat = tmmhsStat;
 	}
 
 }
