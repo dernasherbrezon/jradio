7e07c4dc20d444354eb219a54b9fab1017fa3021
dernasherbrezon <rodionovamp@mail.ru>
2019-10-16 07:09:25 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/technosat/TmCameraStatus2.java
+++ src/main/java/ru/r2cloud/jradio/technosat/TmCameraStatus2.java
@@ -5,39 +5,39 @@ import java.io.IOException;
 
 public class TmCameraStatus2 {
 
-	private boolean I2CEN;     // I2C enabled
-	private boolean I2CLK;     // I2C locked
-	private boolean BUSY;      // Cam busy
+	private boolean i2cEn; // I2C enabled
+	private boolean i2cLk; // I2C locked
+	private boolean busy; // Cam busy
 
 	public TmCameraStatus2(DataInputStream dis) throws IOException {
 		int raw = dis.readUnsignedByte();
-		I2CEN = ((raw >> 7) & 0x1) > 0;
-		I2CLK = ((raw >> 6) & 0x1) > 0;
-		BUSY = ((raw >> 5) & 0x1) > 0;
+		i2cEn = ((raw >> 7) & 0x1) > 0;
+		i2cLk = ((raw >> 6) & 0x1) > 0;
+		busy = ((raw >> 5) & 0x1) > 0;
 	}
 
-	public boolean isI2CEN() {
-		return I2CEN;
+	public boolean isI2cEn() {
+		return i2cEn;
 	}
 
-	public void setI2CEN(boolean i2cen) {
-		I2CEN = i2cen;
+	public void setI2cEn(boolean i2cEn) {
+		this.i2cEn = i2cEn;
 	}
 
-	public boolean isI2CLK() {
-		return I2CLK;
+	public boolean isI2cLk() {
+		return i2cLk;
 	}
 
-	public void setI2CLK(boolean i2clk) {
-		I2CLK = i2clk;
+	public void setI2cLk(boolean i2cLk) {
+		this.i2cLk = i2cLk;
 	}
 
-	public boolean isBUSY() {
-		return BUSY;
+	public boolean isBusy() {
+		return busy;
 	}
 
-	public void setBUSY(boolean bUSY) {
-		BUSY = bUSY;
+	public void setBusy(boolean busy) {
+		this.busy = busy;
 	}
 
 }
