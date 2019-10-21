8cbcbecff64fa9afb8781e375df9d4ab78b3f56c
dernasherbrezon <rodionovamp@mail.ru>
2019-10-21 10:07:22 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/tubix20/MobitexRandomizer.java
+++ src/main/java/ru/r2cloud/jradio/tubix20/MobitexRandomizer.java
@@ -6,14 +6,14 @@ public class MobitexRandomizer {
 	private static final int[] sequence = new int[511 * 8];
 
 	static {
-		int scramble_shift_reg = 0x01FF;
+		int scrambleShiftRegister = 0x01FF;
 		for (int i = 0; i < sequence.length * 8; i++) {
-			sequence[i / 8] = (sequence[i / 8] << 1) | (scramble_shift_reg & 0x1);
+			sequence[i / 8] = (sequence[i / 8] << 1) | (scrambleShiftRegister & 0x1);
 			// Check 5th and 9th Stage of
-			if (((scramble_shift_reg & 0x0011) == 0x0010) || ((scramble_shift_reg & 0x0011) == 0x0001)) {
-				scramble_shift_reg |= 0x0200;
+			if (((scrambleShiftRegister & 0x0011) == 0x0010) || ((scrambleShiftRegister & 0x0011) == 0x0001)) {
+				scrambleShiftRegister |= 0x0200;
 			}
-			scramble_shift_reg = (scramble_shift_reg >> 1) & 0x01FF;
+			scrambleShiftRegister = (scrambleShiftRegister >> 1) & 0x01FF;
 		}
 	}
 
