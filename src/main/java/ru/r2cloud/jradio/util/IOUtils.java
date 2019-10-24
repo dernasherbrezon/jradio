e209b9672b80fe4d84a97ec513fa017c488c02ab
dernasherbrezon <rodionovamp@mail.ru>
2019-10-24 08:26:05 +0100

    fixing code smells

--- src/main/java/ru/r2cloud/jradio/util/IOUtils.java
+++ src/main/java/ru/r2cloud/jradio/util/IOUtils.java
@@ -6,7 +6,7 @@ import java.io.InputStream;
 
 public class IOUtils {
 
-	public static void readFully(InputStream is, byte b[]) throws IOException {
+	public static void readFully(InputStream is, byte[] b) throws IOException {
 		int n = 0;
 		while (n < b.length) {
 			int count = is.read(b, n, b.length - n);
@@ -17,4 +17,7 @@ public class IOUtils {
 		}
 	}
 
+	private IOUtils() {
+		// do nothing
+	}
 }
