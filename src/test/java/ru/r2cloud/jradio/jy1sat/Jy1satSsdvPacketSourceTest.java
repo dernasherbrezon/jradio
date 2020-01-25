0b1b11cf9a8f64557b52f16cccd4bb1144a5f087
dernasherbrezon <rodionovamp@mail.ru>
2020-01-25 12:38:56 +0000

    add some tests

--- src/test/java/ru/r2cloud/jradio/jy1sat/Jy1satSsdvPacketSourceTest.java
+++ src/test/java/ru/r2cloud/jradio/jy1sat/Jy1satSsdvPacketSourceTest.java
@@ -1,10 +1,14 @@
 package ru.r2cloud.jradio.jy1sat;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 import javax.imageio.ImageIO;
 
@@ -29,6 +33,24 @@ public class Jy1satSsdvPacketSourceTest {
 			}
 		}
 		assertEquals(1, total);
+	}
+
+	@Test(expected = NoSuchElementException.class)
+	public void testIllegalToCallNextFirst() throws Exception {
+		try (BeaconInputStream<Jy1satBeacon> bis = new BeaconInputStream<>(Jy1satSsdvPacketSourceTest.class.getClassLoader().getResourceAsStream("jy1sat.bin"), Jy1satBeacon.class)) {
+			new Jy1satSsdvPacketSource(bis).next();
+		}
+	}
+
+	@Test
+	public void testInvalidPayload() throws Exception {
+		Jy1satBeacon beacon = new Jy1satBeacon();
+		beacon.setPayload(new byte[2]);
+
+		List<Jy1satBeacon> beacons = new ArrayList<>();
+		beacons.add(beacon);
+		SsdvDecoder decoder = new SsdvDecoder(new Jy1satSsdvPacketSource(beacons.iterator()));
+		assertFalse(decoder.hasNext());
 	}
 
 	private static void assertSsdvImage(String expectedName, SsdvImage cur) throws IOException {
