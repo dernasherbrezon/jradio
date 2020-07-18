e3b2012a4117b5c2f1548119be318a10601ecb3c
dernasherbrezon <rodionovamp@mail.ru>
2020-07-18 11:51:36 +0100

    fix test

--- src/test/java/ru/r2cloud/jradio/blocks/InterpFIRFilterTest.java
+++ src/test/java/ru/r2cloud/jradio/blocks/InterpFIRFilterTest.java
@@ -13,7 +13,7 @@ public class InterpFIRFilterTest {
 		Context ctx = new Context();
 		ctx.setChannels(1);
 		float[] taps = Firdes.gaussian(1.5, 2 * (32000f / 1200), 0.5, 12);
-		try (InterpFIRFilter filter = new InterpFIRFilter(new InputStreamSource(SymbolSyncTest.class.getClassLoader().getResourceAsStream("polyphase.bin"), ctx), 2, taps)) {
+		try (InterpFIRFilter filter = new InterpFIRFilter(new InputStreamSource(InterpFIRFilterTest.class.getClassLoader().getResourceAsStream("polyphase.bin"), ctx), 2, taps)) {
 			TestUtil.assertFloatInput("interpFirFilter.bin", filter);
 		}
 	}
