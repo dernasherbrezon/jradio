2f857ce63a448e8cb7e393abcf1b851b87247d3d
dernasherbrezon <rodionovamp@mail.ru>
2020-08-08 09:21:43 +0100

    remove fcs check
    it might be corrupted, where viterbi + reed solomon can recover frame.
    skip this check completely.

--- src/main/java/ru/r2cloud/jradio/aausat4/Aausat4.java
+++ src/main/java/ru/r2cloud/jradio/aausat4/Aausat4.java
@@ -26,11 +26,6 @@ public class Aausat4 extends BeaconSource<Aausat4Beacon> {
 
 	@Override
 	protected Aausat4Beacon parseBeacon(byte[] raw) throws UncorrectableException, IOException {
-		int fsm = hardDecode(raw);
-		if (fsm != LONG_PACKET_FSM) {
-			return null;
-		}
-
 		// long frame
 		byte[] viterbi = viterbiSoft.decode(Arrays.copyOfRange(raw, 8, raw.length));
 		Randomize.shuffle(viterbi);
@@ -39,20 +34,6 @@ public class Aausat4 extends BeaconSource<Aausat4Beacon> {
 		Aausat4Beacon current = new Aausat4Beacon();
 		current.readExternal(data);
 		return current;
-	}
-
-	private static int hardDecode(byte[] raw) {
-		int result = 0;
-		for (int i = 0; i < 8; i++) {
-			byte toCheck;
-			if (raw[i] > 0) {
-				toCheck = 1;
-			} else {
-				toCheck = 0;
-			}
-			result = (result << 1) | (toCheck & 0x1);
-		}
-		return result;
 	}
 
 }
