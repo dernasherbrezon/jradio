7959609c91aad7d7db82b223ded290aa9c7d923c
dernasherbrezon <rodionovamp@mail.ru>
2020-08-16 09:07:03 +0100

    add test for meteor admin analog packet

--- src/test/java/ru/r2cloud/jradio/meteor/MeteorAdminPacketTest.java
+++ src/test/java/ru/r2cloud/jradio/meteor/MeteorAdminPacketTest.java
@@ -13,4 +13,11 @@ public class MeteorAdminPacketTest {
 		AssertJson.assertObjectsEqual("MeteorAdminPacket.json", packet);
 	}
 
+	@Test
+	public void testAnalogTelemetry() throws Exception {
+		byte[] data = new byte[] { 2, 24, -89, -93, -110, -35, -102, -65, 13, 57, 53, -104, 0, 15, -56, -54, 80, 79, 76, 116, 117, 116, 101, 103, 111, 89, -127, -79, -122, 82, -51, 90, -25, -68, 117, 59, -127, -42, 16, 25, 115, 65, -78, 54, -20, 72, -9, 5, 19, 113 };
+		MeteorAdminPacket packet = new MeteorAdminPacket(data);
+		AssertJson.assertObjectsEqual("MeteorAdminPacketAnalog.json", packet);
+	}
+
 }
