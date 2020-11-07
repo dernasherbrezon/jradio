f3fcfe35057c8ab4ae92f87b7f60593e5529cc20
dernasherbrezon <rodionovamp@mail.ru>
2020-11-07 12:01:59 +0000

    enhance gomx1 test
    + use afsk demodulator instead of handcrafted blocks

--- src/main/java/ru/r2cloud/jradio/gomx1/Gomx1.java
+++ src/main/java/ru/r2cloud/jradio/gomx1/Gomx1.java
@@ -3,13 +3,16 @@ package ru.r2cloud.jradio.gomx1;
 import java.io.IOException;
 
 import ru.r2cloud.jradio.BeaconSource;
-import ru.r2cloud.jradio.MessageInput;
+import ru.r2cloud.jradio.ByteInput;
+import ru.r2cloud.jradio.blocks.CorrelateAccessCodeTag;
+import ru.r2cloud.jradio.blocks.FixedLengthTagger;
+import ru.r2cloud.jradio.blocks.TaggedStreamToPdu;
 import ru.r2cloud.jradio.fec.ccsds.UncorrectableException;
 
 public class Gomx1 extends BeaconSource<Gomx1Beacon> {
 
-	public Gomx1(MessageInput input) {
-		super(input);
+	public Gomx1(ByteInput input) {
+		super(new AX100Decoder(new TaggedStreamToPdu(new FixedLengthTagger(new CorrelateAccessCodeTag(input, 4, "11000011101010100110011001010101", true), (255 + 3) * 8)), false, false, false));
 	}
 
 	@Override
