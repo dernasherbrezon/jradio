ab72c815bfe721a6f5c65cd4ed1356441dad0e6e
dernasherbrezon <rodionovamp@mail.ru>
2020-11-22 14:26:39 +0000

    increase bandwidth a bit

--- src/main/java/ru/r2cloud/jradio/demod/BpskDemodulator.java
+++ src/main/java/ru/r2cloud/jradio/demod/BpskDemodulator.java
@@ -30,7 +30,7 @@ public class BpskDemodulator implements ByteInput {
 		}
 		float samplesPerSymbol = next.getContext().getSampleRate() / baudRate;
 		next = new RmsAgcComplex(next, 2e-2f / samplesPerSymbol, 1.0f);
-		next = new FLLBandEdge(next, samplesPerSymbol, 0.35f, 100, (float) (2 * Math.PI / next.getContext().getSampleRate() * 20));
+		next = new FLLBandEdge(next, samplesPerSymbol, 0.35f, 100, (float) (3 * Math.PI / next.getContext().getSampleRate() * 20));
 		next = new LowPassFilterComplex(next, 1.0, baudRate, baudRate * 0.1, Window.WIN_HAMMING, 6.76);
 		int nfilts = 16;
 		float[] rrcTaps = Firdes.rootRaisedCosine(nfilts, nfilts, 1.0f / samplesPerSymbol, 0.35f, (int) (11 * samplesPerSymbol * nfilts));
