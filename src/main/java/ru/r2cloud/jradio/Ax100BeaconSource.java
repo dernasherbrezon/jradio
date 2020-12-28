74ae1e464935a158b9cea605eeb5525453b37d37
dernasherbrezon <rodionovamp@mail.ru>
2020-12-28 16:36:18 +0000

    migrate from correlationaccesstag block correlatesyncword

--- src/main/java/ru/r2cloud/jradio/Ax100BeaconSource.java
+++ src/main/java/ru/r2cloud/jradio/Ax100BeaconSource.java
@@ -1,13 +1,12 @@
 package ru.r2cloud.jradio;
 
 import java.io.IOException;
+import java.util.Collections;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ru.r2cloud.jradio.blocks.CorrelateAccessCodeTag;
-import ru.r2cloud.jradio.blocks.FixedLengthTagger;
-import ru.r2cloud.jradio.blocks.TaggedStreamToPdu;
+import ru.r2cloud.jradio.blocks.CorrelateSyncword;
 import ru.r2cloud.jradio.fec.ccsds.UncorrectableException;
 import ru.r2cloud.jradio.gomx1.AX100Decoder;
 
@@ -20,13 +19,13 @@ public class Ax100BeaconSource<T extends Beacon> extends BeaconSource<T> {
 	public Ax100BeaconSource(ByteInput input, int beaconSizeBytes, Class<T> clazz) {
 		this(input, beaconSizeBytes, "10010011000010110101000111011110", clazz, false, true, true);
 	}
-	
+
 	public Ax100BeaconSource(ByteInput input, int beaconSizeBytes, Class<T> clazz, boolean forceViterbi, boolean forceScrambler, boolean forceReedSolomon) {
 		this(input, beaconSizeBytes, "10010011000010110101000111011110", clazz, forceViterbi, forceScrambler, forceReedSolomon);
 	}
-	
+
 	public Ax100BeaconSource(ByteInput input, int beaconSizeBytes, String accessCode, Class<T> clazz, boolean forceViterbi, boolean forceScrambler, boolean forceReedSolomon) {
-		super(new AX100Decoder(new TaggedStreamToPdu(new FixedLengthTagger(new CorrelateAccessCodeTag(input, 4, accessCode, true), (beaconSizeBytes + 3) * 8)), forceViterbi, forceScrambler, forceReedSolomon));
+		super(new AX100Decoder(new CorrelateSyncword(input, 4, Collections.singleton(accessCode), (beaconSizeBytes + 3) * 8, true), forceViterbi, forceScrambler, forceReedSolomon));
 		this.clazz = clazz;
 	}
 
--- src/test/java/ru/r2cloud/jradio/fox/FoxTest.java
+++ src/test/java/ru/r2cloud/jradio/fox/FoxTest.java
@@ -9,10 +9,7 @@ import org.junit.After;
 import org.junit.Test;
 
 import ru.r2cloud.jradio.AssertJson;
-import ru.r2cloud.jradio.blocks.CorrelateAccessCodeTag;
-import ru.r2cloud.jradio.blocks.FixedLengthTagger;
-import ru.r2cloud.jradio.blocks.SoftToHard;
-import ru.r2cloud.jradio.blocks.TaggedStreamToPdu;
+import ru.r2cloud.jradio.blocks.CorrelateSyncword;
 import ru.r2cloud.jradio.demod.FskDemodulator;
 import ru.r2cloud.jradio.source.WavFileSource;
 
@@ -24,12 +21,10 @@ public class FoxTest {
 	public void testSuccess() throws Exception {
 		WavFileSource source = new WavFileSource(FoxTest.class.getClassLoader().getResourceAsStream("slowFox1a.wav"));
 		FskDemodulator demod = new FskDemodulator(source, 200, 0.0f, 120, 200);
-		SoftToHard s2h = new SoftToHard(demod);
 		Set<String> codes = new HashSet<>();
 		codes.add("0011111010");
 		codes.add("1100000101");
-		CorrelateAccessCodeTag correlate = new CorrelateAccessCodeTag(s2h, 0, codes, false);
-		TaggedStreamToPdu pdu = new TaggedStreamToPdu(new FixedLengthTagger(correlate, Fox.SLOW_FRAME_SIZE * 10));
+		CorrelateSyncword pdu = new CorrelateSyncword(demod, 0, codes, Fox.SLOW_FRAME_SIZE * 10, false);
 		input = new Fox<>(pdu, Fox1ABeacon.class);
 		assertTrue(input.hasNext());
 		AssertJson.assertObjectsEqual("Fox1ABeacon.json", input.next());
@@ -39,18 +34,16 @@ public class FoxTest {
 	public void testHighSpeed() throws Exception {
 		WavFileSource source = new WavFileSource(FoxTest.class.getClassLoader().getResourceAsStream("fox1dWithImage.wav"));
 		FskDemodulator demod = new FskDemodulator(source, 9600, 0.0f, 1, 2000);
-		SoftToHard s2h = new SoftToHard(demod);
 		Set<String> codes = new HashSet<>();
 		codes.add("0011111010");
 		codes.add("1100000101");
-		CorrelateAccessCodeTag correlate = new CorrelateAccessCodeTag(s2h, 0, codes, false);
-		TaggedStreamToPdu pdu = new TaggedStreamToPdu(new FixedLengthTagger(correlate, HighSpeedFox.HIGH_SPEED_FRAME_SIZE * 10));
+		CorrelateSyncword pdu = new CorrelateSyncword(demod, 0, codes, HighSpeedFox.HIGH_SPEED_FRAME_SIZE * 10, false);
 		HighSpeedFox<Fox1DBeacon> input = new HighSpeedFox<>(pdu, Fox1DBeacon.class);
 		assertTrue(input.hasNext());
 		AssertJson.assertObjectsEqual("Fox1DBeacon-highspeed.json", input.next());
 		input.close();
 	}
-	
+
 	@After
 	public void stop() throws Exception {
 		if (input != null) {
