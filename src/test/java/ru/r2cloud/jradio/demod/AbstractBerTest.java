0259fbdaa3c66ce90c3532b74e106b5366d555d7
dernasherbrezon <rodionovamp@mail.ru>
2021-07-29 10:10:25 +0100

    fix test

--- src/test/java/ru/r2cloud/jradio/demod/AbstractBerTest.java
+++ src/test/java/ru/r2cloud/jradio/demod/AbstractBerTest.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import org.junit.Test;
 
 import ru.r2cloud.jradio.ByteInput;
-import ru.r2cloud.jradio.blocks.HardToSoft;
+import ru.r2cloud.jradio.Context;
 
 public class AbstractBerTest {
 
@@ -21,9 +21,42 @@ public class AbstractBerTest {
 
 		@Override
 		public ByteInput createModulatorDemodulator(ByteInput input, float ebno) {
-			return new HardToSoft(input);
+			return new UnpackedToSoft(input);
 		}
 
 	}
-	
+
+	private static class UnpackedToSoft implements ByteInput {
+
+		private final ByteInput input;
+		private final Context ctx;
+
+		public UnpackedToSoft(ByteInput input) {
+			this.input = input;
+			this.ctx = new Context(input.getContext());
+			this.ctx.setSoftBits(true);
+		}
+
+		@Override
+		public void close() throws IOException {
+			input.close();
+		}
+
+		@Override
+		public byte readByte() throws IOException {
+			byte bit = input.readByte();
+			if (bit == 0) {
+				return Byte.MIN_VALUE;
+			} else {
+				return Byte.MAX_VALUE;
+			}
+		}
+
+		@Override
+		public Context getContext() {
+			return ctx;
+		}
+
+	}
+
 }
