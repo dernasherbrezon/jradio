98899c678b8b7e137dda0c6d727552fe8f624007
dernasherbrezon <rodionovamp@mail.ru>
2022-02-14 20:30:06 +0000

    refactor a bit

--- src/main/java/ru/r2cloud/jradio/blocks/FrequencyModulator.java
+++ src/main/java/ru/r2cloud/jradio/blocks/FrequencyModulator.java
@@ -2,18 +2,18 @@ package ru.r2cloud.jradio.blocks;
 
 import java.io.IOException;
 
+import ru.r2cloud.jradio.ComplexFloatInput;
 import ru.r2cloud.jradio.Context;
 import ru.r2cloud.jradio.FloatInput;
 import ru.r2cloud.jradio.util.MathUtils;
 
-public class FrequencyModulator implements FloatInput {
+public class FrequencyModulator extends ComplexFloatInput {
 
 	private final FloatInput input;
 	private final float sensitivity;
 	private final Context context;
 
 	private final float[] complex = new float[2];
-	private int complexIndex;
 	private float phase = 0;
 
 	public FrequencyModulator(FloatInput input, float sensitivity) {
@@ -21,24 +21,18 @@ public class FrequencyModulator implements FloatInput {
 		this.sensitivity = sensitivity;
 		this.context = new Context(input.getContext());
 		this.context.setChannels(2);
-		this.complexIndex = complex.length;
 	}
 
 	@Override
-	public float readFloat() throws IOException {
-		if (complexIndex >= complex.length) {
-			complexIndex = 0;
+	public float[] readComplex() throws IOException {
+		phase = phase + sensitivity * input.readFloat();
+		phase = (float) ((phase + Math.PI) % (2 * Math.PI) - Math.PI);
 
-			phase = phase + sensitivity * input.readFloat();
-			phase = (float) ((phase + Math.PI) % (2 * Math.PI) - Math.PI);
-
-			int angle = MathUtils.floatToFixed(phase);
-			MathUtils.sincos(angle, complex);
-		}
-
-		float result = complex[complexIndex];
-		complexIndex++;
-		return result;
+		int angle = MathUtils.floatToFixed(phase);
+		// for angle=0, it will produce: [1.0000024, 7.2191946E-9]
+		// outside [-1.0f;1.0f] interval
+		MathUtils.sincos(angle, complex);
+		return complex;
 	}
 
 	@Override
