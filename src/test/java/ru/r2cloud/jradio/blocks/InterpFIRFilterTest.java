4fed2bdff59da6ca1c461d770781195ce57fd2aa
dernasherbrezon <rodionovamp@mail.ru>
2022-02-14 20:42:05 +0000

    verify tail

--- src/test/java/ru/r2cloud/jradio/blocks/InterpFIRFilterTest.java
+++ src/test/java/ru/r2cloud/jradio/blocks/InterpFIRFilterTest.java
@@ -31,8 +31,10 @@ public class InterpFIRFilterTest {
 		Context ctx = new Context();
 		ctx.setChannels(1);
 		float[] taps = Firdes.gaussian(1.5, 2 * (32000f / 1200), 0.5, 12);
+		float[] tail = new float[] { -1.7165489E-4f, -1.6697908E-4f, -6.2933344E-5f, -6.0601466E-5f, 9.337405E-5f, 9.0499736E-5f, -1.0465083E-6f, -1.3428325E-6f, -3.1872907E-5f, -3.100735E-5f };
 		try (InterpFIRFilter filter = new InterpFIRFilter(new InputStreamSource(InterpFIRFilterTest.class.getClassLoader().getResourceAsStream("polyphase.bin"), ctx), 2, taps)) {
 			TestUtil.assertFloatInput("interpFirFilter.bin", filter);
+			TestUtil.assertTail(tail, filter);
 		}
 	}
 
