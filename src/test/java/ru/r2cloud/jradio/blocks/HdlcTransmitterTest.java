d85458fc7d01d8a3cba02ffc36dc9f6b7e0710e6
dernasherbrezon <rodionovamp@mail.ru>
2022-02-21 21:15:45 +0000

    add test for scrambled data

--- src/test/java/ru/r2cloud/jradio/blocks/HdlcTransmitterTest.java
+++ src/test/java/ru/r2cloud/jradio/blocks/HdlcTransmitterTest.java
@@ -6,6 +6,7 @@ import org.junit.After;
 import org.junit.Test;
 
 import ru.r2cloud.jradio.ArrayByteInput;
+import ru.r2cloud.jradio.ByteInput;
 
 public class HdlcTransmitterTest {
 
@@ -18,6 +19,20 @@ public class HdlcTransmitterTest {
 		transmitter = new HdlcTransmitter();
 		byte[] encoded = transmitter.encode(data);
 		receiver = new HdlcReceiver(new ArrayByteInput(encoded), 10);
+		byte[] actual = receiver.readBytes();
+		assertEquals(data.length, actual.length);
+		for (int i = 0; i < data.length; i++) {
+			assertEquals(data[i], actual[i]);
+		}
+	}
+	
+	@Test
+	public void testSendReceiveScrambled() throws Exception {
+		byte[] data = new byte[] { (byte) 0xF1, (byte) 0xA7, (byte) 0x7C, 0x56 };
+		transmitter = new HdlcTransmitter();
+		byte[] encoded = transmitter.encode(data);
+		ByteInput scrambled = new NrziEncode(new Scrambler(new ArrayByteInput(encoded), 0x21, 0x00, 16));
+		receiver = new HdlcReceiver(new Descrambler(new NrziDecode(scrambled), 0x21, 0, 16), 10);
 		byte[] actual = receiver.readBytes();
 		assertEquals(data.length, actual.length);
 		for (int i = 0; i < data.length; i++) {
