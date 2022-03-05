337e752840a58360c782dc51b93917bf1b247010
dernasherbrezon <rodionovamp@mail.ru>
2022-03-05 22:18:46 +0000

    better ax25 ber test

--- src/test/java/ru/r2cloud/jradio/Ax25G3ruhBer.java
+++ src/test/java/ru/r2cloud/jradio/Ax25G3ruhBer.java
@@ -3,8 +3,11 @@ package ru.r2cloud.jradio;
 import java.io.EOFException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
+import org.apache.commons.math3.special.Erf;
+
+import com.google.gson.Gson;
 
 import ru.r2cloud.jradio.blocks.ChannelModel;
 import ru.r2cloud.jradio.blocks.FastNoiseSourceComplex;
@@ -14,40 +17,65 @@ import ru.r2cloud.jradio.blocks.NrziEncode;
 import ru.r2cloud.jradio.blocks.PackedToUnpacked;
 import ru.r2cloud.jradio.blocks.Scrambler;
 import ru.r2cloud.jradio.demod.FskDemodulator;
-import ru.r2cloud.jradio.source.SequentialByteSource;
 import ru.r2cloud.jradio.source.SequentialSource;
 import ru.r2cloud.jradio.source.TimeConstraintedSource;
 
 public class Ax25G3ruhBer {
 
 	private static int numberOfFrames = 100;
-	private static int frameLength = 131;
 	private static float sampleRate = 48000.0f;
 	private static float devitation = 5000.0f;
 	private static float baudRate = 9600.0f;
-	private static long silenceLengthSamples = (long) sampleRate / 8;
-
-	// length 20
-	// checksum = true
-	// 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 0.99 0.99 0.96 0.95 0.68 0.53 0.37 0.27 0.16 0.11 0.08 0.08 0.09 0.06 0.06 0.06 0.06 0.04 0.04 0.06
-	// checksum = false
-	// 0.9840625 0.97925 0.98975 0.9946875 0.9946875 0.9894375 0.984625 0.9946875 0.9946875 0.9946875 0.9946875 0.9879375 0.9946875 0.947625 0.905125 0.77825 0.715375 0.5909375 0.51475 0.2965 0.2109375 0.163375 0.1105625 0.0775625 0.0758125 0.0554375 0.0650625 0.0750625 0.0546875 0.0546875 0.0546875 0.0546875 0.0346875 0.0346875 0.0546875
-
-	// length 131
-	// checksum = true
-	// 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 0.96 0.89 0.7 0.41 0.22 0.12 0.09 0.09 0.06 0.06 0.06 0.06 0.05 0.04 0.06
-	// checksum = false
-	// 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 0.9927862595419847 1.0 0.9751526717557252 0.8884446564885496 0.7935973282442748 0.6203721374045802 0.47770038167938933 0.31206106870229006 0.21568702290076336 0.1688740458015267 0.14463740458015267 0.09211832061068702 0.0908206106870229 0.07028625954198474 0.08005725190839695 0.08005725190839695 0.06 0.06 0.06 0.06 0.05 0.04 0.06
+	private static long silenceLengthSamples = (long) sampleRate / 4;
 
 	public static void main(String[] args) throws Exception {
-		StringBuilder b = new StringBuilder();
-		for (float ebno = 0.0f; ebno < 17.01f; ebno += 0.5f) {
-			b.append(" ").append(calculateBer(ebno));
-		}
-		System.out.println(b.toString());
+		calculateForPreamble();
+		calculateForDifferentLengths();
 	}
 
-	private static double calculateBer(float ebno) throws IOException {
+	private static void calculateForPreamble() throws IOException {
+		Gson gson = new Gson();
+		List<Ax25G3ruhBerStat> stats = new ArrayList<>();
+		for (float ebno = 0.0f; ebno < 17.01f; ebno += 0.5f) {
+			Ax25G3ruhBerStat cur = new Ax25G3ruhBerStat();
+			cur.setEbno(ebno);
+			if (ebno > 13.5f) {
+				cur.setFskBer(0.0);
+			} else {
+				cur.setFskBer(0.5 * Erf.erfc(Math.sqrt(Math.pow(10.0, ebno / 10) / 2)));
+			}
+			cur.setPrepend0(calculateBer(ebno, 0, 20, true));
+			cur.setPrepend3(calculateBer(ebno, 3, 20, true));
+			cur.setPrepend6(calculateBer(ebno, 6, 20, true));
+			cur.setPrepend9(calculateBer(ebno, 9, 20, true));
+			stats.add(cur);
+		}
+		Ax25G3ruhBerStat[] array = stats.toArray(new Ax25G3ruhBerStat[0]);
+		System.out.println(gson.toJson(array));
+	}
+
+	private static void calculateForDifferentLengths() throws IOException {
+		Gson gson = new Gson();
+		List<Ax25G3ruhBerStatLength> stats = new ArrayList<>();
+		for (float ebno = 0.0f; ebno < 17.01f; ebno += 0.5f) {
+			Ax25G3ruhBerStatLength cur = new Ax25G3ruhBerStatLength();
+			cur.setEbno(ebno);
+			if (ebno > 13.5f) {
+				cur.setFskBer(0.0);
+			} else {
+				cur.setFskBer(0.5 * Erf.erfc(Math.sqrt(Math.pow(10.0, ebno / 10) / 2)));
+			}
+			cur.setAx25Byte20Checksum(calculateBer(ebno, 0, 20, true));
+			cur.setAx25Byte20NoChecksum(calculateBer(ebno, 0, 20, false));
+			cur.setAx25Byte131Checksum(calculateBer(ebno, 0, 131, true));
+			cur.setAx25Byte131NoChecksum(calculateBer(ebno, 0, 131, false));
+			stats.add(cur);
+		}
+		Ax25G3ruhBerStatLength[] array = stats.toArray(new Ax25G3ruhBerStatLength[0]);
+		System.out.println(gson.toJson(array));
+	}
+
+	private static double calculateBer(float ebno, int prepend, int frameLength, boolean checksum) throws IOException {
 		float noiseVoltage = (float) (Math.sqrt(sampleRate / baudRate) / Math.pow(10, (ebno / 20)));
 
 		Context silenceCtx = new Context();
@@ -55,7 +83,7 @@ public class Ax25G3ruhBer {
 		silenceCtx.setSampleRate(sampleRate);
 		silenceCtx.setTotalSamples(silenceLengthSamples);
 
-		HdlcTransmitter transmitter = new HdlcTransmitter();
+		HdlcTransmitter transmitter = new HdlcTransmitter(prepend, 0);
 
 		byte[] inputData = new byte[frameLength];
 		for (int i = 0; i < inputData.length; i++) {
@@ -64,18 +92,9 @@ public class Ax25G3ruhBer {
 
 		List<FloatInput> inputs = new ArrayList<>();
 		for (int i = 0; i < numberOfFrames; i++) {
-			List<ByteInput> frameData = new ArrayList<>();
-			byte[] preamble = new byte[3];
-			Arrays.fill(preamble, (byte) 0xaa);
-			frameData.add(new PackedToUnpacked(new ArrayByteInput(preamble), 1, Endianness.GR_MSB_FIRST));
-			frameData.add(new NrziEncode(new Scrambler(new ArrayByteInput(transmitter.encode(inputData)), 0x21, 0x00, 16)));
-
-			Context bb = new Context();
-			bb.setSampleRate(baudRate);
-			SequentialByteSource frames = new SequentialByteSource(frameData, bb);
-
 			inputs.add(new TimeConstraintedSource(new FastNoiseSourceComplex(noiseVoltage, (i + 1) * 42, 1024 * 16), silenceCtx));
-			inputs.add(new ChannelModel(new GfskModulator(frames, sampleRate / baudRate, (float) (2 * Math.PI * devitation / sampleRate), 1.0f), noiseVoltage, 0, 1.0f, null, (i + 1) * 42));
+			NrziEncode source = new NrziEncode(new Scrambler(new ArrayByteInput(transmitter.encode(inputData)), 0x21, 0x00, 16));
+			inputs.add(new ChannelModel(new GfskModulator(source, sampleRate / baudRate, (float) (2 * Math.PI * devitation / sampleRate), 1.0f), noiseVoltage, 0, 1.0f, null, (i + 1) * 42));
 		}
 		inputs.add(new TimeConstraintedSource(new FastNoiseSourceComplex(noiseVoltage, (numberOfFrames + 1) * 42, 1024 * 16), silenceCtx));
 
@@ -85,7 +104,7 @@ public class Ax25G3ruhBer {
 		FloatInput next = new SequentialSource(inputs, ssCtx);
 
 		FskDemodulator demod = new FskDemodulator(next, (int) baudRate, devitation, 1, 2000, false);
-		Ax25G3ruhBeaconSource<RawBeacon> source = new Ax25G3ruhBeaconSource<>(demod, RawBeacon.class, true);
+		Ax25G3ruhBeaconSource<RawBeacon> source = new Ax25G3ruhBeaconSource<>(demod, RawBeacon.class, checksum);
 
 		int totalBits = numberOfFrames * inputData.length * 8;
 		int correctBits = 0;
