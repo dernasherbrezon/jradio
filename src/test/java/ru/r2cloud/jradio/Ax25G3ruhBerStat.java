14b74f8d579dd905d6da45cdc5f5ef1795c4d8e5
dernasherbrezon <rodionovamp@mail.ru>
2022-03-05 22:19:28 +0000

    add missing stats

---
+++ src/test/java/ru/r2cloud/jradio/Ax25G3ruhBerStat.java
@@ -0,0 +1,60 @@
+package ru.r2cloud.jradio;
+
+public class Ax25G3ruhBerStat {
+
+	private float ebno;
+	private double fskBer;
+	private double prepend0;
+	private double prepend3;
+	private double prepend6;
+	private double prepend9;
+
+	public void setFskBer(double fskBer) {
+		this.fskBer = fskBer;
+	}
+
+	public double getFskBer() {
+		return fskBer;
+	}
+
+	public float getEbno() {
+		return ebno;
+	}
+
+	public void setEbno(float ebno) {
+		this.ebno = ebno;
+	}
+
+	public double getPrepend0() {
+		return prepend0;
+	}
+
+	public void setPrepend0(double prepend0) {
+		this.prepend0 = prepend0;
+	}
+
+	public double getPrepend3() {
+		return prepend3;
+	}
+
+	public void setPrepend3(double prepend3) {
+		this.prepend3 = prepend3;
+	}
+
+	public double getPrepend6() {
+		return prepend6;
+	}
+
+	public void setPrepend6(double prepend6) {
+		this.prepend6 = prepend6;
+	}
+
+	public double getPrepend9() {
+		return prepend9;
+	}
+
+	public void setPrepend9(double prepend9) {
+		this.prepend9 = prepend9;
+	}
+
+}
---
+++ src/test/java/ru/r2cloud/jradio/Ax25G3ruhBerStatLength.java
@@ -0,0 +1,60 @@
+package ru.r2cloud.jradio;
+
+public class Ax25G3ruhBerStatLength {
+
+	private float ebno;
+	private double fskBer;
+	private double ax25Byte20Checksum;
+	private double ax25Byte20NoChecksum;
+	private double ax25Byte131Checksum;
+	private double ax25Byte131NoChecksum;
+
+	public float getEbno() {
+		return ebno;
+	}
+
+	public void setEbno(float ebno) {
+		this.ebno = ebno;
+	}
+
+	public double getFskBer() {
+		return fskBer;
+	}
+
+	public void setFskBer(double fskBer) {
+		this.fskBer = fskBer;
+	}
+
+	public double getAx25Byte20Checksum() {
+		return ax25Byte20Checksum;
+	}
+
+	public void setAx25Byte20Checksum(double ax25Byte20Checksum) {
+		this.ax25Byte20Checksum = ax25Byte20Checksum;
+	}
+
+	public double getAx25Byte20NoChecksum() {
+		return ax25Byte20NoChecksum;
+	}
+
+	public void setAx25Byte20NoChecksum(double ax25Byte20NoChecksum) {
+		this.ax25Byte20NoChecksum = ax25Byte20NoChecksum;
+	}
+
+	public double getAx25Byte131Checksum() {
+		return ax25Byte131Checksum;
+	}
+
+	public void setAx25Byte131Checksum(double ax25Byte131Checksum) {
+		this.ax25Byte131Checksum = ax25Byte131Checksum;
+	}
+
+	public double getAx25Byte131NoChecksum() {
+		return ax25Byte131NoChecksum;
+	}
+
+	public void setAx25Byte131NoChecksum(double ax25Byte131NoChecksum) {
+		this.ax25Byte131NoChecksum = ax25Byte131NoChecksum;
+	}
+
+}
