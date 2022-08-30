3bdfed83582d967673ac459daa41ff1bc0d246fc
dernasherbrezon <rodionovamp@mail.ru>
2022-08-30 09:01:11 +0100

    some tests

--- src/test/java/ru/r2cloud/jradio/iris/IrisABeaconTest.java
+++ src/test/java/ru/r2cloud/jradio/iris/IrisABeaconTest.java
@@ -1,5 +1,10 @@
 package ru.r2cloud.jradio.iris;
 
+import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanConstructor;
+import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import org.junit.Test;
 
 import ru.r2cloud.jradio.AssertJson;
@@ -15,4 +20,10 @@ public class IrisABeaconTest {
 		AssertJson.assertObjectsEqual("IrisABeacon.json", result);
 	}
 
+	@Test
+	public void testPojo() {
+		assertThat(IrisABeacon.class, allOf(hasValidBeanConstructor(), hasValidGettersAndSetters()));
+		assertThat(SecondaryHeader.class, allOf(hasValidBeanConstructor(), hasValidGettersAndSetters()));
+		assertThat(Telemetry.class, allOf(hasValidBeanConstructor(), hasValidGettersAndSetters()));
+	}
 }
